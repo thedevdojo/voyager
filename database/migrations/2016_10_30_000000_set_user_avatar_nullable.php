d55c1eac2980515036ea0d74baf0c8f99bd2ac0c
Kunal Varma <kunalvarma05@gmail.com>
2016-10-30 02:21:59 +0530

    Updating avatar column in users table to allow null/empty values

---
+++ database/migrations/2016_10_30_000000_set_user_avatar_nullable.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SetUserAvatarNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('users', function($table)
+        {
+            $table->string('avatar')->nullable()->change();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function($table)
+        {
+            $table->string('avatar')->nullable(false)->change();
+        });
+    }
+}
