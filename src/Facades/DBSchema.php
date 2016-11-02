cbf8e96fb200be4a483a16196927facd178204ab
André Sardo <a.fsardo@gmail.com>
2016-11-02 02:41:33 +0000

    Added POSGRES support

--- src/Facades/DBSchema.php
+++ src/Facades/DBSchema.php
@@ -9,6 +9,8 @@ class DBSchema
 {
 
     const SQLITE = 'sqlite';
+    const MYSQL = 'mysql';
+    const PGSQL = 'pgsql';
 
     public static function tables() 
     {
@@ -16,12 +18,30 @@ class DBSchema
             return DB::select("SELECT name FROM sqlite_master WHERE type='table' UNION ALL SELECT name FROM sqlite_temp_master WHERE type='table'");
         }
 
-        return DB::select('SHOW TABLES');
+        if (static::isMYSQL()) {
+            return DB::select("SHOW TABLES");
+        }
+
+        if (static::isPOSTGRES()) {
+            return DB::select("SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema = 'public'");
+        }
+
+        throw new \Exception("Voyager: Database driver not supported");
     }
 
     public static function isSQLITE() 
     {
         return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) == self::SQLITE;
+    }
+
+    public static function isMYSQL() 
+    {
+        return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) == self::MYSQL;
+    }
+
+    public static function isPOSTGRES() 
+    {
+        return DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME) == self::PGSQL;
     }
 
 }
