ec0b8db1ac665dc15253fc75bdc5e13375b9cc96
Matt Maiorano <maiorano84@hotmail.com>
2016-11-07 23:42:19 -0500

    Derp

--- src/views/master.blade.php
+++ src/views/master.blade.php
@@ -76,7 +76,7 @@
                                 <?php $breadcrumb_url = ''; ?>
                                 @for($i = 1; $i <= count(Request::segments()); $i++)
                                   <?php $breadcrumb_url .= '/' . Request::segment($i); ?>
-                                  @if(Request::segment($i) != config('voyager.routes.prefix')) && !is_numeric(Request::segment($i)))
+                                  @if(Request::segment($i) != config('voyager.routes.prefix') && !is_numeric(Request::segment($i)))
 
                                       @if($i < count(Request::segments()) & $i > 0)
                                         <li class="active"><a href="{{ $breadcrumb_url }}">{{ ucwords(str_replace('-', ' ', str_replace('_', ' ', Request::segment($i)))) }}</a></li>
