53ba7d41a1163b3f9458a5ae80a7146216b13796
Tony Lea <tnylea@gmail.com>
2016-11-10 14:15:04 -0800

    fixing the seed file for the user roles migration fix

--- publishable/database/seeds/UserRolesTableSeeder.php
+++ publishable/database/seeds/UserRolesTableSeeder.php
@@ -19,19 +19,13 @@ class UserRolesTableSeeder extends Seeder
         \DB::table('user_roles')->insert(array (
             0 => 
             array (
-                'id' => 1,
                 'role_id' => 1,
                 'user_id' => 1,
-                'created_at' => '2016-10-21 22:31:20',
-                'updated_at' => '2016-10-21 22:31:20',
             ),
             1 => 
             array (
-                'id' => 2,
                 'role_id' => 2,
                 'user_id' => 1,
-                'created_at' => '2016-10-21 22:31:20',
-                'updated_at' => '2016-10-21 22:31:20',
             ),
         ));
         
