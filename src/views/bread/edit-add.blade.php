c71e725626ac10fd305631ff280b8639309b6438
Joshua Flowers <joshuatf@gmail.com>
2016-11-22 19:27:18 +0700

    Fix relationship class compatiblity for php56

--- src/views/bread/edit-add.blade.php
+++ src/views/bread/edit-add.blade.php
@@ -84,7 +84,8 @@
                                         <select class="form-control select2" name="{{ $row->field }}[]" multiple>
                                             @if(isset($options->relationship))
                                                 <?php $selected_values = $dataTypeContent->{$row->field}->pluck($options->relationship->key)->all(); ?>
-                                                <?php $relationshipOptions = get_class($dataTypeContent->{$row->field}()->getRelated())::all(); ?>
+                                                <?php $relationshipClass = get_class($dataTypeContent->{$row->field}()->getRelated()); ?>
+                                                <?php $relationshipOptions = $relationshipClass::all(); ?>
                                                 @foreach($relationshipOptions as $relationshipOption)
                                                     <option value="{{ $relationshipOption->{$options->relationship->key} }}" @if(in_array($relationshipOption->{$options->relationship->key}, $selected_values)){{ 'selected="selected"' }}@endif>{{ $relationshipOption->{$options->relationship->label} }}</option>
                                                 @endforeach
