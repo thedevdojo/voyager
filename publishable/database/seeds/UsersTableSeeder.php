8cfd36724e84abff81f19927d26603893c72a8fe
Mark Topper <mark@ulties.com>
2016-12-03 21:41:16 +0100

    Refactor UsersTableSeeder

--- publishable/database/seeds/UsersTableSeeder.php
+++ publishable/database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use TCG\Voyager\Models\User;
+use TCG\Voyager\Models\Role;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,20 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('users')->delete();
+        if (User::count() == 0) {
+            $role = Role::where('name', 'admin')->firstOrFail();
 
-        \DB::table('users')->insert([
-            0 => [
-                'id'             => 1,
-                'name'           => 'Admin',
-                'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
+            User::create([
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('password'),
                 'remember_token' => str_random(60),
-                'created_at'     => '2016-01-28 11:20:57',
-                'updated_at'     => '2016-10-25 14:32:23',
-                'avatar'         => 'users/default.png',
-                'role_id'        => 1,
-            ],
-        ]);
+                'role_id' => $role->id,
+            ]);
+        }
     }
 }
