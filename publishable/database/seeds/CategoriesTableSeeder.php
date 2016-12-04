63c34d4685567e44932dc95ca5a32bb0149063f4
Mark Topper <mark@ulties.com>
2016-12-04 11:49:43 +0100

    Refactor CategoriesTableSeeder

--- publishable/database/seeds/CategoriesTableSeeder.php
+++ publishable/database/seeds/CategoriesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use TCG\Voyager\Models\Category;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -11,27 +12,22 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('categories')->delete();
-
-        \DB::table('categories')->insert([
-            0 => [
-                'id'         => 1,
-                'parent_id'  => null,
-                'order'      => 1,
-                'name'       => 'Category 1',
-                'slug'       => 'category-1',
-                'created_at' => '2016-10-27 00:54:55',
-                'updated_at' => '2016-10-27 00:54:55',
-            ],
-            1 => [
-                'id'         => 2,
-                'parent_id'  => null,
-                'order'      => 1,
-                'name'       => 'Category 2',
-                'slug'       => 'category-2',
-                'created_at' => '2016-10-27 00:54:55',
-                'updated_at' => '2016-10-27 00:54:55',
-            ],
+        $category = Category::firstOrNew([
+            'slug' => 'category-1',
         ]);
+        if (!$category->exists) {
+            $category->fill([
+                'name'       => 'Category 1',
+            ])->save();
+        }
+
+        $category = Category::firstOrNew([
+            'slug' => 'category-2',
+        ]);
+        if (!$category->exists) {
+            $category->fill([
+                'name'       => 'Category 2',
+            ])->save();
+        }
     }
 }
