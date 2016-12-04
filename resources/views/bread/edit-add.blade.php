8d7ccfe60bb653ddd67391b12b822ae87e2c014b
Tony Lea <tnylea@gmail.com>
2016-12-03 20:48:45 -0800

    Adding new functionality for the belongsTo relationship and the belongsToMany relationship

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -75,18 +75,26 @@
                                     @elseif($row->type == "select_dropdown")
                                         <?php $options = json_decode($row->details); ?>
                                         @if(isset($options->relationship))
-                                            <?php $selected_value = (isset($dataTypeContent->{$row->field}) && !empty(old($row->field, $dataTypeContent->{$row->field}))) ? old($row->field, $dataTypeContent->{$row->field}) : old($row->field); ?>
-                                            
-                                            <select class="form-control select2" name="{{ $row->field }}">
-                                                <?php $relationshipClass = get_class(app($dataType->model_name)->{$row->field}()->getRelated()); ?>
-                                                <?php $relationshipOptions = $relationshipClass::all(); ?>
-                                                @foreach($relationshipOptions as $key => $relationshipOption)
-                                                    <option value="{{ $relationshipOption->{$options->relationship->key} }}" @if($selected_value == $key){{ 'selected="selected"' }}@endif>{{ $relationshipOption->{$options->relationship->label} }}</option>
-                                                @endforeach
-                                            </select>
+                                            {{-- If this is a relationship and the method does not exist, show a warning message --}}
+                                            @if( !method_exists( $dataType->model_name, $row->field ) )
+                                                <p class="label label-warning"><i class="voyager-warning"></i> Make sure to setup the appropriate relationship in the {{ $row->field . '()' }} method of the {{ $dataType->model_name }} class.</p>   
+                                            @endif
+
+                                            @if( method_exists( $dataType->model_name, $row->field ) )
+                                                <?php $selected_value = (isset($dataTypeContent->{$row->field}) && !empty(old($row->field, $dataTypeContent->{$row->field}))) ? old($row->field, $dataTypeContent->{$row->field}) : old($row->field); ?>
+                                                <select class="form-control select2" name="{{ $row->field }}">
+                                                    <?php $relationshipClass = get_class(app($dataType->model_name)->{$row->field}()->getRelated()); ?>
+                                                    <?php $relationshipOptions = $relationshipClass::all(); ?>
+                                                    @foreach($relationshipOptions as $relationshipOption)
+                                                        <option value="{{ $relationshipOption->{$options->relationship->key} }}" @if($selected_value == $relationshipOption->{$options->relationship->key}){{ 'selected="selected"' }}@endif>{{ $relationshipOption->{$options->relationship->label} }}</option>
+                                                    @endforeach
+                                                </select>
+                                            @else
+                                                <select class="form-control select2" name="{{ $row->field }}"></select>
+                                            @endif
                                         @else
                                             <?php $selected_value = (isset($dataTypeContent->{$row->field}) && !empty(old($row->field, $dataTypeContent->{$row->field}))) ? old($row->field, $dataTypeContent->{$row->field}) : old($row->field); ?>
-                                            <select class="form-control" name="{{ $row->field }}">
+                                            <select class="form-control select2" name="{{ $row->field }}">
                                                 <?php $default = (isset($options->default) && !isset($dataTypeContent->{$row->field})) ? $options->default : NULL; ?>
                                                 @if(isset($options->options))
                                                     @foreach($options->options as $key => $option)
@@ -98,14 +106,22 @@
 
                                     @elseif($row->type == "select_multiple")
                                         <?php $options = json_decode($row->details); ?>
+                                        {{-- If this is a relationship and the method does not exist, show a warning message --}}
+                                        @if(isset($options->relationship) && !method_exists( $dataType->model_name, $row->field ) )
+                                            <p class="label label-warning"><i class="voyager-warning"></i> Make sure to setup the appropriate relationship in the {{ $row->field . '()' }} method of the {{ $dataType->model_name }} class.</p>   
+                                        @endif
+                                        
                                         <select class="form-control select2" name="{{ $row->field }}[]" multiple>
                                             @if(isset($options->relationship))
-                                                <?php $selected_values = isset($dataTypeContent->{$row->field}) ? $dataTypeContent->{$row->field}->pluck($options->relationship->key)->all() : array(); ?>
-                                                <?php $relationshipClass = get_class(app($dataType->model_name)->{$row->field}()->getRelated()); ?>
-                                                <?php $relationshipOptions = $relationshipClass::all(); ?>
-                                                @foreach($relationshipOptions as $relationshipOption)
-                                                    <option value="{{ $relationshipOption->{$options->relationship->key} }}" @if(in_array($relationshipOption->{$options->relationship->key}, $selected_values)){{ 'selected="selected"' }}@endif>{{ $relationshipOption->{$options->relationship->label} }}</option>
-                                                @endforeach
+                                                <!-- Check that the method relationship exists -->
+                                                @if( method_exists( $dataType->model_name, $row->field ) )
+                                                    <?php $selected_values = isset($dataTypeContent->{$row->field}) ? $dataTypeContent->{$row->field}()->getRelated()->pluck($options->relationship->key)->all() : array(); ?>
+                                                    <?php $relationshipClass = get_class(app($dataType->model_name)->{$row->field}()->getRelated()); ?>
+                                                    <?php $relationshipOptions = $relationshipClass::all(); ?>
+                                                    @foreach($relationshipOptions as $relationshipOption)
+                                                        <option value="{{ $relationshipOption->{$options->relationship->key} }}" @if(in_array($relationshipOption->{$options->relationship->key}, $selected_values)){{ 'selected="selected"' }}@endif>{{ $relationshipOption->{$options->relationship->label} }}</option>
+                                                    @endforeach
+                                                @endif
                                             @endif
                                         </select>
 
