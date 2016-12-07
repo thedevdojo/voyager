887a919c8fc3ec36c50f5d7099f089858fe477ac
Mark Topper <mark@ulties.com>
2016-12-07 14:01:43 +0100

    Make admin command (#329)
    * Create AdminCommand.php
    
    * Load admin command

---
+++ src/Commands/AdminCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace TCG\Voyager\Commands;
+
+use TCG\Voyager\Models\Role;
+use TCG\Voyager\Models\User;
+use Illuminate\Console\Command;
+use TCG\Voyager\Models\Permission;
+use Symfony\Component\Console\Input\InputOption;
+
+class AdminCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'voyager:admin';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make sure a user have the admin role, and that the role has all permissions.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $email = $this->argument('email');
+
+        $role = Role::firstOrNew([
+            'name' => 'admin',
+        ]);
+        if (!$role->exists) {
+            $role->fill([
+                'display_name' => 'Administrator',
+            ])->save();
+        }
+
+        $permissions = Permission::all();
+
+        $role->permissions()->sync(
+            $permissions->pluck('id')->all()
+        );
+
+        $user = User::where('email', $email)->firstOrFail();
+        $user->role_id = $role->id;
+        $user->save();
+
+        $this->info('The user have now full access to your site.');
+    }
+
+    /**
+     * Get command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            ['email', InputOption::VALUE_REQUIRED, 'The email of the user.', null],
+        ];
+    }
+}
