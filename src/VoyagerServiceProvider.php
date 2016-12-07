bb3051605fabe2c6833047d460b035eaa44c2df3
Mark Topper <mark@ulties.com>
2016-12-07 13:12:25 +0100

    Load admin command

--- src/VoyagerServiceProvider.php
+++ src/VoyagerServiceProvider.php
@@ -92,5 +92,6 @@ class VoyagerServiceProvider extends ServiceProvider
     {
         $this->commands(Commands\InstallCommand::class);
         $this->commands(Commands\ControllersCommand::class);
+        $this->commands(Commands\AdminCommand::class);
     }
 }
