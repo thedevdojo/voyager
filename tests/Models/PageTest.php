78c0d70393f9eb0647595e0e2a79421adde9a314
Frank Perez <frank@fjp.io>
2016-12-09 20:40:30 -0500

    Apply Style CI Path for Changes.

--- tests/Models/PageTest.php
+++ tests/Models/PageTest.php
@@ -2,8 +2,8 @@
 
 namespace TCG\Voyager\Tests;
 
-use TCG\Voyager\Models\Page;
 use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Models\Page;
 
 class PageTest extends TestCase
 {
@@ -17,31 +17,30 @@ class PageTest extends TestCase
     /** @test */
     public function can_create_a_page_with_logged_in_user_auto_assigned()
     {
-    	// Arrange
-    	$user = Auth::loginUsingId(1);
+        // Arrange
+        $user = Auth::loginUsingId(1);
 
-    	$page = new Page;
+        $page = new Page();
 
-    	$page->fill([
-    		'slug' => 'test-slug',
-    		'title' => 'Test Title',
-    		'excerpt' => 'Test Excerpt',
-    		'body' => 'Test Body',
-    		'meta_description' => 'Test Description',
-    		'meta_keywords' => 'Test Meta Keywords',
-		]);
-    	
-    	// Act
-    	$page->save();
-    
-    	// Assert
-    	$this->assertEquals(1, $page->author_id);
-    	$this->assertEquals('test-slug', $page->slug);
-    	$this->assertEquals('Test Title', $page->title);
-    	$this->assertEquals('Test Excerpt', $page->excerpt);
-    	$this->assertEquals('Test Body', $page->body);
-    	$this->assertEquals('Test Description', $page->meta_description);
-    	$this->assertEquals('Test Meta Keywords', $page->meta_keywords);
-    
+        $page->fill([
+            'slug'             => 'test-slug',
+            'title'            => 'Test Title',
+            'excerpt'          => 'Test Excerpt',
+            'body'             => 'Test Body',
+            'meta_description' => 'Test Description',
+            'meta_keywords'    => 'Test Meta Keywords',
+        ]);
+
+        // Act
+        $page->save();
+
+        // Assert
+        $this->assertEquals(1, $page->author_id);
+        $this->assertEquals('test-slug', $page->slug);
+        $this->assertEquals('Test Title', $page->title);
+        $this->assertEquals('Test Excerpt', $page->excerpt);
+        $this->assertEquals('Test Body', $page->body);
+        $this->assertEquals('Test Description', $page->meta_description);
+        $this->assertEquals('Test Meta Keywords', $page->meta_keywords);
     }
 }
