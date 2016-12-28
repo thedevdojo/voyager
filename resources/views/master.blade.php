c1e856e798022621e059ee68d915a83264c37fcc
Mark Topper <mark@ulties.com>
2016-12-28 11:31:00 +0100

    🍺

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -170,8 +170,12 @@ $menuExpanded = isset($_COOKIE['expandedMenu']) && $_COOKIE['expandedMenu'] == 1
 </div>
 <footer class="app-footer">
     <div class="site-footer-right">
-        Made with <i class="voyager-heart"></i> by <a href="http://thecontrolgroup.com" target="_blank">The Control
-            Group</a> - {{ Voyager::getVersion() }}
+        @if (rand(1,100) == 100)
+            <i class="voyager-rum-1"></i> Made with rum and even more rum
+        @else
+            Made with <i class="voyager-heart"></i> by <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
+        @endif
+        - {{ Voyager::getVersion() }}
     </div>
 </footer>
 <!-- Javascript Libs -->
