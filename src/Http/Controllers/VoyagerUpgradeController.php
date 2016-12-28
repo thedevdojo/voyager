659c716b0ef33d952bfe833c249f20d3a55ba469
Tony Lea <tnylea@gmail.com>
2016-12-27 19:08:09 -0800

    Trial 2 to fix the styleCI

--- src/Http/Controllers/VoyagerUpgradeController.php
+++ src/Http/Controllers/VoyagerUpgradeController.php
@@ -3,8 +3,8 @@
 namespace TCG\Voyager\Http\Controllers;
 
 use Illuminate\Database\Schema\Blueprint;
-use TCG\Voyager\Voyager;
 use Schema;
+use TCG\Voyager\Voyager;
 
 class VoyagerUpgradeController extends Controller
 {
@@ -12,21 +12,22 @@ class VoyagerUpgradeController extends Controller
     {
         $upgraded = $this->upgrade_v0_10_6();
 
-        if($upgraded){
-            return redirect()->route('voyager.dashboard')->with(['message' => "Database Schema has been Updated.", 'alert-type' => 'success',]);
+        if($upgraded) {
+            return redirect()->route('voyager.dashboard')->with(['message' => 'Database Schema has been Updated.', 'alert-type' => 'success']);
         } else {
-        	return redirect()->route('voyager.dashboard');
+            return redirect()->route('voyager.dashboard');
         }
     }
 
     private function upgrade_v0_10_6()
     {
-    	if( !Schema::hasColumn('data_types', 'server_side') ){
+    	if( !Schema::hasColumn('data_types', 'server_side')) {
     		Schema::table('data_types', function (Blueprint $table) {
             	$table->tinyInteger('server_side')->default(0)->after('generate_permissions');
-        	});
-        	return true;
-    	}
+            });
+        	
+            return true;
+        }
     	return false;
     }
 
