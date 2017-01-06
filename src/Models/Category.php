4d1b5295749b8c2a3836c67b3e81f350f247d79f
An Tran <tran.binhan@hotmail.com>
2017-01-06 15:33:49 +0000

    Apply fixes from StyleCI

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -17,7 +17,8 @@ class Category extends Model
             ->orderBy('created_at', 'DESC');
     }
 
-    public function parent_id() {
-        return $this->belongsTo(Category::class);
-    }    
+    public function parent_id()
+    {
+        return $this->belongsTo(self::class);
+    }
 }
