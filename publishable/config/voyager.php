44bbde9f9d67e92d42a7f225aeb3cb1ad0b876ad
bestmomo <colorstonic@free.fr>
2017-01-22 18:55:22 +0100

    Update voyager.php

--- publishable/config/voyager.php
+++ publishable/config/voyager.php
@@ -86,4 +86,22 @@ return [
         ],
     ],
 
+        /*
+    |--------------------------------------------------------------------------
+    | Administration Config
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify voyager administration settings
+    |
+    */
+
+    'admin' => [
+/*        'product' => [
+            'name' => 'Product',
+            'model' => '\App\Product', 
+            'route' => 'admin.products',
+            'image' => '/images/widget-backgrounds/01.png'
+        ],*/
+    ],
+    
 ];
