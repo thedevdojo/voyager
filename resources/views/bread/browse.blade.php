c61f2cdec2f179be73f869a8df8b2d7a21f5f96f
Federico <Frondor@users.noreply.github.com>
2017-01-27 11:36:42 -0300

    TravisCI corrections

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -43,12 +43,14 @@
                                             @elseif($row->type == 'date')
                                             {{ property_exists($options, 'format') ? \Carbon\Carbon::parse($data->{$row->field})->formatLocalized($options->format) : $dataTypeContent->{$row->field} }}
                                             @elseif($row->type == 'checkbox')
-                                                @if(property_exists($options, 'on') && property_exists($options, 'off'))
+                                                @if($options && property_exists($options, 'on') && property_exists($options, 'off'))
                                                     @if($data->{$row->field})
                                                     <span class="label label-info">{{ $options->on }}</span>
                                                     @else
                                                     <span class="label label-primary">{{ $options->off }}</span>
                                                     @endif
+                                                @else
+                                                {{ $data->{$row->field} }}
                                                 @endif
                                             @else
                                                 {{ $data->{$row->field} }}
