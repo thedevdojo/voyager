cfe456bf94d74e2ef69ec7e58e1b5020930dd0dd
Federico <Frondor@users.noreply.github.com>
2017-01-27 07:47:13 -0300

    Refactoring and corrections

--- src/Traits/BreadRelationshipParser.php
+++ src/Traits/BreadRelationshipParser.php
@@ -10,19 +10,20 @@ trait BreadRelationshipParser
 {
     /**
      * Build the relationships array for the model's eager load.
-     *
-     * @param DataType $dataType
-     *
-     * @return array
+     * 
+     * @param  DataType $dataType 
+     * 
+     * @return Array
      */
     protected function getRelationships(DataType $dataType)
     {
-        $relationships = [];
+        $relationships = array();
+
         $dataType->browseRows->each(function ($item) use (&$relationships) {
             $details = json_decode($item->details);
             if (isset($details->relationship) && isset($item->field)) {
                 $relation = $details->relationship;
-                $relationships[$item->field] = function ($query) use ($relation) {
+                $relationships[camel_case($item->field)] = function ($query) use ($relation) {
                     // select only what we need
                     $query->select($relation->key, $relation->label);
                 };
@@ -34,28 +35,22 @@ trait BreadRelationshipParser
 
     /**
      * Replace relationships' keys for labels and create READ links if a slug is provided.
-     *
+     * 
      * @param  $dataTypeContent     Can be either an eloquent Model, Collection or LengthAwarePaginator instance.
-     * @param DataType $dataType
-     *
+     * @param  DataType $dataType
+     * 
      * @return $dataTypeContent
      */
-    protected function resolveRelations($dataTypeContent, DataType $dataType)
+    protected function resolveRelations($dataTypeContent, DataType $dataType, bool $isModel = false)
     {
-        // In case of using server-side pagination, we need to work on the Collection
+        // In case of using server-side pagination, we need to work on the Collection (BROWSE)
         if ($dataTypeContent instanceof LengthAwarePaginator) {
             $dataTypeCollection = $dataTypeContent->getCollection();
         }
-        // If it's a model just make the changes directly on it
-        elseif ($dataTypeContent instanceof Model) {
-            $relations = $dataTypeContent->getRelations();
-            if (!empty($relations)) {
-                foreach ($relations as $field => $value) {
-                    $dataTypeContent = $this->relationToLink($dataTypeContent, $field, $value, $dataType);
-                }
-            }
+        // If it's a model just make the changes directly on it (READ / EDIT)
+        else if ($dataTypeContent instanceof Model) {
 
-            return $dataTypeContent;
+            return $this->relationToLink($dataTypeContent, $dataType);
         }
         // Or we assume it's a Collection
         else {
@@ -63,39 +58,39 @@ trait BreadRelationshipParser
         }
 
         $dataTypeCollection->transform(function ($item) use ($dataType) {
-            $relations = $item->getRelations();
-            if (!empty($relations)) {
-                foreach ($relations as $field => $value) {
-                    $item = $this->relationToLink($item, $field, $value, $dataType);
-                }
-            }
-
-            return $item;
+            return $this->relationToLink($item, $dataType);
         });
 
         return $dataTypeContent instanceof LengthAwarePaginator ? $dataTypeContent->setCollection($dataTypeCollection) : $dataTypeCollection;
     }
 
     /**
-     * Create the URL for relationship's anchors in BROWSE and READ views.
-     *
-     * @param Model    $item     Object to modify
-     * @param string   $field    Relation field to modify (must match the relationship's method)
-     * @param Model    $relation Relation model
-     * @param DataType $dataType
-     *
-     * @return Model $item
+     * Create the URL for relationship's anchors in BROWSE and READ views
+     * 
+     * @param  Model    $item       Object to modify
+     * @param  DataType $dataType  
+     *  
+     * @return Model    $item
      */
-    protected function relationToLink(Model $item, $field, Model $relation, DataType $dataType)
+    protected function relationToLink(Model $item, DataType $dataType)
     {
-        $bread_data = $dataType->browseRows->where('field', $field)->first();
-        $relationData = json_decode($bread_data->details)->relationship;
-        $id = $item[$field];
-        $item[$field] = $relation[$relationData->label];
-        if (isset($relationData->page_slug)) {
-            $item[$field.'_page_slug'] = url($relationData->page_slug, $id);
+        $relations = $item->getRelations();
+        // If there are not-null relations
+        if (! empty($relations) && array_filter($relations)) {
+            foreach ($relations as $field => $relation) {
+                $field = snake_case($field);
+                $bread_data = $dataType->browseRows->where('field', $field)->first();
+                $relationData = json_decode($bread_data->details)->relationship;
+                $id = $item[$field];
+                $item[$field] = $relation[$relationData->label];
+                if (isset($relationData->page_slug)) {
+                    $item[$field.'_page_slug'] = url($relationData->page_slug, $id);
+                }
+
+            }
         }
 
         return $item;
+        
     }
 }
