e1a0efa38d551af361a4593dfb2645094d57b88b
Mark Topper <mark@ulties.com>
2017-02-01 13:45:27 +0100

    Testing pgsql

--- src/Facades/DBSchema.php
+++ src/Facades/DBSchema.php
@@ -48,22 +48,10 @@ class DBSchema
         $connection = config('database.default', 'mysql');
         $driver = config('database.connections.'.$connection.'.driver', 'mysql');
 
-        if ($driver == 'sqlite') {
-            $columns = DB::select(DB::raw("PRAGMA table_info({$table})"));
-
-            return collect($columns)->map(function ($item) {
-                return [
-                    'field'   => $item->name,
-                    'type'    => $item->type,
-                    'null'    => ($item->notnull) ? 'NO' : 'YES',
-                    'key'     => ($item->pk) ? 'PRI' : '',
-                    'default' => ($default = preg_replace("/((^')|('$))/", '', $item->dflt_value)) ? $default : null,
-                    'extra'   => ($item->pk == 1 && $item->type == 'integer') ? 'auto_increment' : '',
-                ];
-            });
-        } else {
-            $schema_name = DB::connection()->getDatabaseName();
-            $raw = "SELECT column_name    AS 'field',
+        switch ($driver) {
+            case 'mysql':
+                $schema_name = DB::connection()->getDatabaseName();
+                $raw = "SELECT column_name    AS 'field',
                        column_type    AS 'type',
                        is_nullable    AS 'null',
                        column_key     AS 'key',
@@ -73,16 +61,76 @@ class DBSchema
                 WHERE  table_schema = '{$schema_name}'
                 AND    table_name = '{$table}'";
 
-            return collect(DB::select(DB::raw($raw)))->map(function ($item) {
-                return [
-                    'field'   => $item->field,
-                    'type'    => $item->type,
-                    'null'    => $item->null,
-                    'key'     => $item->key,
-                    'default' => $item->default,
-                    'extra'   => $item->extra,
-                ];
-            });
+                return collect(DB::select(DB::raw($raw)))->map(function ($item) {
+                    return [
+                        'field'   => $item->field,
+                        'type'    => $item->type,
+                        'null'    => $item->null,
+                        'key'     => $item->key,
+                        'default' => $item->default,
+                        'extra'   => $item->extra,
+                    ];
+                });
+
+            case 'pgsql':
+                $schema_name = DB::connection()->getDatabaseName();
+                $raw = "SELECT  
+                        f.attnum AS number,  
+                        f.attname AS name,  
+                        f.attnum,  
+                        f.attnotnull AS notnull,  
+                        pg_catalog.format_type(f.atttypid,f.atttypmod) AS type,  
+                        CASE  
+                            WHEN p.contype = 'p' THEN 't'  
+                            ELSE 'f'  
+                        END AS primarykey,  
+                        CASE  
+                            WHEN p.contype = 'u' THEN 't'  
+                            ELSE 'f'
+                        END AS uniquekey,
+                        CASE
+                            WHEN p.contype = 'f' THEN g.relname
+                        END AS foreignkey,
+                        CASE
+                            WHEN p.contype = 'f' THEN p.confkey
+                        END AS foreignkey_fieldnum,
+                        CASE
+                            WHEN p.contype = 'f' THEN g.relname
+                        END AS foreignkey,
+                        CASE
+                            WHEN p.contype = 'f' THEN p.conkey
+                        END AS foreignkey_connnum,
+                        CASE
+                            WHEN f.atthasdef = 't' THEN d.adsrc
+                        END AS default
+                    FROM pg_attribute f  
+                        JOIN pg_class c ON c.oid = f.attrelid  
+                        JOIN pg_type t ON t.oid = f.atttypid  
+                        LEFT JOIN pg_attrdef d ON d.adrelid = c.oid AND d.adnum = f.attnum  
+                        LEFT JOIN pg_namespace n ON n.oid = c.relnamespace  
+                        LEFT JOIN pg_constraint p ON p.conrelid = c.oid AND f.attnum = ANY (p.conkey)  
+                        LEFT JOIN pg_class AS g ON p.confrelid = g.oid  
+                    WHERE c.relkind = 'r'::char  
+                        AND n.nspname = '{$schema_name}'
+                        AND c.relname = '{$table}'
+                        AND f.attnum > 0 ORDER BY number
+                ";
+
+                dd(DB::select(DB::raw($raw)));
+
+            default:
+                $columns = DB::select(DB::raw("PRAGMA table_info({$table})"));
+
+                return collect($columns)->map(function ($item) {
+                    return [
+                        'field'   => $item->name,
+                        'type'    => $item->type,
+                        'null'    => ($item->notnull) ? 'NO' : 'YES',
+                        'key'     => ($item->pk) ? 'PRI' : '',
+                        'default' => ($default = preg_replace("/((^')|('$))/", '', $item->dflt_value)) ? $default : null,
+                        'extra'   => ($item->pk == 1 && $item->type == 'integer') ? 'auto_increment' : '',
+                    ];
+                });
         }
     }
 }
