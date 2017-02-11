6fd603de93f897684b3ba97d5279724ab8823829
Federico <lobo__911@hotmail.com>
2017-02-10 21:27:01 -0300

    Fix #709

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -152,18 +152,20 @@
             });
         @endif
 
+        var deleteFormAction;
         $('td').on('click', '.delete', function (e) {
             var form = $('#delete_form')[0];
 
-            form.action = parseActionUrl(form.action, $(this).data('id'));
+            if (!deleteFormAction) { // Save form action initial value
+                deleteFormAction = form.action;
+            }
+
+            form.action = deleteFormAction.match(/\/[0-9]+$/)
+                ? deleteFormAction.replace(/([0-9]+$)/, $(this).data('id'))
+                : deleteFormAction + '/' + $(this).data('id');
+            console.log(form.action);
 
             $('#delete_modal').modal('show');
         });
-
-        function parseActionUrl(action, id) {
-            return action.match(/\/[0-9]+$/)
-                ? action.replace(/([0-9]+$)/, id)
-                : action + '/' + id;
-        }
     </script>
 @stop
