06f82bbe78cfbdbe75ee853f047f8d17e28ce10f
Olav Schettler <olav@schettler.net>
2017-02-14 14:36:09 +0100

    #674 Check for empty() rather than "null"

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -225,7 +225,7 @@ abstract class Controller extends BaseController
             /********** TIMESTAMP TYPE **********/
             case 'timestamp':
                 if ($request->isMethod('PUT')) {
-                    if (null === $request->input($row->field)) {
+                    if (empty($request->input($row->field))) {
                         $content = null;
                     } else {
                         $content = gmdate('Y-m-d H:i:s', strtotime($request->input($row->field)));
