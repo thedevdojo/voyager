67fbcb2ac3f25ebb18c1751a778dc7e2a84808f5
Bart <denbatte@gmail.com>
2017-02-16 23:02:34 +0100

    Fix capital to small letters (https://github.com/the-control-group/voyager/issues/745)
    Fix https://github.com/the-control-group/voyager/issues/745 : capital to small letters.

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -124,14 +124,14 @@
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
                                 aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title"><i class="voyager-trash"></i> Are you sure you want to delete
-                        this {{ $dataType->display_name_singular }}?</h4>
+                        this {{ strtolower($dataType->display_name_singular) }}?</h4>
                 </div>
                 <div class="modal-footer">
                     <form action="{{ route('voyager.'.$dataType->slug.'.index') }}" id="delete_form" method="POST">
                         {{ method_field("DELETE") }}
                         {{ csrf_field() }}
                         <input type="submit" class="btn btn-danger pull-right delete-confirm"
-                               value="Yes, Delete This {{ $dataType->display_name_singular }}">
+                               value="Yes, delete this {{ strtolower($dataType->display_name_singular) }}">
                     </form>
                     <button type="button" class="btn btn-default pull-right" data-dismiss="modal">Cancel</button>
                 </div>
