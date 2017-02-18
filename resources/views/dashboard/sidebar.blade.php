c1ec8894875599e37395f2a20ecf3bf6d565de1d
Tony Lea <tnylea@gmail.com>
2017-02-17 18:13:36 -0800

    Adding the Voyager::setting('admin_logo_image') to admin instead of Voyager::setting('logo')

--- resources/views/dashboard/sidebar.blade.php
+++ resources/views/dashboard/sidebar.blade.php
@@ -4,7 +4,7 @@
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
                     <div class="logo-icon-container">
-                        <?php $admin_logo_img = Voyager::setting('logo', ''); ?>
+                        <?php $admin_logo_img = Voyager::setting('admin_icon_image', ''); ?>
                         @if($admin_logo_img == '')
                             <img src="{{ config('voyager.assets_path') }}/images/logo-icon-light.png" alt="Logo Icon">
                         @else
