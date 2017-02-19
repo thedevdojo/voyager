69c8b8570702d7ec5f0960f75abfcfce1bdc1838
Muah <muah003@gmail.com>
2017-02-19 19:57:31 +0200

    add bio to user columns to avoid getting error in profile view

--- publishable/database/migrations/2016_01_01_000000_add_voyager_user_fields.php
+++ publishable/database/migrations/2016_01_01_000000_add_voyager_user_fields.php
@@ -10,8 +10,9 @@ class AddVoyagerUserFields extends Migration
     public function up()
     {
         Schema::table('users', function ($table) {
-            $table->string('avatar')->nullable()->after('email');
             $table->integer('role_id')->nullable()->after('id');
+            $table->string('avatar')->nullable()->after('email');
+            $table->mediumText('bio')->nullable()->after('avatar');
         });
     }
 
@@ -21,8 +22,9 @@ class AddVoyagerUserFields extends Migration
     public function down()
     {
         Schema::table('users', function ($table) {
-            $table->dropColumn('avatar');
             $table->dropColumn('role_id');
+            $table->dropColumn('avatar');
+            $table->dropColumn('bio');
         });
     }
 }
