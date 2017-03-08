f1e804aecf49c8c939190668e959e9e90e624e49
Blaz Cigale <blaz.cigale@gmail.com>
2017-03-08 22:13:08 +0000

    Javascript for image removal

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -89,15 +89,74 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade modal-danger" id="confirm_delete_modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"
+                            aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="voyager-warning"></i> Are You Sure</h4>
+                </div>
+
+                <div class="modal-body">
+                    <h4>Are you sure you want to delete '<span class="confirm_delete_name"></span>'</h4>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger" id="confirm_delete">Yes, Delete it!
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Delete File Modal -->
 @stop
 
 @section('javascript')
     <script>
+        var params = {}
+
         $('document').ready(function () {
             $('.toggleswitch').bootstrapToggle();
 
             $('.side-body input[data-slug-origin]').each(function(i, el) {
                 $(el).slugify();
+            });
+
+            $('.form-group').on('click', '.remove-multi-image', function (e) {
+                var $image = $(this).parent().siblings('img');
+
+                params = {
+                    slug:   '{{ $dataTypeContent->getTable() }}',
+                    image:  $image.data('image'),
+                    id:     $image.data('id'),
+                    field:  $image.parent().data('field-name'),
+                    _token: '{{ csrf_token() }}'
+                }
+
+                $('.confirm_delete_name').text($image.data('image'));
+                $('#confirm_delete_modal').modal('show');
+
+                $('#confirm_delete').on('click', function(){
+                    $.post('{{ route('voyager.media.remove') }}', params, function (response) {
+                        if ( response
+                            && response.data
+                            && response.data.status
+                            && response.data.status == 200 ) {
+
+                            toastr.success(response.data.message);
+                            $image.parent().fadeOut(300, function() { $(this).remove(); })
+                        } else {
+                            toastr.error("Error removing image.");
+                        }
+                    });
+
+                    $('#confirm_delete_modal').modal('hide');
+                });
+
             });
         });
     </script>
