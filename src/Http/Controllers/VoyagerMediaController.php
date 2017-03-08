a1e5ba31c2a2169dd37f09aed7f19742f0b0dfa7
Blaz Cigale <blaz.cigale@gmail.com>
2017-03-08 21:41:25 +0000

    Added Media remove endpoint with logic

--- src/Http/Controllers/VoyagerMediaController.php
+++ src/Http/Controllers/VoyagerMediaController.php
@@ -215,4 +215,80 @@ class VoyagerMediaController extends Controller
 
         return $files;
     }
+
+    // REMOVE FILE
+    public function remove(Request $request)
+    {
+        try {
+            // GET THE SLUG, ex. 'posts', 'pages', etc.
+            $slug = $request->get('slug');
+
+            // GET image name
+            $image = $request->get('image');
+
+            // GET record id
+            $id = $request->get('id');
+
+            // GET field name
+            $field = $request->get('field');
+
+            // GET THE DataType based on the slug
+            $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+            // Check permission
+            Voyager::canOrFail('delete_'.$dataType->name);
+
+            // Load model and find record
+            $model = app($dataType->model_name);
+            $data = $model::find([$id])->first();
+
+            // Check if field exists
+            if(!isset($data->{$field})) throw new Exception("Field does not exist", 400);
+
+            // Check if valid json
+            if(is_null(@json_decode($data->{$field}))) throw new Exception("Invalid json", 500);
+
+            // Decode field value
+            $fieldData = @json_decode($data->{$field}, true);
+
+            // Flip keys and values
+            $fieldData = array_flip($fieldData);
+
+            // Check if image exists in array
+            if(!array_key_exists($image, $fieldData)) throw new Exception("Image does not exist", 400);
+
+            // Remove image from array
+            unset($fieldData[$image]);
+
+            // Generate json and update field
+            $data->{$field} = json_encode(array_flip($fieldData));
+            $data->save();
+
+            return response()->json([
+               'data' => [
+                   'status'     => 200,
+                   'message'    => 'Image removed',
+               ]
+            ]);
+
+        } catch (Exception $e) {
+            $code = 500;
+            $message = "Internal error";
+
+            if($e->getCode()) {
+                $code = $e->getCode();
+            }
+
+            if($e->getMessage()) {
+                $message = $e->getMessage();
+            }
+
+            return response()->json([
+                'data' => [
+                    'status'    => $code,
+                    'message'   => $message,
+                ]
+            ], $code);
+        }
+    }
 }
