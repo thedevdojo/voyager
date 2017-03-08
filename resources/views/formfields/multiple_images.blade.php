b11792a94630c30d7b7e59113ae7da23a4da379c
Blaz Cigale <blaz.cigale@gmail.com>
2017-03-08 21:41:59 +0000

    Added filed name and row id

--- resources/views/formfields/multiple_images.blade.php
+++ resources/views/formfields/multiple_images.blade.php
@@ -3,8 +3,8 @@
     <?php $images = json_decode($dataTypeContent->{$row->field}); ?>
     @if($images != null)
         @foreach($images as $image)
-            <div class="image-tool-box">
-                <img src="{{ Voyager::image( $image ) }}" />
+            <div class="image-tool-box" data-field-name="{{ $row->field }}" >
+                <img src="{{ Voyager::image( $image ) }}" data-image="{{ $image }}" data-id="{{ $dataTypeContent->id }}" />
                 <div class="image-tools">
                     <i class="glyphicon glyphicon-remove remove-multi-image" title="Remove image"></i>
                 </div>
