00c2878d9fa168a1882a9f2df5248182c47f730d
Bruno Torrinha <bruno@torrinha.com>
2017-03-31 04:46:35 +0100

    Removed methods starting with underscore

--- publishable/database/seeds/TranslationsTableSeeder.php
+++ publishable/database/seeds/TranslationsTableSeeder.php
@@ -33,27 +33,27 @@ class TranslationsTableSeeder extends Seeder
         $_tpl = ['data_types', $_fld];
         $dtp = DataType::where($_fld, 'Post')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Post');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Post');
         }
         $dtp = DataType::where($_fld, 'Page')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Página');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Página');
         }
         $dtp = DataType::where($_fld, 'User')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Utilizador');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Utilizador');
         }
         $dtp = DataType::where($_fld, 'Category')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Categoria');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Categoria');
         }
         $dtp = DataType::where($_fld, 'Menu')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Menu');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Menu');
         }
         $dtp = DataType::where($_fld, 'Role')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Função');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Função');
         }
 
         // Adding translations for 'display_name_plural'
@@ -62,27 +62,27 @@ class TranslationsTableSeeder extends Seeder
         $_tpl = ['data_types', $_fld];
         $dtp = DataType::where($_fld, 'Posts')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Posts');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Posts');
         }
         $dtp = DataType::where($_fld, 'Pages')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Páginas');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Páginas');
         }
         $dtp = DataType::where($_fld, 'Users')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Utilizadores');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Utilizadores');
         }
         $dtp = DataType::where($_fld, 'Categories')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Categorias');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Categorias');
         }
         $dtp = DataType::where($_fld, 'Menus')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Menus');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Menus');
         }
         $dtp = DataType::where($_fld, 'Roles')->firstOrFail();
         if ($dtp->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $dtp->id), 'Funções');
+            $this->trans('pt', $this->arr($_tpl, $dtp->id), 'Funções');
         }
     }
 
@@ -95,21 +95,21 @@ class TranslationsTableSeeder extends Seeder
     {
         $page = Page::where('slug', 'hello-world')->firstOrFail();
         if ($page->exists) {
-            $_arr = $this->_arr(['pages', 'title'], $page->id);
-            $this->_trans('pt', $_arr, 'Olá Mundo');
+            $_arr = $this->arr(['pages', 'title'], $page->id);
+            $this->trans('pt', $_arr, 'Olá Mundo');
             /**
              * For configuring additional languages use it e.g.
              *
              * ```
-             *   $this->_trans('es', $_arr, 'hola-mundo');
-             *   $this->_trans('de', $_arr, 'hallo-welt');
+             *   $this->trans('es', $_arr, 'hola-mundo');
+             *   $this->trans('de', $_arr, 'hallo-welt');
              * ```
              */
-            $_arr = $this->_arr(['pages', 'slug'], $page->id);
-            $this->_trans('pt', $_arr, 'ola-mundo');
+            $_arr = $this->arr(['pages', 'slug'], $page->id);
+            $this->trans('pt', $_arr, 'ola-mundo');
 
-            $_arr = $this->_arr(['pages', 'body'], $page->id);
-            $this->_trans('pt', $_arr, '<p>Olá Mundo. Scallywag grog swab Cat o\'nine tails scuttle rigging hardtack cable nipper Yellow Jack. Handsomely spirits knave lad killick landlubber or just lubber deadlights chantey pinnace crack Jennys tea cup. Provost long clothes black spot Yellow Jack bilged on her anchor league lateen sail case shot lee tackle.</p>'
+            $_arr = $this->arr(['pages', 'body'], $page->id);
+            $this->trans('pt', $_arr, '<p>Olá Mundo. Scallywag grog swab Cat o\'nine tails scuttle rigging hardtack cable nipper Yellow Jack. Handsomely spirits knave lad killick landlubber or just lubber deadlights chantey pinnace crack Jennys tea cup. Provost long clothes black spot Yellow Jack bilged on her anchor league lateen sail case shot lee tackle.</p>'
                                         ."\r\n".'<p>Ballast spirits fluke topmast me quarterdeck schooner landlubber or just lubber gabion belaying pin. Pinnace stern galleon starboard warp carouser to go on account dance the hempen jig jolly boat measured fer yer chains. Man-of-war fire in the hole nipperkin handsomely doubloon barkadeer Brethren of the Coast gibbet driver squiffy.</p>');
         }
     }
@@ -122,68 +122,68 @@ class TranslationsTableSeeder extends Seeder
     private function menusTranslations()
     {
         $_tpl = ['menu_items', 'title'];
-        $mItem = $this->_menuItem('Dashboard');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Painel de Controle');
+        $_item = $this->mItem('Dashboard');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Painel de Controle');
         }
 
-        $mItem = $this->_menuItem('Media');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Media');
+        $_item = $this->mItem('Media');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Media');
         }
 
-        $mItem = $this->_menuItem('Posts');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Publicações');
+        $_item = $this->mItem('Posts');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Publicações');
         }
 
-        $mItem = $this->_menuItem('Users');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Utilizadores');
+        $_item = $this->mItem('Users');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Utilizadores');
         }
 
-        $mItem = $this->_menuItem('Categories');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Categorias');
+        $_item = $this->mItem('Categories');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Categorias');
         }
 
-        $mItem = $this->_menuItem('Pages');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Páginas');
+        $_item = $this->mItem('Pages');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Páginas');
         }
 
-        $mItem = $this->_menuItem('Roles');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Funções');
+        $_item = $this->mItem('Roles');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Funções');
         }
 
-        $mItem = $this->_menuItem('Tools');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Ferramentas');
+        $_item = $this->mItem('Tools');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Ferramentas');
         }
 
-        $mItem = $this->_menuItem('Menu Builder');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Menus');
+        $_item = $this->mItem('Menu Builder');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Menus');
         }
 
-        $mItem = $this->_menuItem('Database');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Base de dados');
+        $_item = $this->mItem('Database');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Base de dados');
         }
 
-        $mItem = $this->_menuItem('Settings');
-        if ($mItem->exists) {
-            $this->_trans('pt', $this->_arr($_tpl, $mItem->id), 'Configurações');
+        $_item = $this->mItem('Settings');
+        if ($_item->exists) {
+            $this->trans('pt', $this->arr($_tpl, $_item->id), 'Configurações');
         }
     }
 
-    protected function _menuItem($title)
+    private function mItem($title)
     {
         return MenuItem::where('title', $title)->firstOrFail();
     }
 
-    protected function _arr($par, $id)
+    private function arr($par, $id)
     {
         return [
             'table_name'  => $par[0],
@@ -192,7 +192,7 @@ class TranslationsTableSeeder extends Seeder
         ];
     }
 
-    protected function _trans($lang, $keys, $value)
+    private function trans($lang, $keys, $value)
     {
         $_t = Translation::firstOrNew(array_merge($keys, [
             'locale' => $lang,
