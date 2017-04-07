d9e345aa766686b53aaf1a7a13a198c93f3f1bc2
AdrianKuriata <adriandj83@gmail.com>
2017-04-07 20:55:13 +0200

    Update Controller.php

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -96,7 +96,8 @@ abstract class Controller extends BaseController
         return $data;
     }
 
-    public function validateBread($request, $data){
+    public function validateBread($request, $data)
+    {
         $rules = [];
         $messages = [];
 
