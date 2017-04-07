b69ef6316b4c8c1639f86aa896efa2bbf5436010
emchooo <emir.softic@gmail.com>
2017-04-07 06:21:00 +0200

    styles

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -56,15 +56,14 @@ class Post extends Model
     /**
     *   Method for returning specific thumbnail for post.
     */
-
-    public function thumbnail($type) 
+    public function thumbnail($type)
     {
         // We take image from posts field
         $image  = $this->attributes['image'];
         // We need to get extension type ( .jpeg , .png ...)
         $ext    = pathinfo($image, PATHINFO_EXTENSION);
         // We remove extension from file name so we can append thumbnail type
-        $name   = rtrim($image,'.'.$ext);
+        $name   = rtrim($image, '.'. $ext);
         // We merge original name + type + extension
         return $name . '-' . $type . '.' . $ext;
     }
