8d8f89b48d775b58f466ea65dd19e2549630f088
AdrianKuriata <adriandj83@gmail.com>
2017-04-08 10:48:23 +0200

    Update val.js
    Sending just clicked form and validation with files working properly.

--- publishable/assets/js/val.js
+++ publishable/assets/js/val.js
@@ -1,29 +1,42 @@
 $(document).ready(function(){
-	$(".form-edit-add").submit(function(e){
-		e.preventDefault()
+	$("[type='submit']").on('click', function(e){
+		$($(this).parents('form')).submit(function(e){
+			e.preventDefault();
+			var form = $(this);
+			var url = $(this).attr('action');
+			var data = new FormData(this);
 
-		var url = $(this).attr('action');
-		var data = $(this).serializeArray();
-
-		$.ajax({
-			url: url,
-			type: 'POST',
-			dataType: 'json',
-			data: data,
-			beforeSend: function(){
-				$("body").css("cursor", "progress");
-				$(".form-group").removeClass("has-error");
-				$(".form-group").find(".help-block").text("");
-			},
-			success: function(d){
-				$("body").css("cursor", "auto");
-				$.each(d.errors, function(key, row){
-					$("[name='"+key+"']").parents(".form-group").addClass("has-error").append("<span class='help-block'>"+row+"</span>");
-				});
-			},
-			error: function(){
-				$(".form-edit-add").unbind("submit").submit();
-			}
+			$.ajax({
+				url: url,
+				type: 'POST',
+				dataType: 'json',
+				data: data,
+				processData: false,
+				contentType: false,
+				beforeSend: function(){
+					$("body").css("cursor", "progress");
+					$(".form-group").removeClass("has-error");
+					$(".help-block").empty();
+				},
+				success: function(d){
+					$("body").css("cursor", "auto");
+					if($(".help-block").length > 0)
+					{
+						$.each(d.errors, function(key, row){
+							$("[name='"+key+"']").parents(".form-group").addClass("has-error").find(".help-block").text(row);
+						});
+					}
+					else
+					{
+						$.each(d.errors, function(key, row){
+							$("[name='"+key+"']").parents(".form-group").addClass("has-error").append("<span class='help-block'>"+row+"</span>");
+						});
+					}
+				},
+				error: function(){
+					$(form).unbind("submit").submit();
+				}
+			});
 		});
 	});
 });
