c84f8c323cd7a19738e5ca06234a6e1df8d78410
AdrianKuriata <adriandj83@gmail.com>
2017-04-08 11:54:58 +0200

    Update val.js
    Fixed issue with forms which deleting rows, now working good.

--- publishable/assets/js/val.js
+++ publishable/assets/js/val.js
@@ -3,40 +3,49 @@ $(document).ready(function(){
 		$($(this).parents('form')).submit(function(e){
 			e.preventDefault();
 			var form = $(this);
-			var url = $(this).attr('action');
-			var data = new FormData(this);
+			var method = $(form).find('[name="_method"]').attr('value');
 
-			$.ajax({
-				url: url,
-				type: 'POST',
-				dataType: 'json',
-				data: data,
-				processData: false,
-				contentType: false,
-				beforeSend: function(){
-					$("body").css("cursor", "progress");
-					$(".form-group").removeClass("has-error");
-					$(".help-block").empty();
-				},
-				success: function(d){
-					$("body").css("cursor", "auto");
-					if($(".help-block").length > 0)
-					{
-						$.each(d.errors, function(key, row){
-							$("[name='"+key+"']").parents(".form-group").addClass("has-error").find(".help-block").text(row);
-						});
+			if(method != 'DELETE' || method == undefined)
+			{
+				var url = $(this).attr('action');
+				var data = new FormData(this);
+
+				$.ajax({
+					url: url,
+					type: 'POST',
+					dataType: 'json',
+					data: data,
+					processData: false,
+					contentType: false,
+					beforeSend: function(){
+						$("body").css("cursor", "progress");
+						$(".form-group").removeClass("has-error");
+						$(".help-block").empty();
+					},
+					success: function(d){
+						$("body").css("cursor", "auto");
+						if($(".help-block").length > 0)
+						{
+							$.each(d.errors, function(key, row){
+								$("[name='"+key+"']").parents(".form-group").addClass("has-error").find(".help-block").text(row);
+							});
+						}
+						else
+						{
+							$.each(d.errors, function(key, row){
+								$("[name='"+key+"']").parents(".form-group").addClass("has-error").append("<span class='help-block'>"+row+"</span>");
+							});
+						}
+					},
+					error: function(){
+						$(form).unbind("submit").submit();
 					}
-					else
-					{
-						$.each(d.errors, function(key, row){
-							$("[name='"+key+"']").parents(".form-group").addClass("has-error").append("<span class='help-block'>"+row+"</span>");
-						});
-					}
-				},
-				error: function(){
-					$(form).unbind("submit").submit();
-				}
-			});
+				});
+			}
+			else
+			{
+				$(form).unbind("submit").submit();
+			}
 		});
 	});
 });
