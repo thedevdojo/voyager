750c00034e07d28aca9705ed6341de76a3079406
Gustavo Novaro <gnovaro@users.noreply.github.com>
2017-04-11 16:57:46 -0300

    Create 2017_11_04_000000_alter_post_nullable_fields_table.php
    Make some posts fields nullable by default

---
+++ publishable/database/migrations/2017_11_04_000000_alter_post_nullable_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+class AlterPostNullableFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('excerpt')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->text('excerpt');
+            $table->text('meta_description');
+            $table->text('meta_keywords');
+        });
+    }
+}
