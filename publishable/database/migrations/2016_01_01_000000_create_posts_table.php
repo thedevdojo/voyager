2c25de843f65b11bc3d3365171c7880573d84ece
Gustavo Novaro <gnovaro@users.noreply.github.com>
2017-04-11 16:13:01 -0300

    Meta description and keywords nullable

--- publishable/database/migrations/2016_01_01_000000_create_posts_table.php
+++ publishable/database/migrations/2016_01_01_000000_create_posts_table.php
@@ -23,8 +23,8 @@ class CreatePostsTable extends Migration
             $table->text('body');
             $table->string('image')->nullable();
             $table->string('slug')->unique();
-            $table->text('meta_description');
-            $table->text('meta_keywords');
+            $table->text('meta_description')->nullable();
+            $table->text('meta_keywords')->nullable();
             $table->enum('status', ['PUBLISHED', 'DRAFT', 'PENDING'])->default('DRAFT');
             $table->boolean('featured')->default(0);
             $table->timestamps();
