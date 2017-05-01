de9cb11777538e07e480a66fe5e39e3a912919a9
Maxime <dodmax@gmail.com>
2017-05-01 19:03:55 +0700

    Scroll first error into view after ajax validation

--- publishable/assets/js/val.js
+++ publishable/assets/js/val.js
@@ -22,6 +22,14 @@ $(document).ready(function(){
 				$("body").css("cursor", "auto");
 
 				$.each(d.errors, function(key, row){
+                                        //Scroll to first error
+                                        if (Object.keys(d.errors).indexOf(key) === 0) {
+                                            $('html, body').animate({
+                                                scrollTop: $("[name='"+key+"']").parent().offset().top
+                                                        - $('nav.navbar').height() + 'px'
+                                            }, 'fast');
+                                        }
+                                        
 					$("[name='"+key+"']").parent().addClass("has-error");
 					$("[name='"+key+"']").parent().append("<span class='help-block' style='color:#f96868'>"+row+"</span>")
 				});
