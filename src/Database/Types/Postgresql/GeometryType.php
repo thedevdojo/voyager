5b68723593844d673db07e6b339903a7f2b2f507
abdgad <abdgad@users.noreply.github.com>
2017-05-03 00:32:21 +0100

    Add GeometryType for Postgresql (#1211)
    Thanks!

---
+++ src/Database/Types/Postgresql/GeometryType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TCG\Voyager\Database\Types\Postgresql;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use TCG\Voyager\Database\Types\Type;
+
+class GeometryType extends Type
+{
+    const NAME = 'geometry';
+
+    public function getSQLDeclaration(array $field, AbstractPlatform $platform)
+    {
+        return 'geometry';
+    }
+}
