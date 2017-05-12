817d7aff859e3480b9bfdd88351acfd80a238221
Chris E <chris@sourcecube.de>
2017-05-12 18:01:28 +0200

    introducing factories to tests

---
+++ tests/database/factories/UserFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+$factory->define(\TCG\Voyager\Models\User::class, function(Faker\Generator $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'role_id' => function() {
+            return factory(\TCG\Voyager\Models\Role::class)->create()->id;
+        },
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+
+});
