326ee3806d126ce657ebabd5b9469ac34b474480
Tony Lea <tnylea@gmail.com>
2017-06-02 20:31:15 -0700

    Adding new buttons styles

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,9 +43,15 @@
 
     <!-- Few Dynamic Styles -->
     <style type="text/css">
-        .voyager .side-menu .navbar-header, .widget .btn-primary, .widget .btn-primary:focus, .widget .btn-primary:hover, .widget .btn-primary:active, .widget .btn-primary.active, .widget .btn-primary:active:focus{
+        .voyager .side-menu .navbar-header {
             background:{{ config('voyager.primary_color','#22A7F0') }};
             border-color:{{ config('voyager.primary_color','#22A7F0') }};
+        }
+        .widget .btn-primary{
+            border-color:{{ config('voyager.primary_color','#22A7F0') }};
+        }
+        .widget .btn-primary:focus, .widget .btn-primary:hover, .widget .btn-primary:active, .widget .btn-primary.active, .widget .btn-primary:active:focus{
+            background:{{ config('voyager.primary_color','#22A7F0') }};
         }
         .voyager .breadcrumb a{
             color:{{ config('voyager.primary_color','#22A7F0') }};
