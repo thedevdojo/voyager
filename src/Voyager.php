603ea5d6eb7acb7097e510744a4254197c50e12f
Mark Topper <mark@ulties.com>
2017-06-21 19:47:16 +0200

    Remake PR#1285

--- src/Voyager.php
+++ src/Voyager.php
@@ -166,13 +166,16 @@ class Voyager
         // Check if permission exist
         $exist = $this->permissions->where('key', $permission)->first();
 
-        if ($exist) {
-            $user = $this->getUser();
-            if ($user == null || !$user->hasPermission($permission)) {
-                return false;
-            }
+        // Permission not found
+        if (!$exist) {
+            throw new \Exception('Permission does not exist', 400);
         }
 
+        $user = $this->getUser();
+        if ($user == null || !$user->hasPermission($permission)) {
+            return false;
+        }
+  
         return true;
     }
 
