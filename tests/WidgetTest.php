d966cd87d647b95e879807af0efd914508485667
Mark Topper <mark@ulties.com>
2017-06-21 14:15:41 +0200

    Refactor and fix spacing issues

--- tests/WidgetTest.php
+++ tests/WidgetTest.php
@@ -27,44 +27,9 @@ class WidgetTest extends TestCase
     {
         $dimmers = Facade::group('voyager::dimmers');
 
-        $html = "
-            <div class=\"panel widget center bgimage\" style=\"margin-bottom:0;overflow:hidden;background-image:url('http://localhost/vendor/tcg/voyager/assets/images/widget-backgrounds/02.png');\">
-                <div class=\"dimmer\"></div>
-                <div class=\"panel-content\">
-                    <i class='voyager-group'></i>
-                    <h4>1 user</h4>
-                    <p>You have 1 user in your database. Click on button below to view all users.</p>
-                    <a href=\"http://localhost/admin/users\" class=\"btn btn-primary\">View all users</a>
-                </div>
-            </div>
-            <hr>
-            <div class=\"panel widget center bgimage\" style=\"margin-bottom:0;overflow:hidden;background-image:url('http://localhost/vendor/tcg/voyager/assets/images/widget-backgrounds/03.png');\">
-                <div class=\"dimmer\"></div>
-                <div class=\"panel-content\">
-                    <i class='voyager-group'></i>
-                    <h4>4 posts</h4>
-                    <p>You have 4 posts in your database. Click on button below to view all posts.</p>
-                    <a href=\"http://localhost/admin/posts\" class=\"btn btn-primary\">View all posts</a>
-                </div>
-            </div>
-            <hr>
-            <div class=\"panel widget center bgimage\" style=\"margin-bottom:0;overflow:hidden;background-image:url('http://localhost/vendor/tcg/voyager/assets/images/widget-backgrounds/03.png');\">
-                <div class=\"dimmer\"></div>
-                <div class=\"panel-content\">
-                    <i class='voyager-group'></i>
-                    <h4>1 page</h4>
-                    <p>You have 1 page in your database. Click on button below to view all pages.</p>
-                    <a href=\"http://localhost/admin/pages\" class=\"btn btn-primary\">View all pages</a>
-                </div>
-            </div>";
-
-        $replace = function ($html) {
-            return preg_replace('/\s+/', '', $html);
-        };
-
         $this->assertEquals(
-            $replace($html),
-            $replace($dimmers->setSeparator('<hr>')->display())
+            file_get_contents(__DIR__.'/rendered_widgets.html'),
+            (string) $dimmers->setSeparator("\n<hr>\n")->display()
         );
     }
 
