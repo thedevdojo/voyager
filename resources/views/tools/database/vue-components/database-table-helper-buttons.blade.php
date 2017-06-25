736096e4d5163be821f8d6d12e8082f10ff1d8ac
Gabriel Jaime <acagabo@gmail.com>
2017-06-25 03:09:22 -0300

    Update database-table-helper-buttons.blade.php

--- resources/views/tools/database/vue-components/database-table-helper-buttons.blade.php
+++ resources/views/tools/database/vue-components/database-table-helper-buttons.blade.php
@@ -2,7 +2,7 @@
     <div>
         <div class="btn btn-success" @click="addNewColumn">+ {{ __('voyager.database.add_new_column') }}</div>
         <div class="btn btn-success" @click="addTimestamps">+ {{ __('voyager.database.add_timestamps') }}</div>
-        <div class="btn btn-success" @click="addSoftDeletes">+ {{ __('voyager.database.add_timestamps') }}</div>
+        <div class="btn btn-success" @click="addSoftDeletes">+ {{ __('voyager.database.add_softdeletes') }}</div>
     </div>
 @endsection
 
