4baa02f6d0186b85fd6ea6732f5b750ea8efee1f
Bruno Torrinha <bruno@torrinha.com>
2017-06-29 03:26:56 +0100

    Moved refactor into another PR

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -4,12 +4,16 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 @stop
 
-@section('page_title',
-    __('voyager.generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->display_name_singular)
+@if(isset($dataTypeContent->id))
+    @section('page_title', __('voyager.generic.edit').' '.$dataType->display_name_singular)
+@else
+    @section('page_title', __('voyager.generic.add').' '.$dataType->display_name_singular)
+@endif
 
 @section('page_header')
     <h1 class="page-title">
-        <i class="{{ $dataType->icon }}"></i> @if(isset($dataTypeContent->id)){{ __('voyager.generic.edit') }}@else{{ __('voyager.generic.new') }}@endif {{ $dataType->display_name_singular }}
+        <i class="{{ $dataType->icon }}"></i>
+        {{ __('voyager.generic.'.(isset($dataTypeContent->id) ? 'edit' : 'add')).' '.$dataType->display_name_singular }}
     </h1>
     @include('voyager::multilingual.language-selector')
 @stop
@@ -45,10 +49,12 @@
                                 </div>
                             @endif
 
-                            <!-- Adding / Editing -->
-                            @php
-                                $dataTypeRows = $dataType->{(isset($dataTypeContent->id) ? 'editRows' : 'addRows' )};
-                            @endphp
+                            <!-- If we are editing -->
+                            @if(isset($dataTypeContent->id))
+                                <?php $dataTypeRows = $dataType->editRows; ?>
+                            @else
+                                <?php $dataTypeRows = $dataType->addRows; ?>
+                            @endif
 
                             @foreach($dataTypeRows as $row)
 
