8b03dc5aea10f24b81dbc873fa4f9d86c5d57676
Eric Fletcher <fletch3555@users.noreply.github.com>
2017-07-14 20:12:23 -0400

    Fix #906
    Fixes the remaining outstanding issue in #906

--- publishable/database/seeds/MenuItemsTableSeeder.php
+++ publishable/database/seeds/MenuItemsTableSeeder.php
@@ -21,7 +21,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Dashboard',
-                'url'        => route('voyager.dashboard', [], false),
+                'route'      => 'voyager.dashboard',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -36,7 +36,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Media',
-                'url'        => route('voyager.media.index', [], false),
+                'route'      => 'voyager.media.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -51,7 +51,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Posts',
-                'url'        => route('voyager.posts.index', [], false),
+                'route'      => 'voyager.posts.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -66,7 +66,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Users',
-                'url'        => route('voyager.users.index', [], false),
+                'route'      => 'voyager.users.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -81,7 +81,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Categories',
-                'url'        => route('voyager.categories.index', [], false),
+                'route'      => 'voyager.categories.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -96,7 +96,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Pages',
-                'url'        => route('voyager.pages.index', [], false),
+                'route'      => 'voyager.pages.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -111,7 +111,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Roles',
-                'url'        => route('voyager.roles.index', [], false),
+                'route'      => 'voyager.roles.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -141,7 +141,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Menu Builder',
-                'url'        => route('voyager.menus.index', [], false),
+                'route'      => 'voyager.menus.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -156,7 +156,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Database',
-                'url'        => route('voyager.database.index', [], false),
+                'route'      => 'voyager.database.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
@@ -171,7 +171,7 @@ class MenuItemsTableSeeder extends Seeder
             $menuItem = MenuItem::firstOrNew([
                 'menu_id'    => $menu->id,
                 'title'      => 'Settings',
-                'url'        => route('voyager.settings.index', [], false),
+                'route'      => 'voyager.settings.index',
             ]);
             if (!$menuItem->exists) {
                 $menuItem->fill([
