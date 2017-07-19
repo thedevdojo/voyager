02e3b6c4fb8006f127610ed36ea11bda240b5ddd
Serge Ledig <contact@cotiga.fr>
2017-07-19 14:14:07 +0200

    Text formatting

--- publishable/lang/fr/voyager.php
+++ publishable/lang/fr/voyager.php
@@ -131,42 +131,42 @@ return [
     ],
 
     'media' => [
-        'add_new_folder'        => 'Ajouter un dossier',
-        'audio_support'         => 'Votre navigateur ne supporte pas l\'élément audio.',
-        'create_new_folder'     => 'Créer un nouveau dossier',
-        'delete_folder_question' => 'La suppression d\'un dossier supprime tous son contenu !',
-        'destination_folder'    => 'Dossier de destination',
-        'drag_drop_info'        => 'Glissez/déposez des fichiers ou cliquez ci-dessous pour télécharger',
-        'error_already_exists'  => 'Désolé, il existe déjà un fichier/dossier avec ce nom dans ce dossier.',
-        'error_creating_dir'    => 'Désolé, quelque chose n\'a pas fonctionné lors de la création du dossier, '.
-                                   'vérifiez les autorisations SVP',
-        'error_deleting_file'   => 'Désolé, quelque chose n\'a pas fonctionné lors de la suppression du fichier, '.
-                                   'vérifiez les autorisations SVP',
-        'error_deleting_folder' => 'Désolé, quelque chose n\'a pas fonctionné lors de la suppression du dossier, '.
-                                   'vérifiez les autorisations SVP',
-        'error_may_exist'       => 'Un fichier ou un dossier avec ce nom existe déjà. Choisissez un autre nom '.
-                                   'ou supprimez le fichier/dossier existant.',
-        'error_moving'          => 'Désolé, il y a un problème pour déplacer ce fichier/dossier, '.
-                                   'vérifiez les autorisations SVP',
-        'error_uploading'       => 'Échec du téléchargement : une erreur inconnue s\'est produite !',
-        'folder_exists_already' => 'Désolé, ce dossier existe déjà. Supprimez-le pour le récréer ou '.
-                                   'choisissez un autre nom',
-        'image_does_not_exist'  => 'L\'image n\'existe pas',
-        'image_removed'         => 'Image supprimée',
-        'library'               => 'Médiathèque',
-        'loading'               => 'CHARGEMENT DES FICHIERS MULTIMEDIA',
-        'move_file_folder'      => 'Déplacer fichier/dossier',
-        'new_file_folder'       => 'Nouveau nom de fichier/dossier',
-        'new_folder_name'       => 'Nouveau nom de dossier',
-        'no_files_here'         => 'Nouveau fichier ici.',
-        'no_files_in_folder'    => 'Il n\'y a pas de fichier dans ce dossier.',
-        'nothing_selected'      => 'Aucun fichier ou dossier sélectionné',
-        'rename_file_folder'    => 'renommer le fichier/dossier',
-        'success_uploaded_file' => 'Téléchargement du fichier réussi !',
-        'success_uploading'     => 'Image téléchargée avec succès !',
-        'uploading_wrong_type'  => 'Échec du téléchargement : format de fichier non pris en charge ou volume trop '.
-                                   'important !',
-        'video_support'         => 'Votre navigateur ne prend pas en charge la balise vidéo.',
+        'add_new_folder'         => 'Ajouter un dossier',
+        'audio_support'          => 'Votre navigateur ne supporte pas l\'élément audio.',
+        'create_new_folder'      => 'Créer un nouveau dossier',
+        'delete_folder_question' => 'La suppression d\'un dossier supprime tout son contenu !',
+        'destination_folder'     => 'Dossier de destination',
+        'drag_drop_info'         => 'Glissez/déposez des fichiers ou cliquez ci-dessous pour télécharger',
+        'error_already_exists'   => 'Désolé, il existe déjà un fichier/dossier avec ce nom dans ce dossier.',
+        'error_creating_dir'     => 'Désolé, quelque chose n\'a pas fonctionné lors de la création du dossier, '.
+                                    'vérifiez les autorisations SVP',
+        'error_deleting_file'    => 'Désolé, quelque chose n\'a pas fonctionné lors de la suppression du fichier, '.
+                                    'vérifiez les autorisations SVP',
+        'error_deleting_folder'  => 'Désolé, quelque chose n\'a pas fonctionné lors de la suppression du dossier, '.
+                                    'vérifiez les autorisations SVP',
+        'error_may_exist'        => 'Un fichier ou un dossier avec ce nom existe déjà. Choisissez un autre nom '.
+                                    'ou supprimez le fichier/dossier existant.',
+        'error_moving'           => 'Désolé, il y a un problème pour déplacer ce fichier/dossier, '.
+                                    'vérifiez les autorisations SVP',
+        'error_uploading'        => 'Échec du téléchargement : une erreur inconnue s\'est produite !',
+        'folder_exists_already'  => 'Désolé, ce dossier existe déjà. Supprimez-le pour le récréer ou '.
+                                    'choisissez un autre nom',
+        'image_does_not_exist'   => 'L\'image n\'existe pas',
+        'image_removed'          => 'Image supprimée',
+        'library'                => 'Médiathèque',
+        'loading'                => 'CHARGEMENT DES FICHIERS MULTIMEDIA',
+        'move_file_folder'       => 'Déplacer fichier/dossier',
+        'new_file_folder'        => 'Nouveau nom de fichier/dossier',
+        'new_folder_name'        => 'Nouveau nom de dossier',
+        'no_files_here'          => 'Nouveau fichier ici.',
+        'no_files_in_folder'     => 'Il n\'y a pas de fichier dans ce dossier.',
+        'nothing_selected'       => 'Aucun fichier ou dossier sélectionné',
+        'rename_file_folder'     => 'renommer le fichier/dossier',
+        'success_uploaded_file'  => 'Téléchargement du fichier réussi !',
+        'success_uploading'      => 'Image téléchargée avec succès !',
+        'uploading_wrong_type'   => 'Échec du téléchargement : format de fichier non pris en charge ou volume trop '.
+                                    'important !',
+        'video_support'          => 'Votre navigateur ne prend pas en charge la balise vidéo.',
     ],
 
     'menu_builder' => [
@@ -375,8 +375,8 @@ return [
                                     'l\'ajouter à vos paramètres clé : <code>google_analytics_client_id</code>. '.
                                     'Obtenez une clé dans l\'espace développeur Google :',
         'set_view'               => 'Sélectionner une vue',
-        'this_vs_last_week'      => 'Cette semaine vs la semaine dernière',
-        'this_vs_last_year'      => 'Cette année vs l\'année dernière',
+        'this_vs_last_week'      => 'Cette semaine contre la semaine dernière',
+        'this_vs_last_year'      => 'Cette année contre l\'année dernière',
         'top_browsers'           => 'Top navigateurs',
         'top_countries'          => 'Top pays',
         'various_visualizations' => 'Visualisations diverses',
