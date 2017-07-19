496396029e91a534256ed8298ffea330906fe3cc
Tony Lea <tnylea@gmail.com>
2017-07-19 02:21:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Http/Controllers/VoyagerCompassController.php
+++ src/Http/Controllers/VoyagerCompassController.php
@@ -2,12 +2,12 @@
 
 namespace TCG\Voyager\Http\Controllers;
 
-use TCG\Voyager\Facades\Voyager;
-use \Artisan;
-use Illuminate\Http\Request;
+use Artisan;
 use Exception;
-use Symfony\Component\Process\Process;
+use Illuminate\Http\Request;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use TCG\Voyager\Facades\Voyager;
 
 class VoyagerCompassController extends Controller
 {
@@ -23,7 +23,6 @@ class VoyagerCompassController extends Controller
         // Check permission
         //Voyager::canOrFail('browse_compass');
         //
-        
 
         $message = '';
         $active_tab = '';
@@ -63,14 +62,13 @@ class VoyagerCompassController extends Controller
 
         $artisan_output = '';
 
-        if($request->isMethod('post')){
+        if ($request->isMethod('post')) {
             $command = $request->command;
             $args = $request->args;
-            $args = (isset($args)) ? " " . $args : "";
-            
-            try{
+            $args = (isset($args)) ? ' '.$args : '';
 
-                $process = new Process('cd ' . base_path() . ' && php artisan ' . $command . $args);
+            try {
+                $process = new Process('cd '.base_path().' && php artisan '.$command.$args);
                 $process->run();
 
                 if (!$process->isSuccessful()) {
@@ -82,64 +80,63 @@ class VoyagerCompassController extends Controller
                 //$artisan_output = exec('cd ' . base_path() . ' && php artisan ' . $command . $args);
                 // Artisan::call($command . $args);
                 // $artisan_output = Artisan::output();
-            } catch(Exception $e){
+            } catch (Exception $e) {
                 $artisan_output = $e->getMessage();
             }
             $active_tab = 'commands';
         }
 
-
-    	$logs = LogViewer::all();
+        $logs = LogViewer::all();
         $files = LogViewer::getFiles(true);
         $current_file = LogViewer::getFileName();
-        
+
         // get the full list of artisan commands and store the output
         $commands = $this->getArtisanCommands();
 
         return view('voyager::compass.index', compact('logs', 'files', 'current_file', 'active_tab', 'commands', 'artisan_output'))->with($message);
     }
 
-    private function getArtisanCommands(){
+    private function getArtisanCommands()
+    {
         Artisan::call('list');
-        
+
         // Get the output from the previous command
         $artisan_output = Artisan::output();
         $artisan_output = $this->cleanArtisanOutput($artisan_output);
         $commands = $this->getCommandsFromOutput($artisan_output);
-        
 
         return $commands;
     }
 
-    private function cleanArtisanOutput($output){
+    private function cleanArtisanOutput($output)
+    {
 
         // Add each new line to an array item and strip out any empty items
         $output = array_filter(explode("\n", $output));
 
         // Get the current index of: "Available commands:"
-        $index = array_search("Available commands:", $output);
+        $index = array_search('Available commands:', $output);
 
         // Remove all commands that precede "Available commands:", and remove that
         // Element itself -1 for offset zero and -1 for the previous index (equals -2)
-        $output = array_slice($output, $index-2, count($output));
+        $output = array_slice($output, $index - 2, count($output));
 
         return $output;
     }
 
-    private function getCommandsFromOutput($output){
-
+    private function getCommandsFromOutput($output)
+    {
         $commands = [];
 
-        foreach($output as $output_line){
-            if(empty(trim(substr($output_line, 0, 2)))){
+        foreach ($output as $output_line) {
+            if (empty(trim(substr($output_line, 0, 2)))) {
                 $parts = preg_split('/  +/', trim($output_line));
-                $command = (object)['name' => trim(@$parts[0]), 'description' => trim(@$parts[1])];
+                $command = (object) ['name' => trim(@$parts[0]), 'description' => trim(@$parts[1])];
                 array_push($commands, $command);
             }
         }
 
         return $commands;
-
     }
 
     private function redirect($to)
