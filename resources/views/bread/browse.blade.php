7e0700a1e357dc9a26969d91eb1e9e69112b580a
AvengerWeb <i@avenger-web.ru>
2017-08-11 15:27:49 +0300

    Use getKeyName() instead of protected prop primaryKey

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -141,8 +141,8 @@
                                                 href="javascript:;"
                                                 title="{{ __('voyager.generic.delete') }}"
                                                 class="btn btn-sm btn-danger pull-right delete"
-                                                data-id="{{ $data->{$data->primaryKey} }}"
-                                                id="delete-{{ $data->{$data->primaryKey} }}"
+                                                data-id="{{ $data->{$data->getKeyName()} }}"
+                                                id="delete-{{ $data->{$data->getKeyName()} }}"
                                             >
                                                 <i class="voyager-trash"></i> <span class="hidden-xs hidden-sm">
                                                     {{ __('voyager.generic.delete') }}
@@ -150,12 +150,12 @@
                                             </a>
                                         @endif
                                         @if (Voyager::can('edit_'.$dataType->name))
-                                            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $data->{$data->primaryKey}) }}" title="{{ __('voyager.generic.edit') }}" class="btn btn-sm btn-primary pull-right edit">
+                                            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $data->{$data->getKeyName()}) }}" title="{{ __('voyager.generic.edit') }}" class="btn btn-sm btn-primary pull-right edit">
                                                 <i class="voyager-edit"></i> <span class="hidden-xs hidden-sm">{{ __('voyager.generic.edit') }}</span>
                                             </a>
                                         @endif
                                         @if (Voyager::can('read_'.$dataType->name))
-                                            <a href="{{ route('voyager.'.$dataType->slug.'.show', $data->{$data->primaryKey}) }}" title="{{ __('voyager.generic.view') }}" class="btn btn-sm btn-warning pull-right">
+                                            <a href="{{ route('voyager.'.$dataType->slug.'.show', $data->{$data->getKeyName()}) }}" title="{{ __('voyager.generic.view') }}" class="btn btn-sm btn-warning pull-right">
                                                 <i class="voyager-eye"></i> <span class="hidden-xs hidden-sm">{{ __('voyager.generic.view') }}</span>
                                             </a>
                                         @endif
--- resources/views/posts/browse.blade.php
+++ resources/views/posts/browse.blade.php
@@ -69,17 +69,17 @@
                                     @endforeach
                                     <td class="no-sort no-click">
                                         @if (Voyager::can('delete_'.$dataType->name))
-                                            <div class="btn-sm btn-danger pull-right delete" data-id="{{ $data->{$data->primaryKey} }}">
+                                            <div class="btn-sm btn-danger pull-right delete" data-id="{{ $data->{$data->getKeyName()} }}">
                                                 <i class="voyager-trash"></i> {{ __('voyager.generic.delete') }}
                                             </div>
                                         @endif
                                         @if (Voyager::can('edit_'.$dataType->name))
-                                            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $data->{$data->primaryKey}) }}" class="btn-sm btn-primary pull-right edit">
+                                            <a href="{{ route('voyager.'.$dataType->slug.'.edit', $data->{$data->getKeyName()}) }}" class="btn-sm btn-primary pull-right edit">
                                                 <i class="voyager-edit"></i> {{ __('voyager.generic.edit') }}
                                             </a>
                                         @endif
                                         @if (Voyager::can('read_'.$dataType->name))
-                                            <a href="{{ route('voyager.'.$dataType->slug.'.show', $data->{$data->primaryKey}) }}" class="btn-sm btn-warning pull-right">
+                                            <a href="{{ route('voyager.'.$dataType->slug.'.show', $data->{$data->getKeyName()}) }}" class="btn-sm btn-warning pull-right">
                                                 <i class="voyager-eye"></i> {{ __('voyager.generic.view') }}
                                             </a>
                                         @endif
