1bd7ef6c3f91b3c467cd29f18aac7143dfd41e1a
aleksey.kostenko.dev <aleksey.kostenko.dev@gmail.com>
2017-08-21 23:57:52 +0300

    fix image

--- resources/views/formfields/image.blade.php
+++ resources/views/formfields/image.blade.php
@@ -4,4 +4,6 @@
     <img src="{{ Voyager::image($dataTypeContent->{$row->field}) }}"
          style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;">
     <input @if($row->required == 1) required @endif type="file" name="{{ $row->field }}">
+@else
+    <input @if($row->required == 1) required @endif type="file" name="{{ $row->field }}">
 @endif
