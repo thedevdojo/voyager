35483b1c4de0fe710e296c0328dd8ce8748c4961
aleksey.kostenko.dev <aleksey.kostenko.dev@gmail.com>
2017-08-26 15:22:21 +0300

    fix multiple crop images

--- resources/views/formfields/crop.blade.php
+++ resources/views/formfields/crop.blade.php
@@ -21,13 +21,13 @@
 </button>
 
 @if($image)
-    <a type="button" class="btn btn-primary" id="download-{{ $id }}" href="{{ $image }}" download="{{ $image }}" target="_blank">
-        <i class="voyager-download"></i> {{ __('voyager.generic.download') }}
-    </a>
-
-    <button type="button" class="btn btn-warning" id="edit-{{ $id }}" >
+    <button type="button" class="btn btn-primary" id="edit-{{ $id }}" >
         <i class="voyager-edit"></i> {{ __('voyager.generic.edit') }}
     </button>
+
+    <a type="button" class="btn btn-warning" id="download-{{ $id }}" href="{{ $image }}" download="{{ $image }}" target="_blank">
+        <i class="voyager-download"></i> {{ __('voyager.generic.download') }}
+    </a>
 @endif
 <div id="uploadPreview" style="display:none;"></div>
 
@@ -68,53 +68,53 @@
                 {{ $photoParams->name }}.loadImg("{{ $dataTypeContent->getCroppedPhoto($photoParams->name, $photoParams->size->name) }}?{{ time() }}");
             @endif
         @endforeach
-    });
 
-    CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+        CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 
-    $("#upload-{{ $id }}, #dropzone-{{ $id }}").dropzone({
-        url: "{{ route('voyager.media.upload') }}",
-        previewsContainer: "#uploadPreview",
+        $("#upload-{{ $id }}, #dropzone-{{ $id }}").dropzone({
+            url: "{{ route('voyager.media.upload') }}",
+            previewsContainer: "#uploadPreview",
 
-        sending: function(file, xhr, formData) {
-            formData.append("_token", CSRF_TOKEN);
-            formData.append("upload_path", "{{ $dataType->slug.'/'.date('F').date('Y') }}");
-        },
-        success: function(e, res){
-            if (res.success){
-                uploadImage(res.path)
-                toastr.success(res.message, "Sweet Success!");
-            } else {
-                toastr.error(res.message, "Whoopsie!");
+            sending: function(file, xhr, formData) {
+                formData.append("_token", CSRF_TOKEN);
+                formData.append("upload_path", "{{ $dataType->slug.'/'.date('F').date('Y') }}");
+            },
+            success: function(e, res){
+                if (res.success){
+                    uploadImage(res.path)
+                    toastr.success(res.message, "Sweet Success!");
+                } else {
+                    toastr.error(res.message, "Whoopsie!");
+                }
+            },
+            error: function(e, res, xhr){
+                toastr.error(res, "Whoopsie");
             }
-        },
-        error: function(e, res, xhr){
-            toastr.error(res, "Whoopsie");
+        });
+
+        $('#edit-{{ $id }}').click(function(){
+            uploadImage("{{ $dataTypeContent->{$row->field} }}")
+        });
+
+        function uploadImage(imagePath){
+            $("#dropzone-{{ $id }}").find(".crop-container").remove();
+            $("#dropzone-{{ $id }}").find(".noUi-base").remove();
+
+            $('#{{ $id }}').val(imagePath);
+            $('#dropzone-{{ $id }}').removeClass('disabled');
+
+            @foreach($options->crop as $photoParams)
+                var {{ $photoParams->name }} = new Image.crop();
+                {{ $photoParams->name }}.init(".{{ $photoParams->name }}");
+                {{ $photoParams->name }}.loadImg("/{{ basename(storage_url('')) }}/" + imagePath);
+
+                $("button:submit").click(function() {
+                    $('input[name={{ $row->field.'_'.$photoParams->name }}]').val(
+                        JSON.stringify(coordinates({{ $photoParams->name }}))
+                    );
+                })
+            @endforeach
         }
     });
-
-    $('#edit-{{ $id }}').click(function(){
-        uploadImage("{{ $dataTypeContent->{$row->field} }}")
-    });
-
-    function uploadImage(imagePath){
-        $("#dropzone-{{ $id }}").find(".crop-container").remove();
-        $("#dropzone-{{ $id }}").find(".noUi-base").remove();
-
-        $('#{{ $id }}').val(imagePath);
-        $('.dropzone-block').removeClass('disabled');
-
-        @foreach($options->crop as $photoParams)
-            var {{ $photoParams->name }} = new Image.crop();
-            {{ $photoParams->name }}.init(".{{ $photoParams->name }}");
-            {{ $photoParams->name }}.loadImg("/{{ basename(storage_url('')) }}/" + imagePath);
-
-            $("button:submit").click(function() {
-                $('input[name={{ $row->field.'_'.$photoParams->name }}]').val(
-                    JSON.stringify(coordinates({{ $photoParams->name }}))
-                );
-            })
-        @endforeach
-    }
     </script>
 @endsection
