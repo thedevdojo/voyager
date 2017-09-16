04bfc07edb1b7d8f1037485f0f5ac77eecd01f55
Scorpion <davo_bidzian@mail.ru>
2017-09-16 18:48:26 +0300

    Apply fixes from StyleCI

--- src/Http/Middleware/VoyagerRouteBlocker.php
+++ src/Http/Middleware/VoyagerRouteBlocker.php
@@ -3,29 +3,27 @@
 namespace TCG\Voyager\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
 use TCG\Voyager\Facades\Voyager;
 
 class VoyagerRouteBlocker
 {
-
     public function handle($request, Closure $next)
     {
-		$enable = config('voyager.blocker.enable');
-		
-		if ($enable) {
-			$allow = config('voyager.blocker.ip_white_list');
-			$ip    = $request->getClientIp();
+        $enable = config('voyager.blocker.enable');
 
-			$allowed = explode(',', $allow);
+        if ($enable) {
+            $allow = config('voyager.blocker.ip_white_list');
+            $ip    = $request->getClientIp();
 
-			if (in_array($ip, $allowed)) {
-				return $next($request);
-			}
+            $allowed = explode(',', $allow);
 
-			return abort(404);
-		}
-		
+            if (in_array($ip, $allowed)) {
+                return $next($request);
+            }
+
+            return abort(404);
+        }
+
         return $next($request);
     }
 }
