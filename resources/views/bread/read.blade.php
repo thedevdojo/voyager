0995b5d093cbb09eaaf6fa778c63c9df9f2c04d1
Daniel Rodriguez <daniel@darit.mx>
2017-10-14 00:12:50 -0500

    fix hardcoded page title to lang translation

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -1,6 +1,6 @@
 @extends('voyager::master')
 
-@section('page_title','View '.$dataType->display_name_singular)
+@section('page_title', __('voyager.generic.view').' '.$dataType->display_name_singular)
 
 @section('page_header')
     <h1 class="page-title">
