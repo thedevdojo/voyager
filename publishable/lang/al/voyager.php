76984a27f535c17db6b2dccc119d65a84f1987de
Sidrit Trandafili <forge405@gmail.com>
2017-10-24 18:35:50 +0200

    wip

--- publishable/lang/al/voyager.php
+++ publishable/lang/al/voyager.php
@@ -335,7 +335,7 @@ return [
         'post_link_text' => 'Shiko të gjitha postimet',
         'post_text'      => 'Ju keni: count: string në databazën tuaj. Klikoni butonin '
         .'më poshtë për të parë të gjitha postimet. ',
-        'user'           => 'Përdorues', 'Përdorues'
+        'user'           => 'Përdorues',
         'user_link_text' => 'Shikoni të gjithë përdoruesit',
         'user_text'      => 'Ju keni: count: string në databazën tuaj. Klikoni butonin'
         .' më poshtë për të parë të gjithë përdoruesit. ',
