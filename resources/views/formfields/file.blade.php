eb6cc1b20568115526a54a1654c85c46181d9372
Sergii <khoroshko@gmail.com>
2017-10-28 00:34:07 +0300

    Issues 2090, files/images upload fix

--- resources/views/formfields/file.blade.php
+++ resources/views/formfields/file.blade.php
@@ -1,7 +1,7 @@
 @if(isset($dataTypeContent->{$row->field}))
     @if(json_decode($dataTypeContent->{$row->field}))
         @foreach(json_decode($dataTypeContent->{$row->field}) as $file)
-            <br/><a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) or '' }}"> {{ Storage::disk(config('voyager.storage.disk'))->url($file->original_name) or '' }} </a>
+            <br/><a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"> {{ $file->original_name ?: '' }} </a>
         @endforeach
     @else
         <a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($dataTypeContent->{$row->field}) }}"> Download </a>
