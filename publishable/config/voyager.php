d525a4da90ace12554ee3260e53995178bf99100
Mark Topper <mark@ulties.com>
2017-10-31 02:52:49 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- publishable/config/voyager.php
+++ publishable/config/voyager.php
@@ -138,14 +138,14 @@ return [
         // Add custom list items to navbar's dropdown
         'navbar_items' => [
             'Profile' => [
-                'route'         => 'voyager.profile',
-                'classes'       => 'class-full-of-rum',
-                'icon_class'    => 'voyager-person',
+                'route'      => 'voyager.profile',
+                'classes'    => 'class-full-of-rum',
+                'icon_class' => 'voyager-person',
             ],
             'Home' => [
-                'route'         => '/',
-                'icon_class'    => 'voyager-home',
-                'target_blank'  => true,
+                'route'        => '/',
+                'icon_class'   => 'voyager-home',
+                'target_blank' => true,
             ],
             'Logout' => [
                 'route'      => 'voyager.logout',
