80a4c75a466ce69dbb6b381187436166472abfbd
Viktor Maár <viktor.maarsson@gmail.com>
2017-11-02 10:25:09 +0100

    Fix button colors

--- resources/views/multilingual/language-selector.blade.php
+++ resources/views/multilingual/language-selector.blade.php
@@ -2,7 +2,7 @@
     <div class="language-selector">
         <div class="btn-group btn-group-sm" role="group" data-toggle="buttons">
             @foreach(config('voyager.multilingual.locales') as $lang)
-                <label class="btn btn-primary{{ ($lang === config('voyager.multilingual.default')) ? " active" : "" }}">
+                <label class="language-selector-button btn btn-default {{ ($lang === config('voyager.multilingual.default')) ? " active" : "" }}">
                     <input type="radio" name="i18n_selector" id="{{$lang}}" autocomplete="off"{{ ($lang === config('voyager.multilingual.default')) ? ' checked="checked"' : '' }}> {{ strtoupper($lang) }}
                 </label>
             @endforeach
