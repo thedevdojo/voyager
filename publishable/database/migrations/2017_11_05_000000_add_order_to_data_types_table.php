ec1cf8c9260cce187c1cc933c0d78888243b5707
Christoph <yipsocom@googlemail.com>
2017-11-05 18:18:57 +0100

    Order Bread Items

---
+++ publishable/database/migrations/2017_11_05_000000_add_order_to_data_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOrderToDatatypesTable extends Migration
+{
+    /**
+    * Run the migrations.
+    *
+    * @return void
+    */
+    public function up()
+    {
+        Schema::table('data_types', function (Blueprint $table) {
+            $table->string('order_ident_column')->after('server_side');
+            $table->string('order_column')->after('server_side');
+        });
+    }
+
+    /**
+    * Reverse the migrations.
+    *
+    * @return void
+    */
+    public function down()
+    {
+        Schema::table('data_types', function (Blueprint $table) {
+            $table->dropColumn('order_ident_column');
+            $table->dropColumn('order_column');
+        });
+    }
+}
