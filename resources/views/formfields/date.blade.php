ef4d88f51425aa30178b1e41d6f6a48e89814606
Pablo Quiroga <dev.pabloquiroga@gmail.com>
2017-11-09 15:09:18 -0300

    Revert #2077 formfields changes

--- resources/views/formfields/date.blade.php
+++ resources/views/formfields/date.blade.php
@@ -1,3 +1,3 @@
-<input type="date" class="form-control"  data-name="{{ $row->display_name }}"  name="{{ $row->field }}"
+<input type="date" class="form-control" name="{{ $row->field }}"
        placeholder="{{ $row->display_name }}"
        value="@if(isset($dataTypeContent->{$row->field})){{ gmdate('Y-m-d', strtotime(old($row->field, $dataTypeContent->{$row->field}))) }}@else{{old($row->field)}}@endif">
