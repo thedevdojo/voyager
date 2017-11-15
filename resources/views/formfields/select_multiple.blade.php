c354817e644361e074e7bbedaad46b0c55c2eda9
Mohamed Abo Bdwey <m.bdwey@gmail.com>
2017-11-15 16:31:23 +0200

    select_multiple need array on browse but this template try to decode is a like if it's json so this change unify browse and edit,create

--- resources/views/formfields/select_multiple.blade.php
+++ resources/views/formfields/select_multiple.blade.php
@@ -3,7 +3,7 @@
     <p class="label label-warning"><i class="voyager-warning"></i> {{ __('voyager.form.field_select_dd_relationship', ['method' => camel_case($row->field).'()', 'class' => $dataType->model_name]) }}</p>
 @endif
 @php
-$dataTypeContent->{$row->field} = json_decode($dataTypeContent->{$row->field})
+// $dataTypeContent->{$row->field} = json_decode($dataTypeContent->{$row->field})
 @endphp
 <select class="form-control select2"  data-name="{{ $row->display_name }}" name="{{ $row->field }}[]" multiple>
     @if(isset($options->relationship))
