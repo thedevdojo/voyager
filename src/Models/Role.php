4df250083df3cd941d5d6c0985ea6e9d519580f4
Bruno Torrinha <bruno@torrinha.com>
2017-11-30 06:39:18 +0000

    Revise Role->users()

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -9,10 +9,19 @@ class Role extends Model
 {
     protected $guarded = [];
 
+    public function usersDefault()
+    {
+        return $this->hasMany(Voyager::modelClass('User'))->select('*');
+    }
+
+    public function usersAlternative()
+    {
+        return $this->belongsToMany(Voyager::modelClass('User'), 'user_roles')->select('users.*');
+    }
+
     public function users()
     {
-        return $this->hasMany(Voyager::modelClass('User'))->get()
-                    ->merge($this->belongsToMany(Voyager::modelClass('User'), 'user_roles')->get());
+        return $this->usersDefault()->union($this->usersAlternative()->toBase());
     }
 
     public function permissions()
