9f0db3a7ef9ffa247bf8db1a08699a09fd85b403
Unissn7 <pc@edu.ax>
2017-12-23 16:14:28 +0800

    Update voyager.php

--- publishable/lang/zh_CN/voyager.php
+++ publishable/lang/zh_CN/voyager.php
@@ -83,7 +83,7 @@ return [
         'timestamp'              => '时间戳',
         'title'                  => '标题',
         'type'                   => '类型',
-        'unsigned'               => 'Unsigned',
+        'unsigned'               => '非负',
         'unstick_sidebar'        => '取消固定侧边栏',
         'update'                 => '更新',
         'update_failed'          => '更新失败',
@@ -344,7 +344,7 @@ return [
     ],
     // DataTable translations from: https://github.com/DataTables/Plugins/tree/master/i18n
 'datatable' => [
-        'sEmptyTable'     => '处理中...',
+        'sEmptyTable'     => '暂时没有内容哦',
         'sInfo'           => '显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项',
         'sInfoEmpty'      => '显示第 0 至 0 项结果，共 0 项',
         'sInfoFiltered'   => '(由 _MAX_ 项结果过滤)',
