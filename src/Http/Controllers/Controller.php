5357bd83a41c9d73bd96633f20b2315cb8848994
mrkindy <i.abotaleb@ed.com.eg>
2018-01-01 01:20:35 +0200

    Validation problem #2205

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -127,9 +127,9 @@ abstract class Controller extends BaseController
             if (isset($options->validation)) {
                 if (isset($options->validation->rule)) {
                     if (!is_array($options->validation->rule)) {
-                        $rules[$row->display_name] = explode('|', $options->validation->rule);
+                        $rules[$row->field] = explode('|', $options->validation->rule);
                     } else {
-                        $rules[$row->display_name] = $options->validation->rule;
+                        $rules[$row->field] = $options->validation->rule;
                     }
                 }
 
