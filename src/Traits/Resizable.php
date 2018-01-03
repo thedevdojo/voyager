674dd8754798b8dae0c3e7236622670250dde2c2
Handi Wijoyo <handiwijoyo@gmail.com>
2018-01-03 17:57:30 +0700

    Add optional attribute parameter

--- src/Traits/Resizable.php
+++ src/Traits/Resizable.php
@@ -5,16 +5,28 @@ namespace TCG\Voyager\Traits;
 trait Resizable
 {
     /**
-     *   Method for returning specific thumbnail for model.
+     * Method for returning specific thumbnail for model.
+     *
+     * @param string $type
+     * @param string $attribute
+     * @return string
      */
-    public function thumbnail($type)
+    public function thumbnail($type, $attribute = 'image')
     {
+        // Return empty string if the field not found
+        if(!isset($this->attributes[$attribute])) {
+            return '';
+        }
+
         // We take image from posts field
-        $image = $this->attributes['image'];
+        $image = $this->attributes[$attribute];
+
         // We need to get extension type ( .jpeg , .png ...)
         $ext = pathinfo($image, PATHINFO_EXTENSION);
+
         // We remove extension from file name so we can append thumbnail type
         $name = rtrim($image, '.'.$ext);
+
         // We merge original name + type + extension
         return $name.'-'.$type.'.'.$ext;
     }
