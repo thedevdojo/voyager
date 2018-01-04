5ccdaa0e13d5d245f8102dcfb2e0a0bb52f2ee4c
Eric Fletcher <efletcher@paychex.com>
2018-01-04 15:30:28 -0500

    Moved getRelationship() from just Role model to trait implemented in all Voyager models

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -4,11 +4,13 @@ namespace TCG\Voyager\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Traits\HasRelationships;
 
 class Role extends Model
 {
+    use HasRelationships;
+
     protected $guarded = [];
-    protected static $relationships = [];
 
     public function users()
     {
@@ -18,14 +20,5 @@ class Role extends Model
     public function permissions()
     {
         return $this->belongsToMany(Voyager::modelClass('Permission'));
-    }
-
-    public static function getRelationship($id)
-    {
-        if (!isset(self::$relationships[$id])) {
-            self::$relationships[$id] = self::find($id);
-        }
-
-        return self::$relationships[$id];
     }
 }
