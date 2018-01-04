f25caa6573ac15bf7e66439f033e4814bf5ca1c4
JapSeyz <JapSeyz@JapSeyz.com>
2018-01-04 01:59:43 +0100

    Caching setting values

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,22 +1,32 @@
 <?php
 
-if (!function_exists('setting')) {
+declare(strict_types=1);
+
+if (! \function_exists('setting')) {
     function setting($key, $default = null)
     {
-        return TCG\Voyager\Facades\Voyager::setting($key, $default);
+        $setting = Cache::rememberForever('settings.' . $key, function () {
+            return TCG\Voyager\Facades\Voyager::setting($key, null);
+        });
+
+        if ($setting === null) {
+            return $default;
+        }
+
+        return $setting;
     }
 }
 
-if (!function_exists('menu')) {
+if (! \function_exists('menu')) {
     function menu($menuName, $type = null, array $options = [])
     {
         return TCG\Voyager\Facades\Voyager::model('Menu')->display($menuName, $type, $options);
     }
 }
 
-if (!function_exists('voyager_asset')) {
+if (! \function_exists('voyager_asset')) {
     function voyager_asset($path, $secure = null)
     {
-        return asset(config('voyager.assets_path').'/'.$path, $secure);
+        return asset(config('voyager.assets_path') . '/' . $path, $secure);
     }
 }
--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace TCG\Voyager\Models;
 
+use Cache;
 use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
@@ -11,4 +14,17 @@ class Setting extends Model
     protected $guarded = [];
 
     public $timestamps = false;
+
+    /**
+     * Clear cache on value change
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setValueAttribute($value)
+    {
+        Cache::forget('settings.' . $this->key);
+
+        $this->attributes['value'] = $value;
+    }
 }
