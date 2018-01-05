bd0daa657337b33113fd745371871cf26646b1bf
Sergio Peris <dev.sertxu@gmail.com>
2018-01-05 21:12:59 +0100

    Update image.blade.php

--- resources/views/formfields/image.blade.php
+++ resources/views/formfields/image.blade.php
@@ -1,5 +1,8 @@
+<br>
 @if(isset($dataTypeContent->{$row->field}))
-    <img src="@if( !filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $dataTypeContent->{$row->field} ) }}@else{{ $dataTypeContent->{$row->field} }}@endif"
-         style="width:200px; height:auto; clear:both; display:block; padding:2px; border:1px solid #ddd; margin-bottom:10px;">
+    <div class="img_settings_container" data-field-name="{{ $row->field }}">
+        <img src="{{ Voyager::image($dataTypeContent->{$row->field}) }}" data-image="{{ $dataTypeContent->{$row->field} }}" data-id="{{ $dataTypeContent->id }}">
+        <a href="#" class="voyager-x remove-multi-image"></a>
+    </div>
 @endif
 <input @if($row->required == 1 && !isset($dataTypeContent->{$row->field})) required @endif type="file" data-name="{{ $row->display_name }}"  name="{{ $row->field }}">
