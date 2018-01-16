69787b68f18591965d59a9a99446933ebca5c7d6
Mark Topper <mark@ulties.com>
2018-01-16 14:26:37 +0100

    Remove hook specific things

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -65,9 +65,6 @@ class InstallCommand extends Command
      */
     public function handle(Filesystem $filesystem)
     {
-        $this->info('Setting up the hooks');
-        $this->call('hook:setup');
-
         $this->info('Publishing the Voyager assets, database, language, and config files');
         $this->call('vendor:publish', ['--provider' => VoyagerServiceProvider::class]);
         $this->call('vendor:publish', ['--provider' => ImageServiceProviderLaravel5::class]);
@@ -115,6 +112,9 @@ class InstallCommand extends Command
         if ($this->option('with-dummy')) {
             $this->seed('VoyagerDummyDatabaseSeeder');
         }
+
+        $this->info('Setting up the hooks');
+        $this->call('hook:setup');
 
         $this->info('Adding the storage symlink to your public folder');
         $this->call('storage:link');
