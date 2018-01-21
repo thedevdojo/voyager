a63340bf7bad35f664f088a43d45ebbd32c1e454
AsemAlalami <asem.almi@gmail.com>
2018-01-21 16:41:21 +0200

    use Illuminate\Support\Facades\Auth in VoyagerRoleController

--- src/Http/Controllers/VoyagerRoleController.php
+++ src/Http/Controllers/VoyagerRoleController.php
@@ -3,6 +3,7 @@
 namespace TCG\Voyager\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use TCG\Voyager\Facades\Voyager;
 
 class VoyagerRoleController extends VoyagerBreadController
