778b64ff3b032ce1be40a9710f9ea4891acb1139
vblinden <me@vblinden.com>
2018-02-13 15:58:09 +0100

    Add ability to add new actions from other projects

--- src/Voyager.php
+++ src/Voyager.php
@@ -23,6 +23,9 @@ use TCG\Voyager\Models\Role;
 use TCG\Voyager\Models\Setting;
 use TCG\Voyager\Models\User;
 use TCG\Voyager\Traits\Translatable;
+use TCG\Voyager\Actions\ViewAction;
+use TCG\Voyager\Actions\EditAction;
+use TCG\Voyager\Actions\DeleteAction;
 
 class Voyager
 {
@@ -41,6 +44,12 @@ class Voyager
     protected $users = [];
 
     protected $viewLoadingEvents = [];
+
+    protected $actions = [
+        DeleteAction::class,
+        EditAction::class,
+        ViewAction::class,
+    ];
 
     protected $models = [
         'Category'   => Category::class,
@@ -137,6 +146,11 @@ class Voyager
         return $this;
     }
 
+    public function addAction($action)
+    {
+        array_push($this->actions, $action);
+    }
+
     public function addAfterFormField($handler)
     {
         if (!$handler instanceof AfterHandlerInterface) {
@@ -146,6 +160,11 @@ class Voyager
         $this->afterFormFields[$handler->getCodename()] = $handler;
 
         return $this;
+    }
+
+    public function actions()
+    {
+        return $this->actions;
     }
 
     public function formFields()
