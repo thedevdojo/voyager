1e019b034c09bf54b99774da04862ec6f2186e24
vblinden <me@vblinden.com>
2018-02-13 15:57:15 +0100

    Created AbstractAction

---
+++ src/Actions/AbstractAction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace TCG\Voyager\Actions;
+
+abstract class AbstractAction implements ActionInterface
+{
+    protected $dataType;
+    protected $data;
+
+    public function __construct($dataType, $data)
+    {
+        $this->dataType = $dataType;
+        $this->data = $data;
+    }
+
+    public function getDataType()
+    {
+        return null;
+    }
+
+    public function getAttributes()
+    {
+        return [];
+    }
+
+    public function convertAttributesToHtml()
+    {
+        $result = '';
+
+        foreach ($this->getAttributes() as $key => $attribute) {
+            $result .= $key . '="' . $attribute . '"';
+        }
+
+        return $result;
+    }
+
+    public function shouldActionDisplayOnDataType()
+    {
+        return ($this->dataType->name === $this->getDataType() || $this->getDataType() === null);
+    }
+}
