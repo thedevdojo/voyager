eecc95d451c7aec9419f6293ff912175d008b02c
vblinden <me@vblinden.com>
2018-02-14 15:43:17 +0100

    Pass the DataType name

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -178,7 +178,7 @@
                                             @foreach(Voyager::actions() as $action)
                                                 @php $action = new $action($dataType, $data); @endphp                                          
                                                 @if ($action->shouldActionDisplayOnDataType())
-                                                <a href="{{ $action->getRoute() }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
+                                                <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
                                                     <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
                                                 </a>
                                                 @endif
