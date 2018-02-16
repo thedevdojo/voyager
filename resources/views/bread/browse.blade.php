3f3d6a588473ebc9c2dd7aead0aca576c17eb500
vblinden <me@vblinden.com>
2018-02-16 22:48:32 +0100

    Seperate the logic in a different view

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -176,16 +176,8 @@
                                         @endforeach
                                         <td class="no-sort no-click" id="bread-actions">
                                             @foreach(Voyager::actions() as $action)
-                                                @php $action = new $action($dataType, $data); @endphp
-
-                                                @if ($action->shouldActionDisplayOnDataType())
-                                                    @can($action->getPolicy(), $data)
-                                                        <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
-                                                            <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
-                                                        </a>
-                                                    @endcan
-                                                @endif
-                                            @endforeach                                          
+                                                @include('voyager::bread.partials.actions', ['action' => $action]);
+                                            @endforeach
                                         </td>
                                     </tr>
                                     @endforeach
