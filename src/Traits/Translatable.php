b59c3dd96d2d6b85828ad35f72f668a7d9e133e7
Eric Fletcher <fletch3555@users.noreply.github.com>
2018-02-23 11:03:47 -0500

    Fix Fallback query
    Fixes #2716

--- src/Traits/Translatable.php
+++ src/Traits/Translatable.php
@@ -54,11 +54,14 @@ trait Translatable
         }
 
         $query->with(['translations' => function (Relation $query) use ($locale, $fallback) {
-            $query->where('locale', $locale);
+            
+            $query->where(function($q) use ($locale, $fallback) {
+                $q->where('locale', $locale);
 
-            if ($fallback !== false) {
-                $query->orWhere('locale', $fallback);
-            }
+                if ($fallback !== false) {
+                    $q->orWhere('locale', $fallback);
+                }
+            });
         }]);
     }
 
@@ -85,14 +88,16 @@ trait Translatable
                 return;
             }
 
-            if (is_array($locales)) {
-                $query->whereIn('locale', $locales);
-            } else {
-                $query->where('locale', $locales);
-            }
+            $query->where(function($q) use ($locales, $fallback) {
+                if (is_array($locales)) {
+                    $q->whereIn('locale', $locales);
+                } else {
+                    $q->where('locale', $locales);
+                }
 
-            if ($fallback !== false) {
-                $query->orWhere('locale', $fallback);
+                if ($fallback !== false) {
+                    $q->orWhere('locale', $fallback);
+                }
             }
         }]);
     }
