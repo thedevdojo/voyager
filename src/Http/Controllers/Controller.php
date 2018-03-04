thedevdojo/voyager

commit 6e9df821bbcef7a80ccf20856f26474cddf3feca

fix relationship bug (#2121)
* fix relationship bug 

the relationship row field should be changed after the content is delivered

* fix style error

* fix style again

src/Http/Controllers/Controller.php
@@ -48,10 +48,6 @@ abstract class Controller extends BaseController
         foreach ($rows as $row) {
             $options = json_decode($row->details);
 
-            if ($row->type == 'relationship' && $options->type != 'belongsToMany') {
-                $row->field = @$options->column;
-            }
-
             // if the field for this row is absent from the request, continue
             // checkboxes will be absent when unchecked, thus they are the exception
             if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
@@ -59,6 +55,10 @@ abstract class Controller extends BaseController
             }
 
             $content = $this->getContentBasedOnType($request, $slug, $row);
+
+            if ($row->type == 'relationship' && $options->type != 'belongsToMany') {
+                $row->field = @$options->column;
+            }
 
             /*
              * merge ex_images and upload images

commit 741b5e0c8d58ca03fdc51f8acd3f5e0383ebd9d6

Consider relationship field when creating record
Fix bug introduced in #2121 causing the relationship field to be
skipped in test at line 53

Also fixes #2141 and possibily also #2144

src/Http/Controllers/Controller.php
@@ -48,17 +48,21 @@ abstract class Controller extends BaseController
         foreach ($rows as $row) {
             $options = json_decode($row->details);
 
+            if($row->type == 'relationship') {
+                $reqField = $options->column;
+                if ($options->type != 'belongsToMany')
+                    $row->field = @$options->column;
+            } else {
+                $reqField = $row->field;
+            }
+
             // if the field for this row is absent from the request, continue
             // checkboxes will be absent when unchecked, thus they are the exception
-            if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
+            if (!$request->hasFile($reqField) && !$request->has($reqField) && $row->type !== 'checkbox') {
                 continue;
             }
 
             $content = $this->getContentBasedOnType($request, $slug, $row);
-
-            if ($row->type == 'relationship' && $options->type != 'belongsToMany') {
-                $row->field = @$options->column;
-            }
 
             /*
              * merge ex_images and upload images