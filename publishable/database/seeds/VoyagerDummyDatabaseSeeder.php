6463d41c745b5e7554b2e28c2fac54181269d741
Christoph <yipsocom@googlemail.com>
2018-03-16 00:50:17 +0100

    Resync Permission-Roles

--- publishable/database/seeds/VoyagerDummyDatabaseSeeder.php
+++ publishable/database/seeds/VoyagerDummyDatabaseSeeder.php
@@ -22,5 +22,6 @@ class VoyagerDummyDatabaseSeeder extends Seeder
         $this->seed('PostsTableSeeder');
         $this->seed('PagesTableSeeder');
         $this->seed('TranslationsTableSeeder');
+        $this->seed('PermissionRoleTableSeeder');
     }
 }
