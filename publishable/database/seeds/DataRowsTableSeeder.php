6bfd956b68e21d422e709b8c8862e230446edaea
Christoph <yipsocom@googlemail.com>
2018-03-16 20:59:39 +0100

    Display Role-Display-Name instead of Name
    Fixes #1742

--- publishable/database/seeds/DataRowsTableSeeder.php
+++ publishable/database/seeds/DataRowsTableSeeder.php
@@ -603,7 +603,7 @@ class DataRowsTableSeeder extends Seeder
                 'edit'         => 1,
                 'add'          => 1,
                 'delete'       => 0,
-                'details'      => '{"model":"TCG\\\Voyager\\\Models\\\Role","table":"roles","type":"belongsTo","column":"role_id","key":"id","label":"name","pivot_table":"roles","pivot":"0"}',
+                'details'      => '{"model":"TCG\\\Voyager\\\Models\\\Role","table":"roles","type":"belongsTo","column":"role_id","key":"id","label":"display_name","pivot_table":"roles","pivot":"0"}',
                 'order'        => 10,
             ])->save();
         }
@@ -619,7 +619,7 @@ class DataRowsTableSeeder extends Seeder
                 'edit'         => 1,
                 'add'          => 1,
                 'delete'       => 0,
-                'details'      => '{"model":"TCG\\\Voyager\\\Models\\\Role","table":"roles","type":"belongsToMany","column":"id","key":"id","label":"name","pivot_table":"user_roles","pivot":"1"}',
+                'details'      => '{"model":"TCG\\\Voyager\\\Models\\\Role","table":"roles","type":"belongsToMany","column":"id","key":"id","label":"display_name","pivot_table":"user_roles","pivot":"1"}',
                 'order'        => 11,
             ])->save();
         }
