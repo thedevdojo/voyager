d35efa1f3aa59a287fdfe5e7057798ef2261292c
MrCrayon <crayon@mysticrayon.com>
2018-03-19 22:16:36 +0100

    Make Actions column not searchable (#2840)

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -257,6 +257,7 @@
                     array_merge([
                         "order" => [],
                         "language" => __('voyager::datatable'),
+                        "columnDefs" => [['searchable' =>  false, 'targets' => -1 ]],
                     ],
                     config('voyager.dashboard.data_tables', []))
                 , true) !!});
