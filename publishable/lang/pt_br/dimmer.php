61b3ba989e12d0e9e15ca3c446ca2d779f41141e
Felipe Braiani <felipe.braianitech@gmail.com>
2018-03-21 23:59:51 -0400

    Fix a misspelling on dimmer file of pt_br lang.

--- publishable/lang/pt_br/dimmer.php
+++ publishable/lang/pt_br/dimmer.php
@@ -3,11 +3,11 @@
 return [
     'page'                    => 'Página|Páginas',
     'page_link_text'          => 'Ver todas as páginas',
-    'page_text'               => 'Tem :count :string na seu banco de dados. Clique no botão abaixo para ver todas as páginas.',
+    'page_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todas as páginas.',
     'post'                    => 'Publicação|Publicações',
     'post_link_text'          => 'Ver todas as publicações',
-    'post_text'               => 'Tem :count :string na seu banco de dados. Clique no botão abaixo para ver todas as publicações.',
+    'post_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todas as publicações.',
     'user'                    => 'Utilizador|Utilizadores',
     'user_link_text'          => 'Ver todos os utilizadores',
-    'user_text'               => 'Tem :count :string na seu banco de dados. Clique no botão abaixo para ver todos os utilizadores.',
+    'user_text'               => 'Tem :count :string no seu banco de dados. Clique no botão abaixo para ver todos os utilizadores.',
 ];
