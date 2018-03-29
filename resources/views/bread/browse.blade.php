b2b05df561544b8558f3dfa620ab25d58381f107
Labros (PC) <zotos.lab@gmail.com>
2018-03-29 20:11:33 +0300

    Moved English translation for create_model_table from lang/en/bread.php to lang/en/database.php, dixed minor translation bug on views/bread/browse.php

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -194,7 +194,7 @@
                         @if ($isServerSide)
                             <div class="pull-left">
                                 <div role="status" class="show-res" aria-live="polite">{{ trans_choice(
-                                    'voyager.generic.showing_entries', $dataTypeContent->total(), [
+                                    'voyager::generic.showing_entries', $dataTypeContent->total(), [
                                         'from' => $dataTypeContent->firstItem(),
                                         'to' => $dataTypeContent->lastItem(),
                                         'all' => $dataTypeContent->total()
