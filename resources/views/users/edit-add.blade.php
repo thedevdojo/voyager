c68701bb195c7378fcaafd10a2f2c398e66d5b83
Christoph Schweppe <yipsocom@googlemail.com>
2018-04-19 12:14:01 +0200

    Prevent autocomplete on Profile Page

--- resources/views/users/edit-add.blade.php
+++ resources/views/users/edit-add.blade.php
@@ -17,7 +17,7 @@
     <div class="page-content container-fluid">
         <form class="form-edit-add" role="form"
               action="{{ (isset($dataTypeContent->id)) ? route('voyager.'.$dataType->slug.'.update', $dataTypeContent->id) : route('voyager.'.$dataType->slug.'.store') }}"
-              method="POST" enctype="multipart/form-data">
+              method="POST" enctype="multipart/form-data" autocomplete="off">
             <!-- PUT Method if we are editing -->
             @if(isset($dataTypeContent->id))
                 {{ method_field("PUT") }}
@@ -57,7 +57,7 @@
                                     <br>
                                     <small>{{ __('voyager::profile.password_hint') }}</small>
                                 @endif
-                                <input type="password" class="form-control" id="password" name="password" value="">
+                                <input type="password" class="form-control" id="password" name="password" value="" autocomplete="new-password">
                             </div>
 
                             @can('editRoles', $dataTypeContent)
