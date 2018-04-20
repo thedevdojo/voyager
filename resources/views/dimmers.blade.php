91a1012327f67b9b45096ea4cee9d5118f522a6e
Cyril de Wit <cyrilwit@gmail.com>
2018-04-20 20:36:41 +0200

    Remove all widgets that are not accessible for the current user

--- resources/views/dimmers.blade.php
+++ resources/views/dimmers.blade.php
@@ -1,7 +1,17 @@
 @php
 $dimmers = \Arrilot\Widgets\Facade::group('voyager::dimmers');
-$count = $dimmers->count();
 
+// Loop over the defined dashboard widgets. Get the related model and remove the
+// widget from the group if the current user hasn't the permission to read it.
+foreach (config('voyager.dashboard.widgets') as $widgetClass) {
+    $relatedModel = app($widgetClass)->getRelatedModel();
+
+    if (! Auth::user()->can('read', $relatedModel)) {
+        $dimmers->removeByName($widgetClass);
+    }
+}
+
+$count = $dimmers->count();
 $classes = [
     'col-xs-12',
     'col-sm-'.($count >= 2 ? '6' : '12'),
