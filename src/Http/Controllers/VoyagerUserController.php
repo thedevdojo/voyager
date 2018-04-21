d962abd60b13adbaccc4ffa37cd01dae14510366
MrCrayon <crayon@mysticrayon.com>
2018-04-21 09:46:21 +0200

    use Auth::user()->can()

--- src/Http/Controllers/VoyagerUserController.php
+++ src/Http/Controllers/VoyagerUserController.php
@@ -3,8 +3,7 @@
 namespace TCG\Voyager\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use \Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth
 use TCG\Voyager\Facades\Voyager;
 
 class VoyagerUserController extends VoyagerBaseController
@@ -18,7 +17,7 @@ class VoyagerUserController extends VoyagerBaseController
     // POST BR(E)AD
     public function update(Request $request, $id)
     {
-        if (!Gate::check('edit', Voyager::model('User')) && $id == Auth::user()->id) {
+        if (!Auth::user()->can('edit', Voyager::model('User')) && $id == Auth::user()->id) {
             // If can't edit users can still edit its own profile but can't change roles
             // we need to preserve roles already set
             $roles = Auth::user()->roles->pluck('id')->toArray();
