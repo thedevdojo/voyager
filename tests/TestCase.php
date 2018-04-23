158228f0303626bd205769228a71f992a065e191
Cyril de Wit <cyrilwit@gmail.com>
2018-04-23 21:19:30 +0200

    Remove registerPackageFactories to discuss it more

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -154,16 +154,6 @@ class TestCase extends OrchestraTestCase
 
         return $this->assertSee($text);
     }
-
-    /**
-     * Register package related model factories.
-     *
-     * @return void
-     */
-    protected function registerPackageFactories()
-    {
-        $this->withFactories(realpath(__DIR__.'/database/factories'));
-    }
 }
 
 class DisabledTestException extends Handler
