c76e9d739e1d65572a1995dd51d55d495d37fb19
Michal Hojgr <michal.hojgr@gmail.com>
2018-04-24 17:19:50 +0200

    Fix BelongsToMany relationships
    This commit fixes belongsTo relationships. 
    
    Without this change, all belongsTo relationships are never persistet, because they are ignored (condition on line 63. the key in input is "*_belongsto_*_relationship", but `$row->field` is target column name, i.e. `user_id`.

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -54,21 +54,17 @@ abstract class Controller extends BaseController
         foreach ($rows as $row) {
             $options = json_decode($row->details);
 
-            // if the field for this row is absent from the request, continue
-            // checkboxes will be absent when unchecked, thus they are the exception
-            if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
-                // if the field is a belongsToMany relationship, don't remove it
-                // if no content is provided, that means the relationships need to be removed
-                if ((isset($options->type) && $options->type !== 'belongsToMany') || $row->field !== 'user_belongsto_role_relationship') {
-                    continue;
-                }
-            }
-
-            $content = $this->getContentBasedOnType($request, $slug, $row, $options);
-
             if ($row->type == 'relationship' && $options->type != 'belongsToMany') {
                 $row->field = @$options->column;
             }
+            
+            // if the field for this row is absent from the request, continue
+            // checkboxes will be absent when unchecked, thus they are the exception
+            if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
+                continue;
+            }
+
+            $content = $this->getContentBasedOnType($request, $slug, $row, $options);
 
             /*
              * merge ex_images and upload images
