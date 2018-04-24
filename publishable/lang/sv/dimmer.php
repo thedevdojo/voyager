6a723e3d26e501b7dfe3c37b081244230bc491ce
Mark Topper <mark@ulties.com>
2018-04-24 09:24:29 +0000

    Apply fixes from StyleCI

--- publishable/lang/sv/dimmer.php
+++ publishable/lang/sv/dimmer.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'page'				=> 'Sida|Sidor',
+    'page'				        => 'Sida|Sidor',
     'page_link_text' 	=> 'Visa alla sidor',
-    'page_text' 		=> 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla.',
-    'post' 				=> 'Post|Poster',
+    'page_text' 		    => 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla.',
+    'post' 				       => 'Post|Poster',
     'post_link_text' 	=> 'Visa all poster',
-    'post_text' 		=> 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla.',
-    'user' 				=> 'Användare|Användare',
+    'post_text' 		    => 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla.',
+    'user' 				       => 'Användare|Användare',
     'user_link_text' 	=> 'Visa alla användare',
-    'user_text' 		=> 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla posanvändareter.',
+    'user_text' 		    => 'Du har :count :string i din databas. Klicka på knappen nedan för att visa alla posanvändareter.',
 ];
