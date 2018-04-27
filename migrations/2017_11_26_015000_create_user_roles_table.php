7d26183feb6aa355d9749b8b89f0fb047e8f422e
Xavi <xaviqv@gmail.com>
2018-04-27 10:33:15 +0000

    Apply fixes from StyleCI

--- migrations/2017_11_26_015000_create_user_roles_table.php
+++ migrations/2017_11_26_015000_create_user_roles_table.php
@@ -13,15 +13,15 @@ class CreateUserRolesTable extends Migration
      */
     public function up()
     {
-		Schema::table('users', function (Blueprint $table) {
-			$table->unsignedInteger('id')->change();
-		});	
-		
-		Schema::create('user_roles', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('id')->change();
+        });
+
+        Schema::create('user_roles', function (Blueprint $table) {
             $table->integer('user_id')->unsigned()->index();
             $table->integer('role_id')->unsigned()->index();
-			$table->primary(['user_id', 'role_id']);
-			$table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->primary(['user_id', 'role_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
