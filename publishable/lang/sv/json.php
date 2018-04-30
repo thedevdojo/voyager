8ea4eb5cc95265e6eecaa885151bff0c7dc676e8
wp-src <wpmagic70@gmail.com>
2018-04-30 13:51:34 +0430

    Persian language added

--- publishable/lang/sv/json.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'invalid' 			       => 'Ogiltig JSON',
-    'invalid_message' 	 => 'Verkar som du har angett ogiltig JSON.',
-    'valid' 			         => 'Giltig JSON',
-    'validation_errors' => 'Valideringsfel',
-];
--- publishable/lang/sv/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'loggingin' 		   => 'Logga in',
-    'signin_below' 		=> 'Logga in nedan:',
-    'welcome' 			    => 'Välkommen till Voyager. Den saknade Admin-panelen för Laravel',
-];
