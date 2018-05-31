10db147d0864ba3964ae1d16e94d006b130daf8f
Jarratt Ingram <jarratt@six.digital>
2018-05-31 08:02:20 +0200

    Added missing import class

--- src/Http/Controllers/VoyagerBaseController.php
+++ src/Http/Controllers/VoyagerBaseController.php
@@ -2,6 +2,7 @@
 
 namespace TCG\Voyager\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use TCG\Voyager\Database\Schema\SchemaManager;
