f3bed0e213ea109d4f4555acce69cdaf615d2739
Christoph Schweppe <yipsocom@googlemail.com>
2018-06-03 19:21:07 +0200

    Table default charset #2745

--- src/Http/Controllers/VoyagerDatabaseController.php
+++ src/Http/Controllers/VoyagerDatabaseController.php
@@ -66,9 +66,13 @@ class VoyagerDatabaseController extends Controller
         Voyager::canOrFail('browse_database');
 
         try {
+            $conn = 'database.connections.'.config('database.default');
             Type::registerCustomPlatformTypes();
 
-            $table = Table::make($request->table);
+            $table = json_decode($request->table, true);
+            $table['options']['collate'] = config($conn.'.collation', 'utf8mb4_unicode_ci');
+            $table['options']['charset'] = config($conn.'.charset', 'utf8mb4');
+            $table = Table::make($table);
             SchemaManager::createTable($table);
 
             if (isset($request->create_model) && $request->create_model == 'on') {
