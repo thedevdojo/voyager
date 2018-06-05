51518aef1cc9b712fd9e1c7be0cce34ffa754f4d
Sylvain Pillet <sylvain.pillet@medicol.ch>
2018-06-05 15:31:26 +0200

    Forgotten plural of user
    Forgotten plural of user

--- publishable/lang/fr/dimmer.php
+++ publishable/lang/fr/dimmer.php
@@ -7,7 +7,7 @@ return [
     'post'           => 'Article|Articles',
     'post_link_text' => 'Voir tous les articles',
     'post_text'      => 'Vous avez :count :string enregistrés. Cliquez sur le bouton ci-dessous pour afficher tous les articles.',
-    'user'           => 'Utilisateur|Utilisateur',
+    'user'           => 'Utilisateur|Utilisateurs',
     'user_link_text' => 'Voir tous les utilisateurs',
     'user_text'      => 'Vous avez :count :string enregistrés. Cliquez sur le bouton ci-dessous pour afficher tous les utilisateurs.',
 ];
