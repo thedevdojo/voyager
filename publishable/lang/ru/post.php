05d1068ca9cce58b2bb168f6157c8dd9874f2d1e
RomanGatchenko <37340795+RomanGatchenko@users.noreply.github.com>
2018-06-15 00:01:55 +0300

    Update generic.php (#3257)
    * Update generic.php
    
    * Update bread.php
    
    * Update database.php
    
    * Update media.php
    
    * Update post.php
    
    * Update profile.php

--- publishable/lang/ru/post.php
+++ publishable/lang/ru/post.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'additional_fields'=> 'Дополнительные поля',    
     'category'         => 'Категория сообщения',
     'content'          => 'Текст сообщения',
     'details'          => 'Свойства',
