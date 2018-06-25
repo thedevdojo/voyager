34dfd8524b6de82a0a04f200f96307b491da0274
Furkan Kavlak <furkan@kavlak.me>
2018-06-25 18:36:55 +0300

    Translation Improvements for Turkish (#3348)

--- publishable/lang/tr/analytics.php
+++ publishable/lang/tr/analytics.php
@@ -1,12 +1,10 @@
 <?php
 
 return [
-    'by_pageview'  => 'Sayfa görüntülenmeye göre',
-    'by_sessions'  => 'Oturuma göre',
-    'by_users'     => 'Kullanıcıya göre',
-    'no_client_id' => 'To view analytics you\'ll need to get a google analytics client id and '.
-        'add it to your settings for the key <code>google_analytics_client_id'.
-        '</code>. Get your key in your Google developer console:',
+    'by_pageview'            => 'Sayfa görüntülenmesine göre',
+    'by_sessions'            => 'Oturuma göre',
+    'by_users'               => 'Kullanıcıya göre',
+    'no_client_id'           => 'Analitiği görmek için Google Analytics İstemci Kimliği almanız ve <code>google_analytics_client_id</code> kodu içinde ayarlarınıza eklemeniz gerekmektedir. Anahtar kodunuzu Google Geliştirici Konsolu\'ndan alın:',
     'set_view'               => 'Görünüm seçin',
     'this_vs_last_week'      => 'Bu Hafta vs Geçen Hafta',
     'this_vs_last_year'      => 'Bu Yıl vs Geçen Yıl',
