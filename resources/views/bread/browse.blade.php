a70cadb9517c455cfcb64fd7957c1c00bdd32e69
Rajendra Sharma <rajendras@rabbit.co.th>
2018-10-08 14:42:30 +0700

    fixed browse issue if select dropdown is set as dropdown with empty options

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -87,7 +87,8 @@
                                         @endcan
                                         @foreach($dataType->browseRows as $row)
                                             <td>
-                                                <?php $options = json_decode($row->details); ?>
+                                                <?php $options = ($row->details) ? json_decode
+                                                ($row->details) : new stdClass(); ?>
                                                 @if($row->type == 'image')
                                                     <img src="@if( !filter_var($data->{$row->field}, FILTER_VALIDATE_URL)){{ Voyager::image( $data->{$row->field} ) }}@else{{ $data->{$row->field} }}@endif" style="width:100px">
                                                 @elseif($row->type == 'relationship')
