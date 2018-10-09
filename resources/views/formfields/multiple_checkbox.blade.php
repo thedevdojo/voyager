063a1d3bca1f336d701d09bfd1fea5a8539ed4f7
Rajendra Sharma <rajendras@rabbit.co.th>
2018-10-09 09:37:22 +0700

    cs fix

--- resources/views/formfields/multiple_checkbox.blade.php
+++ resources/views/formfields/multiple_checkbox.blade.php
@@ -5,14 +5,11 @@
         @if(isset($dataTypeContent->{$row->field}) || old($row->field))
             <?php $checked = old($row->field, $dataTypeContent->{$row->field}); ?>
         @else
-            <?php $checked = isset($options->checked) &&
-                             $options->checked ? true : false;
-            ?>
+            <?php $checked = isset($options->checked) && $options->checked ? true : false; ?>
         @endif
-        <input type="checkbox" name="{{ $row->field }}[{{$key}}]"
-               {!! $checked ? 'checked="checked"' : '' !!} value="{{$key}}"
 
-        />
+        <input type="checkbox" name="{{ $row->field }}[{{$key}}]"
+               {!! $checked ? 'checked="checked"' : '' !!} value="{{$key}}" />
         {{$label}}
     @endforeach
 @endif
