feba1b95beb47789a1ad774411177f58d650a9a7
Karim Qaderi <32425175+KarimQaderi@users.noreply.github.com>
2018-10-20 19:25:05 +0330

    Update edit-add.blade.php

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -54,7 +54,6 @@
                                 <!-- GET THE DISPLAY OPTIONS -->
                                 @php
                                     $display_options = isset($row->details->display) ? $row->details->display : NULL;
-                                     $options =$row->details;
                                 @endphp
                                 @if (isset($row->details->legend) && isset($row->details->legend->text))
                                     <legend class="text-{{isset($row->details->legend->align) ? $row->details->legend->align : 'center'}}" style="background-color: {{isset($row->details->legend->bgcolor) ? $row->details->legend->bgcolor : '#f0f0f0'}};padding: 5px;">{{$row->details->legend->text}}</legend>
@@ -67,7 +66,7 @@
                                         <label for="name">{{ $row->display_name }}</label>
                                         @include('voyager::multilingual.input-hidden-bread-edit-add')
                                         @if($row->type == 'relationship')
-                                            @include('voyager::formfields.relationship')
+                                            @include('voyager::formfields.relationship', ['options' => $row->details])      
                                         @else
                                             {!! app('voyager')->formField($row, $dataType, $dataTypeContent) !!}
                                         @endif
