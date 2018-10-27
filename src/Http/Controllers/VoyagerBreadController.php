ad9e01595f11d0a74dad82dc9558027260c98080
jehadja <jehad.jaghoub2@gmail.com>
2018-10-27 22:20:16 +0300

    Update VoyagerBreadController.php

--- src/Http/Controllers/VoyagerBreadController.php
+++ src/Http/Controllers/VoyagerBreadController.php
@@ -450,16 +450,16 @@ class VoyagerBreadController extends Controller
 
     public function sortImages(request $request)
     {
-        $input=$request->all();
-        $data =collect(json_decode($input['order']));
+        $input = $request->all();
+        $data = collect(json_decode($input['order']));
         $id = json_decode($input['order'],true)[0]['id'];
-        $field =json_decode($input['order'],true)[0]['fieldName'];
+        $field = json_decode($input['order'],true)[0]['fieldName'];
         $images = $data->pluck("image");
         $slug = $this->getSlug($request);
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
         $this->authorize('edit', app($dataType->model_name));
         $model = app($dataType->model_name);
-        $item  =$model->where("id",$id)->firstOrFail();
+        $item  = $model->where("id",$id)->firstOrFail();
         $item->{$field} = json_encode($images);
         $item->save();
     }
