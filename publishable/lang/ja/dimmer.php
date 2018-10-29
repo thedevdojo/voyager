1b3ceeb9d710d3b0048e12b9e3f13375cb8d4b07
Hiroki Shibahara <shibomb@gmail.com>
2018-10-30 05:57:22 +0900

    Add: Japanese language (ja) files (#3674)
    Update:  Can not translate 'Browse' string in admin BREAD index view.

---
+++ publishable/lang/ja/dimmer.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'page'           => 'ページ|ページ',
+    'page_link_text' => 'すべてのページを表示',
+    'page_text'      => 'データベースに:count:stringが存在します。 すべての:stringを表示するには下記のボタンをクリックください。',
+    'post'           => '投稿|投稿',
+    'post_link_text' => 'すべての投稿を表示',
+    'post_text'      => 'データベースに:count:stringが存在します。 すべての:stringを表示するには下記のボタンをクリックください。',
+    'user'           => 'ユーザー|ユーザー',
+    'user_link_text' => 'すべてのユーザーを表示',
+    'user_text'      => 'データベースに:count:stringが存在します。 すべての:stringを表示するには下記のボタンをクリックください。',
+];
