901370dbff31399b8ffb0d4705ee8243c91ca1f9
Karim Qaderi <32425175+KarimQaderi@users.noreply.github.com>
2018-10-30 12:50:02 +0330

    fix select_dropdown

--- resources/views/formfields/select_dropdown.blade.php
+++ resources/views/formfields/select_dropdown.blade.php
@@ -62,8 +62,8 @@
     <?php $selected_value = (isset($dataTypeContent->{$row->field}) && !is_null(old($row->field, $dataTypeContent->{$row->field}))) ? old($row->field, $dataTypeContent->{$row->field}) : old($row->field); ?>
     <select class="form-control select2" name="{{ $row->field }}">
         <?php $default = (isset($options->default) && !isset($dataTypeContent->{$row->field})) ? $options->default : null; ?>
-        @if(isset($options->options))
-            @foreach($options->options as $key => $option)
+        @if(isset(json_decode($options)->options))
+            @foreach(json_decode($options)->options as $key => $option)
                 <option value="{{ $key }}" @if($default == $key && $selected_value === NULL){{ 'selected="selected"' }}@endif @if($selected_value == $key){{ 'selected="selected"' }}@endif>{{ $option }}</option>
             @endforeach
         @endif
