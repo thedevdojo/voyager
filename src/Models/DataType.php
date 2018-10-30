1d63b98edf4ca32e699defe509e3b2d3ddb6b67e
jehadja <jehad.jaghoub2@gmail.com>
2018-10-30 13:31:03 +0300

    Update DataType.php

--- src/Models/DataType.php
+++ src/Models/DataType.php
@@ -273,6 +273,7 @@ class DataType extends Model
     {
         $this->attributes['details'] = collect($this->details)->merge(['nasted_realtion' => $value]);
     }
+    
     public function getNastedRealtionColumnAttribute()
     {
         return isset($this->details->nasted_realtion_column) ? $this->details->nasted_realtion_column : null;
@@ -282,5 +283,4 @@ class DataType extends Model
     {
         $this->attributes['details'] = collect($this->details)->merge(['nasted_realtion_column' => $value]);
     }
-
 }
