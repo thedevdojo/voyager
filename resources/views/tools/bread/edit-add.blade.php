ada5e4abaf69588059fb01a874392c1bd4cabc7a
Christoph Schweppe <info@cschweppe.de>
2018-12-13 00:04:39 +0100

    Set default ordering for BREAD (#3781)
    * Set default ordering for BREAD
    
    Control the default ordering for bread-browse by selecting an order-column and direction in bread-settings.
    
    Fixes https://github.com/the-control-group/voyager/issues/3222
    
    * Correct alphabetical order in language-file
    
    * StyleCI fix
    
    * StyleCI fix #2
    
    * All directions lower-case, correct URLs for server-side pagination
    
    * Also use direction for ordering
    
    * Add documentation
    
    * Add (proper) german translations

--- resources/views/tools/bread/edit-add.blade.php
+++ resources/views/tools/bread/edit-add.blade.php
@@ -176,7 +176,7 @@
                                 </div>
                             </div>
                             <div class="row clearfix">
-                                <div class="col-md-6 form-group">
+                                <div class="col-md-4 form-group">
                                     <label for="order_column">{{ __('voyager::bread.order_column') }}</label>
                                     <span class="voyager-question"
                                           aria-hidden="true"
@@ -192,7 +192,7 @@
                                         @endforeach
                                       </select>
                                 </div>
-                                <div class="col-md-6 form-group">
+                                <div class="col-md-4 form-group">
                                     <label for="order_display_column">{{ __('voyager::bread.order_ident_column') }}</label>
                                     <span class="voyager-question"
                                           aria-hidden="true"
@@ -206,6 +206,17 @@
                                         @if(isset($dataType) && $dataType->order_display_column == $tbl['field']) selected @endif
                                         >{{ $tbl['field'] }}</option>
                                         @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-md-4 form-group">
+                                    <label for="order_direction">{{ __('voyager::bread.order_direction') }}</label>
+                                    <select name="order_direction" class="select2 form-control">
+                                        <option value="asc" @if(isset($dataType) && $dataType->order_direction == 'asc') selected @endif>
+                                            {{ __('voyager::generic.ascending') }}
+                                        </option>
+                                        <option value="desc" @if(isset($dataType) && $dataType->order_direction == 'desc') selected @endif>
+                                            {{ __('voyager::generic.descending') }}
+                                        </option>
                                     </select>
                                 </div>
                             </div>
