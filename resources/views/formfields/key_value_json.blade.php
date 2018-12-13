ca6c8791b51717bb34b9a32c813f30071b2d05f7
Sergey <bnku@ya.ru>
2018-12-13 19:02:52 +0300

    finally roll back :)

--- resources/views/formfields/key_value_json.blade.php
+++ resources/views/formfields/key_value_json.blade.php
@@ -1,7 +1,6 @@
 @php
     if($dataTypeContent->{$row->field}){
         $old_parameters = json_decode($dataTypeContent->{$row->field});
-        $keys = array_keys((array)$old_parameters);
     }
     $end_id = 0;
 @endphp
