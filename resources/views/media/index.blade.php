980c52a40b555d5db724ac78a83a217b2fd87c6e
Christoph Schweppe <info@cschweppe.de>
2018-12-14 10:10:44 +0100

    Fix MediaManager "Forbidden" error

--- resources/views/media/index.blade.php
+++ resources/views/media/index.blade.php
@@ -258,7 +258,7 @@
                     <!-- End Move File Modal -->
 
                     <!-- Image Modal -->
-                    <div class="modal fade" id="imagemodal">
+                    <div class="modal fade" id="imagemodal" v-if="selected_file.type != 'folder'">
                         <div class="modal-dialog">
                             <div class="modal-content">
                                 <div class="modal-header">
