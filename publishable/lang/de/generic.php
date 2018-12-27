c5eccd0f74a1c6ec8e4bb62ceb0d00a9072fbf77
Christoph Schweppe <info@cschweppe.de>
2018-12-27 13:12:50 +0100

    Define RTL based on locale

--- publishable/lang/de/generic.php
+++ publishable/lang/de/generic.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'is_rtl'                 => 'false',
     'action'                 => 'Aktion',
     'actions'                => 'Aktionen',
     'add'                    => 'Hinzufügen',
