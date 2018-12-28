603971f5f0f4b0bf5fcaae4655a75953e258732e
Christoph Schweppe <info@cschweppe.de>
2018-12-28 22:22:16 +0100

    Check permissions

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -22,9 +22,11 @@
                 </a>
             @endif
         @endcan
-        @if($usesSoftDeletes)
-            <input type="checkbox" @if ($showSoftDeleted) checked @endif id="show_soft_deletes" data-toggle="toggle" data-on="{{ __('voyager::bread.soft_deletes_off') }}" data-off="{{ __('voyager::bread.soft_deletes_on') }}">
-        @endif
+        @can('delete', app($dataType->model_name))
+            @if($usesSoftDeletes)
+                <input type="checkbox" @if ($showSoftDeleted) checked @endif id="show_soft_deletes" data-toggle="toggle" data-on="{{ __('voyager::bread.soft_deletes_off') }}" data-off="{{ __('voyager::bread.soft_deletes_on') }}">
+            @endif
+        @endcan
         @include('voyager::multilingual.language-selector')
     </div>
 @stop
