99b92ecc3fdd56fa13a7b07024cf60ba05659ad0
Christoph Schweppe <info@cschweppe.de>
2018-12-30 13:20:00 +0100

    Fix tests

--- src/FormFields/MultipleCheckboxHandler.php
+++ src/FormFields/MultipleCheckboxHandler.php
@@ -5,14 +5,4 @@ namespace TCG\Voyager\FormFields;
 class MultipleCheckboxHandler extends AbstractHandler
 {
     protected $codename = 'multiple_checkbox';
-
-    public function createContent($row, $dataType, $dataTypeContent, $options)
-    {
-        return view('voyager::formfields.multiple_checkbox', [
-            'row'             => $row,
-            'options'         => $options,
-            'dataType'        => $dataType,
-            'dataTypeContent' => $dataTypeContent,
-        ]);
-    }
 }
