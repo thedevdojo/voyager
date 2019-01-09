361e059fbf1a87c20904a925078bb28e287c3899
Christoph Schweppe <info@cschweppe.de>
2019-01-09 16:03:37 +0100

    Cache 30 days

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -39,7 +39,7 @@ class Menu extends Model
     public static function display($menuName, $type = null, array $options = [])
     {
         // GET THE MENU - sort collection in blade
-        $menu = \Cache::remember('voyager_menu_'.$menuName, (60 * 24 * 39), function () use ($menuName) {
+        $menu = \Cache::remember('voyager_menu_'.$menuName, (60 * 24 * 30), function () use ($menuName) {
             return static::where('name', '=', $menuName)
             ->with(['parent_items.children' => function ($q) {
                 $q->orderBy('order');
