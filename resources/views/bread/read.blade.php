2f104441f3f6ff65f9c6ac11df014fd75084e408
Christoph Schweppe <info@cschweppe.de>
2019-01-29 10:43:26 +0100

    Empty instead of count in read-view

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -77,7 +77,7 @@
                                     @endforeach
 
                                 @elseif(property_exists($row->details, 'options'))
-                                    @if (count(json_decode($dataTypeContent->{$row->field})) > 0)
+                                    @if (!empty(json_decode($data->{$row->field})))
                                         @foreach(json_decode($dataTypeContent->{$row->field}) as $item)
                                             @if (@$row->details->options->{$item})
                                                 {{ $row->details->options->{$item} . (!$loop->last ? ', ' : '') }}
