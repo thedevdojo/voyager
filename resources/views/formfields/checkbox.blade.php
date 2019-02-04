79177d6be556152ccb78bf0e338760f78390366c
emerica <emerica@gmail.com>
2019-02-03 22:44:02 -0700

    Indentation/80col cleanup

--- resources/views/formfields/checkbox.blade.php
+++ resources/views/formfields/checkbox.blade.php
@@ -4,16 +4,16 @@
     <?php $checked = old($row->field, $dataTypeContent->{$row->field}); ?>
 @else
     <?php $checked = isset($options->checked) &&
-      filter_var($options->checked, FILTER_VALIDATE_BOOLEAN) ? true: false; ?>
+        filter_var($options->checked, FILTER_VALIDATE_BOOLEAN) ? true: false; ?>
 @endif
 
 <?php $class = isset($options->class) ? $options->class : "toggleswitch"; ?>
 
 @if(isset($options->on) && isset($options->off))
     <input type="checkbox" name="{{ $row->field }}" class="{{ $class }}"
-           data-on="{{ $options->on }}" {!! $checked ? 'checked="checked"' : '' !!}
-           data-off="{{ $options->off }}">
+        data-on="{{ $options->on }}" {!! $checked ? 'checked="checked"' : '' !!}
+        data-off="{{ $options->off }}">
 @else
     <input type="checkbox" name="{{ $row->field }}" class="{{ $class }}"
-           @if($checked) checked @endif>
+        @if($checked) checked @endif>
 @endif
