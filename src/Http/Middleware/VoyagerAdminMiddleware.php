0c076c3886c9457e9db867f9da12f3f87345fad5
Alexey <info@khachatryan.org>
2019-02-15 18:21:28 +0400

    Changed Auth web user to admin user

--- src/Http/Middleware/VoyagerAdminMiddleware.php
+++ src/Http/Middleware/VoyagerAdminMiddleware.php
@@ -18,8 +18,8 @@ class VoyagerAdminMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if (!Auth::guest()) {
-            $user = auth()->user();
+        if (!Auth::guard('admin')->guest()) {
+            $user = Auth::guard('admin')->user();
             if (isset($user->locale)) {
                 app()->setLocale($user->locale);
             }
