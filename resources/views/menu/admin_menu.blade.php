5dfe098234dd76150e09d7d960d959a3bea7eb69
Christoph Schweppe <info@cschweppe.de>
2019-02-25 13:56:20 +0100

    Show caret for non-active dropdowns

--- resources/views/menu/admin_menu.blade.php
+++ resources/views/menu/admin_menu.blade.php
@@ -1,7 +1,7 @@
 @section('admin-menu')
 <ul class="nav navbar-nav">
     <li v-for="(item, i) in items" :class="classes(item)">
-        <a :target="item.target" :href="item.children.length > 0 ? '#'+item.id+'-dropdown-element' : item.href" :style="'color:'+color(item)" v-bind:data-toggle="item.children.length > 0 ? 'collapse' : false" :aria-expanded="item.active">
+        <a :target="item.target" :href="item.children.length > 0 ? '#'+item.id+'-dropdown-element' : item.href" :style="'color:'+color(item)" v-bind:data-toggle="item.children.length > 0 ? 'collapse' : false" :aria-expanded="item.children.length > 0 ? String(item.active) : false">
             <span :class="'icon '+item.icon_class"></span>
             <span class="title">@{{ item.title }}</span>
         </a>
