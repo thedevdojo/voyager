6484015f870070231cc3df777874b849bf0f2c73
Christoph Schweppe <info@cschweppe.de>
2019-03-05 21:10:39 +0100

    Update migration to support all Laravel versions

--- migrations/2017_11_26_015000_create_user_roles_table.php
+++ migrations/2017_11_26_015000_create_user_roles_table.php
@@ -14,7 +14,13 @@ class CreateUserRolesTable extends Migration
     public function up()
     {
         Schema::create('user_roles', function (Blueprint $table) {
-            $table->bigInteger('user_id')->unsigned()->index();
+            $type = DB::connection()->getDoctrineColumn('users', 'id')->getType()->getName();
+            if ($type == 'bigint') {
+                $table->bigInteger('user_id')->unsigned()->index();
+            } else {
+                $table->integer('user_id')->unsigned()->index();
+            }
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->bigInteger('role_id')->unsigned()->index();
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
