45b9dd46b06cc4be800936d05fc3732b1f3d5dd4
Felipe Braiani <felipe.braianitech@gmail.com>
2019-03-15 09:07:52 -0400

    Updated pt_br language

--- publishable/lang/pt_br/profile.php
+++ publishable/lang/pt_br/profile.php
@@ -1,11 +1,14 @@
 <?php
 
 return [
-    'avatar'        => 'Avatar',
-    'edit'          => 'Editar o meu perfil',
-    'edit_user'     => 'Editar Utilizador',
-    'password'      => 'Senha',
-    'password_hint' => 'Deixar vazio para manter o valor atual',
-    'role'          => 'Função',
-    'user_role'     => 'Função do Utilizador',
+    'avatar'           => 'Avatar',
+    'edit'             => 'Editar o meu perfil',
+    'edit_user'        => 'Editar Utilizador',
+    'password'         => 'Senha',
+    'password_hint'    => 'Deixar vazio para manter o valor atual',
+    'role'             => 'Função',
+    'roles'            => 'Funções',
+    'role_default'     => 'Função Padrão',
+    'roles_additional' => 'Funções Adicionais',
+    'user_role'        => 'Função do Utilizador',
 ];
