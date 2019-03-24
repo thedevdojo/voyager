30ce6d288a4abb8499af903d81c4acde018f4fc3
Ryan McDonald <ryan.mcdonald@outlook.com>
2019-03-24 10:40:19 -0500

    allow stacking of css for custom field types. @stack('javascript') already defined, why not css?

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -43,6 +43,7 @@
     @if(!empty(config('voyager.additional_css')))<!-- Additional CSS -->
         @foreach(config('voyager.additional_css') as $css)<link rel="stylesheet" type="text/css" href="{{ asset($css) }}">@endforeach
     @endif
+	@stack('css')
 
     @yield('head')
 </head>
