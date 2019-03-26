f7c32724c501c7529da60f7bced8b7918147ff67
gaabora <gaabora@gmail.com>
2019-03-26 14:30:04 +0300

    Create 2019_03_26_000000_update_data_rows_field_values.php

---
+++ migrations/2019_03_26_000000_update_data_rows_field_values.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use TCG\Voyager\Models\DataRow;
+use TCG\Voyager\Models\DataType;
+use Illuminate\Support\Str;
+
+class UpdateDataRowsFieldValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $records = TCG\Voyager\Models\DataRow::where('type','relationship')->get();
+        foreach ($records as $rec) {
+            $dataType = DataType::find($rec->data_type_id);
+            $field = Str::singular($dataType->name).'_'.$rec->details->type.'_'.Str::singular($rec->details->table).'_relationship_'.$rec->details->column;
+            
+            $a = $rec->details;
+            $a->_fallback_field = $rec->field;
+            
+            $rec->details = $a;
+            $rec->field = strtolower($field);
+            $rec->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $records = DataRow::where('type','relationship')->get();
+        foreach ($records as $rec) {
+       
+            if (property_exists($rec->details,'_fallback_field')) {
+                $field = $rec->details->_fallback_field;
+            
+                $a = $rec->details;
+                unset($a->_fallback_field);
+
+                $rec->details = $a;
+                $rec->field = $field;
+    
+                $rec->save();
+            }
+        }
+    }
+}
