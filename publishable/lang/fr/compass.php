11c890014cda3be1da5966e47b2c6b9398f31b26
MrCrayon <crayon@mysticrayon.com>
2019-04-05 05:07:32 +0200

    Removed leftover

--- publishable/lang/fr/compass.php
+++ publishable/lang/fr/compass.php
@@ -3,10 +3,6 @@
 return [
     'welcome'                => 'Bienvenue sur Voyager Compass. Chaque bonne nouvelle application ont besoin d\'une boussole pour se diriger dans la bonne direction.
     Dans cette section, vous trouverez plusieurs ressources et tâches administrative afin de vous guider lors de la création de votre application',
-
-        'Welcome to the Voyager Compass. Every good app needs a compass to point them in the right direction.
-    In this section you will find many resources and administrative tasks to help guide you as you build out your application.',
-
     'links'         => [
         'title'                 => 'Liens',
         'documentation'         => 'Documentation',
