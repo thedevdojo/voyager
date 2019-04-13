3c0d41531f58d3005d9f21ccdb3181e7bbac1bbd
Christoph Schweppe <info@cschweppe.de>
2019-04-13 18:34:17 +0200

    Add watermarking/thumbnails to media-picker and media-manager

--- publishable/config/voyager.php
+++ publishable/config/voyager.php
@@ -246,5 +246,20 @@ return [
        'allow_delete'        => true,
        'allow_create_folder' => true,
        'allow_rename'        => true,
+       /*'watermark'           => [
+            'source'         => 'watermark.png',
+            'position'       => 'bottom-left',
+            'x'              => 0,
+            'y'              => 0,
+            'size'           => 15,
+       ],
+       'thumbnails'          => [
+           [
+                'type'  => 'fit',
+                'name'  => 'fit-500',
+                'width' => 500,
+                'height'=> 500
+           ],
+       ]*/
    ],
 ];
