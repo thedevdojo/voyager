e80d37abb8e5cd35ae6dbad35206b6617334f3b0
Andrey Helldar <helldar@ai-rus.com>
2019-04-24 22:00:37 +0300

    Fixed remember old input upon form submit See #4035 issue

--- resources/views/formfields/color.blade.php
+++ resources/views/formfields/color.blade.php
@@ -1,2 +1,2 @@
 <input type="color" class="form-control" name="{{ $row->field }}"
-       value="{{ $dataTypeContent->{$row->field} ?? old($row->field) }}">
+       value="{{ old($row->field, $dataTypeContent->{$row->field}) }}">
