356a729f6f64a9a8a5c2976ca6760d1778a4446e
hirwaf <hirwa.felix@gmail.com>
2019-05-21 10:48:46 +0200

    Check users table that has role_id !

--- migrations/2016_01_01_000000_add_voyager_user_fields.php
+++ migrations/2016_01_01_000000_add_voyager_user_fields.php
@@ -13,7 +13,9 @@ class AddVoyagerUserFields extends Migration
             if (!Schema::hasColumn('users', 'avatar')) {
                 $table->string('avatar')->nullable()->after('email')->default('users/default.png');
             }
-            $table->bigInteger('role_id')->nullable()->after('id');
+            if (!Schema::hasColumn('users', 'role_id')) {
+                $table->bigInteger('role_id')->nullable()->after('id');
+            }           
         });
     }
 
