3b060ed7b9f65a282f3c29ac4aed1062560cf728
Christoph Schweppe <info@cschweppe.de>
2019-05-31 22:02:19 +0300

    Update number-formfield attributes

--- resources/views/formfields/number.blade.php
+++ resources/views/formfields/number.blade.php
@@ -3,8 +3,8 @@
        name="{{ $row->field }}"
        type="number"
        @if($row->required == 1) required @endif
-       @if(isset($options->min)) {{ 'min='.$options->min }} @endif
-       @if(isset($options->max)) {{ 'max='.$options->max }} @endif
+       @if(isset($options->min)) min="{{ $options->min }}" @endif
+       @if(isset($options->max)) max="{{ $options->max }}" @endif
        step="{{ $options->step ?? 'any' }}"
        placeholder="{{ old($row->field, $options->placeholder ?? $row->display_name) }}"
        value="{{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}">
