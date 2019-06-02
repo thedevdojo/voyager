c0ab90c8233f6644ad0ca7037b056c3236bc58cf
Ivan Eu <iveusss@gmail.com>
2019-06-02 15:09:25 +0300

    fix pull request 4

--- src/Http/Controllers/VoyagerBaseController.php
+++ src/Http/Controllers/VoyagerBaseController.php
@@ -104,7 +104,7 @@ class VoyagerBaseController extends Controller
                         $search->related_table = $row->details->table;
                     }
                 }
-                
+
                 $search_filter = ($search->filter == 'equals') ? '=' : 'LIKE';
                 $search_value = ($search->filter == 'equals') ? $search->value : '%'.$search->value.'%';
                 $query->where($search->key, $search_filter, $search_value);
