e2b8b235e49fe9a4fd738d0812205bc02760ec8a
Nicolò <nicveronese@gmail.com>
2019-07-06 18:52:04 +0200

    Skip fields if details columns is not set
    Some fields could have the details set as NULL or the type is not set.
    
    This patch allow to skip thoose fields withthout details or type.
    As specified in the code comments the only exceptios are:
     - Checkboxes
     - BelongsToMany relationship

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -58,7 +58,9 @@ abstract class Controller extends BaseController
             if (!$request->hasFile($row->field) && !$request->has($row->field) && $row->type !== 'checkbox') {
                 // if the field is a belongsToMany relationship, don't remove it
                 // if no content is provided, that means the relationships need to be removed
-                if (isset($row->details->type) && $row->details->type !== 'belongsToMany') {
+                if (isset($row->details->type) && $row->details->type === 'belongsToMany') {
+                    // Do nothing
+                } else {
                     continue;
                 }
             }
