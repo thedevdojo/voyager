aa5d50c8c6c28bc3897af8dbd6ad6c6b31487cb5
MrCrayon <crayon@mysticrayon.com>
2019-07-28 10:33:18 +0200

    Fixed parenthesis

--- resources/views/formfields/time.blade.php
+++ resources/views/formfields/time.blade.php
@@ -1,4 +1,4 @@
 <input @if($row->required == 1) required @endif type="time"  data-name="{{ $row->getTranslatedAttribute('display_name') }}"  class="form-control" name="{{ $row->field }}"
-       placeholder="{{ old($row->field, $options->placeholder ?? $row->getTranslatedAttribute('display_name') }}"
+       placeholder="{{ old($row->field, $options->placeholder ?? $row->getTranslatedAttribute('display_name')) }}"
        {!! isBreadSlugAutoGenerator($options) !!}
        value="{{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}">
