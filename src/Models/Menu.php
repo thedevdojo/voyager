a32ff7c51d6983fd10f5c48b2000da4a7a076baa
Christoph Schweppe <info@cschweppe.de>
2019-07-30 18:46:58 +0200

    Revert "Disable translations (#4283)"
    This reverts commit fa2edbb492a7697e882f9c7ed9cf50249207eaa6.

--- src/Models/Menu.php
+++ src/Models/Menu.php
@@ -108,7 +108,7 @@ class Menu extends Model
     {
         $items = $items->transform(function ($item) {
             // Translate title
-            $item->title = config('voyager.multilingual.enabled') ? $item->getTranslatedAttribute('title') : $item->title;
+            $item->title = $item->getTranslatedAttribute('title');
             // Resolve URL/Route
             $item->href = $item->link(true);
 
