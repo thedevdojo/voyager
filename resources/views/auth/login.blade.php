7003daefcb8d3821067ad986bcfc0595358e31ee
Tony Lea <tony@tonys-mbp-2.home>
2019-08-02 09:58:00 -0700

    Updating some assets and adding a few voyager classes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,23 +2,24 @@
 
 @section('content')
 
-    <div class="min-h-screen flex flex-col justify-center w-4/12 m-auto">
-        
-        <div class="bg-white shadow-md rounded flex">
-            <div class="flex flex-col px-12 py-16 mb-4 flex w-full">
-                <p class="text-gray-900 text-4xl font-black mb-4">Login</p>
-                <div class="mb-4">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="username">Username</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username">
-                </div>
-                <div class="mb-6">
-                    <label class="block text-grey-darker text-sm font-bold mb-2" for="password">Password</label>
-                    <input class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="******************">
-                    <p class="text-red text-xs italic">Please choose a password.</p>
-                </div>
-                <div class="flex items-center justify-between">
-                    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded" type="button">Sign In</button>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">Forgot Password?</a>
+    <div class="md:container m-auto sm:w-full sm:mx-0 xl:px-16">
+        <div class="min-h-screen flex flex-col justify-center w-full md:w-6/12 lg:w-5/12 xl:w-4/12 m-auto bg-white md:bg-gray-200">
+            
+            <div class="bg-white md:shadow-xl rounded flex -mt-12">
+                <div class="flex flex-col px-12 py-12 mb-4 flex w-full">
+                    <h1 class="voyager-h1">Login</h1>
+                    <div class="mb-4">
+                        <label class="voyager-label" for="username">Username</label>
+                        <input class="voyager-input" id="username" type="text" placeholder="Username">
+                    </div>
+                    <div class="mb-6">
+                        <label class="voyager-label" for="password">Password</label>
+                        <input class="voyager-input" id="password" type="password" placeholder="******************">
+                    </div>
+                    <div class="flex items-center justify-between mt-8">
+                        <button class="bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded" type="button">Sign In</button>
+                        <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">Forgot Password?</a>
+                    </div>
                 </div>
             </div>
         </div>
