f97dc9c71d72d8fb5fb0a5345fb751203cbf29e4
Adam Nielsen <1765602+iwasherefirst2@users.noreply.github.com>
2019-08-13 09:51:05 +0200

    adjusted brackets to style guides

--- src/Http/Middleware/VoyagerAdminMiddleware.php
+++ src/Http/Middleware/VoyagerAdminMiddleware.php
@@ -19,10 +19,10 @@ class VoyagerAdminMiddleware
         if (!app('VoyagerAuth')->guest()) {
 
             // Set default guard to voyager admin
-            if (!empty($guard = config('voyager.guard'))){
+            if (!empty($guard = config('voyager.guard'))) {
                 auth()->setDefaultDriver($guard);
             }
-            elseif (app('VoyagerAuth') instanceof \Illuminate\Auth\SessionGuard){
+            elseif (app('VoyagerAuth') instanceof \Illuminate\Auth\SessionGuard) {
               // Extract guard name from unique identifier for the auth session value
               preg_match('/(?<=_).*(?=_)/',app('VoyagerAuth')->getName(), $matches);
               auth()->setDefaultDriver($matches[0]);
