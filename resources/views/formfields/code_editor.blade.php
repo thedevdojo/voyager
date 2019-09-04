3447ffcc91e5f845d34a12223dc13091a62fa87e
owenjones <owen@owenjones.net>
2019-09-04 12:17:39 +0000

    Removed stray tabs within the code editor div

--- resources/views/formfields/code_editor.blade.php
+++ resources/views/formfields/code_editor.blade.php
@@ -1,4 +1,2 @@
-<div id="{{ $row->field }}" data-theme="{{ @$options->theme }}" data-language="{{ @$options->language }}" class="ace_editor min_height_200" name="{{ $row->field }}">
-    {{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}
-</div>
+<div id="{{ $row->field }}" data-theme="{{ @$options->theme }}" data-language="{{ @$options->language }}" class="ace_editor min_height_200" name="{{ $row->field }}">{{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}</div>
 <textarea name="{{ $row->field }}" id="{{ $row->field }}_textarea" class="hidden">{{ old($row->field, $dataTypeContent->{$row->field} ?? $options->default ?? '') }}</textarea>
