5e3fe452d61451553641e2ade0063680b9d1371d
Gruz <arygroup@gmail.com>
2019-09-25 12:05:25 +0300

    Update actions.blade.php
    In older Voyager version this code worked for me:
    
    ```php
    class AppServiceProvider extends ServiceProvider
    {
    ...
       public function boot()
        {
    ... 
                Voyager::replaceAction(DeleteAction::class, TenantDeleteAction::class);
    ...
         }
    ...
    }
    ```
    
    But after Voyager update it stopped working as "get_class" fails on string.

--- resources/views/bread/partials/actions.blade.php
+++ resources/views/bread/partials/actions.blade.php
@@ -1,7 +1,8 @@
 @if($data)
     @php
         // need to recreate object because policy might depend on record data
-        $class = get_class($action);
+        $class = is_object($action) ? get_class($action) : $action;
+        
         $action = new $class($dataType, $data);
     @endphp
     @can ($action->getPolicy(), $data)
