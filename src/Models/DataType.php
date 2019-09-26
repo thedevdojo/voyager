5171050a7a3f6e983d84c17a2a36a5771e2b6e81
MrCrayon <crayon@mysticrayon.com>
2019-09-26 08:07:02 +0200

    details-for-bread
    Enable editing details for relationship in BREAD

--- src/Models/DataType.php
+++ src/Models/DataType.php
@@ -198,7 +198,9 @@ class DataType extends Model
                         'taggable'    => $requestData['relationship_taggable_'.$relationship] ?? '0',
                     ];
 
-                    $requestData['field_details_'.$relationship] = json_encode($relationshipDetails);
+                    $details = json_decode($requestData['field_details_'.$relationship], true);
+                    $merge = array_merge($details, $relationshipDetails);
+                    $requestData['field_details_'.$relationship] = json_encode($merge);
                 }
             }
         }
