f6de7caf249c37963be6da30fe075fafd84b379e
Matt Rabe <matt@websolutionshack.com>
2019-10-17 15:14:59 -1000

    Add optional helperText for formfields Also fix edit-add label `for` attribute

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -69,7 +69,11 @@
 
                                 <div class="form-group @if($row->type == 'hidden') hidden @endif col-md-{{ $display_options->width ?? 12 }} {{ $errors->has($row->field) ? 'has-error' : '' }}" @if(isset($display_options->id)){{ "id=$display_options->id" }}@endif>
                                     {{ $row->slugify }}
-                                    <label class="control-label" for="name">{{ $row->getTranslatedAttribute('display_name') }}</label>
+                                    <label class="control-label" for="{{ $row->field }}">{{ $row->getTranslatedAttribute('display_name') }}</label>
+                                    @if (isset($row->details->helperText))
+                                        <p>{{ $row->details->helperText }}</p>
+                                    @endif
+
                                     @include('voyager::multilingual.input-hidden-bread-edit-add')
                                     @if (isset($row->details->view))
                                         @include($row->details->view, ['row' => $row, 'dataType' => $dataType, 'dataTypeContent' => $dataTypeContent, 'content' => $dataTypeContent->{$row->field}, 'action' => ($edit ? 'edit' : 'add')])
