06074a378a14a764f78aca4ba87384143fbc8ebd
Huynh Nhut Phi <huynhnhutphi1993@gmail.com>
2019-10-22 10:52:51 +0700

    Add Vietnamese Language

---
+++ publishable/lang/vi/seeders.php
@@ -0,0 +1,93 @@
+<?php
+
+return [
+    'data_rows'  => [
+        'author'           => 'Author',
+        'avatar'           => 'Avatar',
+        'body'             => 'Body',
+        'category'         => 'Category',
+        'created_at'       => 'Created At',
+        'display_name'     => 'Display Name',
+        'email'            => 'Email',
+        'excerpt'          => 'Excerpt',
+        'featured'         => 'Featured',
+        'id'               => 'ID',
+        'meta_description' => 'Meta Description',
+        'meta_keywords'    => 'Meta Keywords',
+        'name'             => 'Name',
+        'order'            => 'Order',
+        'page_image'       => 'Page Image',
+        'parent'           => 'Parent',
+        'password'         => 'Password',
+        'post_image'       => 'Post Image',
+        'remember_token'   => 'Remember Token',
+        'role'             => 'Role',
+        'seo_title'        => 'SEO Title',
+        'slug'             => 'Slug',
+        'status'           => 'Status',
+        'title'            => 'Title',
+        'updated_at'       => 'Updated At',
+    ],
+    'data_types' => [
+        'category' => [
+            'singular' => 'Category',
+            'plural'   => 'Categories',
+        ],
+        'menu'     => [
+            'singular' => 'Menu',
+            'plural'   => 'Menus',
+        ],
+        'page'     => [
+            'singular' => 'Page',
+            'plural'   => 'Pages',
+        ],
+        'post'     => [
+            'singular' => 'Post',
+            'plural'   => 'Posts',
+        ],
+        'role'     => [
+            'singular' => 'Role',
+            'plural'   => 'Roles',
+        ],
+        'user'     => [
+            'singular' => 'User',
+            'plural'   => 'Users',
+        ],
+    ],
+    'menu_items' => [
+        'bread'        => 'BREAD',
+        'categories'   => 'Categories',
+        'compass'      => 'Compass',
+        'dashboard'    => 'Dashboard',
+        'database'     => 'Database',
+        'media'        => 'Media',
+        'menu_builder' => 'Menu Builder',
+        'pages'        => 'Pages',
+        'posts'        => 'Posts',
+        'roles'        => 'Roles',
+        'settings'     => 'Settings',
+        'tools'        => 'Tools',
+        'users'        => 'Users',
+    ],
+    'roles'      => [
+        'admin' => 'Administrator',
+        'user'  => 'Normal User',
+    ],
+    'settings'   => [
+        'admin' => [
+            'background_image'           => 'Admin Background Image',
+            'description'                => 'Admin Description',
+            'description_value'          => 'Welcome to Voyager. The Missing Admin for Laravel',
+            'google_analytics_client_id' => 'Google Analytics Client ID (used for admin dashboard)',
+            'icon_image'                 => 'Admin Icon Image',
+            'loader'                     => 'Admin Loader',
+            'title'                      => 'Admin Title',
+        ],
+        'site'  => [
+            'description'                  => 'Site Description',
+            'google_analytics_tracking_id' => 'Google Analytics Tracking ID',
+            'logo'                         => 'Site Logo',
+            'title'                        => 'Site Title',
+        ],
+    ],
+];
