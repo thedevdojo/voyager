add88a66fe9d3bc6f2e236fd85248ec7ef2c60d9
Michael Chekin <mchekin@gmail.com>
2019-11-24 10:08:40 +0100

    Prevent "SQLite doesn't support dropping foreign keys" error.

--- migrations/2017_11_26_013050_add_user_role_relationship.php
+++ migrations/2017_11_26_013050_add_user_role_relationship.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddUserRoleRelationship extends Migration
@@ -27,7 +28,9 @@ class AddUserRoleRelationship extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['role_id']);
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->dropForeign(['role_id']);
+            }
         });
 
         Schema::table('users', function (Blueprint $table) {
