0e1417d3e4d20fd9727b8407373a8ee0ebe3d992
Kalin Dimitrov <30594752+DimitrovK@users.noreply.github.com>
2019-12-11 13:57:47 +0200

    Add files via upload

---
+++ publishable/lang/bg/settings.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'usage_help'           => 'You can get the value of each setting anywhere on your site by calling',
+    'save'                 => 'Save Settings',
+    'new'                  => 'New Setting',
+    'help_name'            => 'Setting name ex: Admin Title',
+    'help_key'             => 'Setting key ex: admin_title',
+    'help_option'          => '(optional, the same options as for the corresponding formfield apply)',
+    'add_new'              => 'Add New Setting',
+    'delete_question'      => 'Are you sure you want to delete the :setting Setting?',
+    'delete_confirm'       => 'Yes, Delete This Setting',
+    'successfully_created' => 'Successfully Created Settings',
+    'successfully_saved'   => 'Successfully Saved Settings',
+    'successfully_deleted' => 'Successfully Deleted Setting',
+    'already_at_top'       => 'This is already at the top of the list',
+    'already_at_bottom'    => 'This is already at the bottom of the list',
+    'key_already_exists'   => 'The key :key already exists',
+    'moved_order_up'       => 'Moved :name setting order up',
+    'moved_order_down'     => 'Moved :name setting order down',
+    'successfully_removed' => 'Successfully removed :name value',
+    'group_general'        => 'General',
+    'group_admin'          => 'Admin',
+    'group_site'           => 'Site',
+    'group'                => 'Group',
+    'help_group'           => 'Group this setting is assigned to',
+];
