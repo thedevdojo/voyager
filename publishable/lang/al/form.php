f65753d5e42f6d51c035a01850e639c1ab9607b3
Eric Fletcher <fletch3555@users.noreply.github.com>
2020-01-06 15:42:21 -0500

    Apply fixes from StyleCI (#4677)

--- publishable/lang/al/form.php
+++ publishable/lang/al/form.php
@@ -4,7 +4,7 @@ return [
     'field_password_keep'          => 'Lëreni bosh për të mbajtur të njëjtën',
     'field_select_dd_relationship' => 'Sigurohuni që të konfiguroni marrëdhënien e '
     .'duhur në metodën e metodës së klasa e klasës.',
-                                      'the :class class.',
+    'the :class class.',
     'type_checkbox'       => 'Kutia e Kontrollit',
     'type_codeeditor'     => 'Editor Kodi',
     'type_file'           => 'Skedar',
