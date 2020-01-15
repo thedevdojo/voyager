5b1935f2291e9b81d9755bf58a39b2a4e35fcf75
Spraynard <kellan.martin@gmail.com>
2020-01-15 07:11:55 -0500

    Enhance Widget Layout (#4559)
    * Updating initial changes to include a way to output more than 3 widgets on dashboard page.
    
    dimmers.blade.php now runs through an array filled with widgetGroups
    
    Voyager.php's 'dimmers()' function now returns an array filled wih WidgetGroups
    
    * Updating dimmers method in Voyager facade
    
    * Update dimmers.blade.php
    
    Removing old, commented code that is not needed anymore.
    
    * Updating voyager.php to fit with comment styling guidelines
    
    * Updating VoyagerTest.php
    
    Updating test and test documentation to fit with the current Dimmer paradigm
    
    Adding in a new test to go over a new constraint through this update. That a dimmer group can only have a max three dimmers.
    
    * update VoyagerTest.php
    
    * Update VoyagerTest.php
    
    Removing unnecessary test
    
    Co-authored-by: Eric Fletcher <fletch3555@users.noreply.github.com>
    Co-authored-by: Christoph Schweppe <info@cschweppe.de>

--- src/Voyager.php
+++ src/Voyager.php
@@ -183,24 +183,40 @@ class Voyager
     }
 
     /**
-     * Get a collection of the dashboard widgets.
+     * Get a collection of dashboard widgets.
+     * Each of our widget groups contain a max of three widgets.
+     * After that, we will switch to a new widget group.
      *
-     * @return \Arrilot\Widgets\WidgetGroup
+     * @return array - Array consisting of \Arrilot\Widget\WidgetGroup objects
      */
     public function dimmers()
     {
         $widgetClasses = config('voyager.dashboard.widgets');
-        $dimmers = Widget::group('voyager::dimmers');
+        $dimmerGroups = [];
+        $dimmerCount = 0;
+        $dimmers = Widget::group("voyager::dimmers-{$dimmerCount}");
 
         foreach ($widgetClasses as $widgetClass) {
             $widget = app($widgetClass);
 
             if ($widget->shouldBeDisplayed()) {
+
+                // Every third dimmer, we consider out WidgetGroup filled.
+                // We switch that out with another WidgetGroup.
+                if ($dimmerCount % 3 === 0 && $dimmerCount !== 0) {
+                    $dimmerGroups[] = $dimmers;
+                    $dimmerGroupTag = ceil($dimmerCount / 3);
+                    $dimmers = Widget::group("voyager::dimmers-{$dimmerGroupTag}");
+                }
+
                 $dimmers->addWidget($widgetClass);
+                $dimmerCount++;
             }
         }
 
-        return $dimmers;
+        $dimmerGroups[] = $dimmers;
+
+        return $dimmerGroups;
     }
 
     public function setting($key, $default = null)
