a9d256daf80c47b518cf3e355de3468dccacc961
Inani El Houssain <inanielhoussain@gmail.com>
2020-06-03 09:37:14 +0100

    Update master.blade.php

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -6,7 +6,7 @@
     <meta name="robots" content="none" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
     <meta name="description" content="admin login">
-    <title>Admin - {{ $title }}</title>
+    <title>@yield('title', 'Admin - '.Voyager::setting("admin.title"))</title>
     <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
     @if (__('voyager::generic.is_rtl') == 'true')
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css">
