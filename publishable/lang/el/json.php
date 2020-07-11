5a104edb1b0ab3a080692c82634b704ac09f1ce4
Velentzas Alexandros <fractalbit@gmail.com>
2020-07-11 21:43:14 +0300

    translated analytics, bread and generic

---
+++ publishable/lang/el/json.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'invalid'           => 'Invalid JSON',
+    'invalid_message'   => 'Seems like you introduced some invalid JSON.',
+    'valid'             => 'Valid JSON',
+    'validation_errors' => 'Validation errors',
+];
