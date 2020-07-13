2ebc82dd0252b70c54e657859acdd3174cca6726
Velentzas Alexandros <fractalbit@gmail.com>
2020-07-13 18:35:19 +0300

    made some improvements

--- publishable/lang/el/settings.php
+++ publishable/lang/el/settings.php
@@ -6,7 +6,7 @@ return [
     'new'                  => 'Νέα ρύθμιση',
     'help_name'            => 'Όνομα ρύθμισης. π.χ.: Τίτλος διαχείρισης',
     'help_key'             => 'Κλειδί ρύθμισης, π.χ.: admin_title',
-    'help_option'          => '(optional, the same options as for the corresponding formfield apply)',
+    'help_option'          => '(προαιρετικό, ισχύουν οι ίδιες επιλογές όπως για το αντίστοιχο πεδίο φόρμας)',
     'add_new'              => 'Προσθήκη νέας ρύθμισης',
     'delete_question'      => 'Είστε σίγουρος ότι θέλετε να διαγράψετε τη ρύθμιση :setting;',
     'delete_confirm'       => 'Ναι, διάγραψε τη ρύθμιση',
