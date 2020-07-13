ee39c06a096ec9e3745e9cbe9f03e07130449cde
Christoph Schweppe <info@cschweppe.de>
2020-07-13 18:54:50 +0000

    Apply fixes from StyleCI

--- publishable/lang/el/analytics.php
+++ publishable/lang/el/analytics.php
@@ -3,7 +3,7 @@
 return [
     'by_pageview'            => 'Ανά εμφάνιση σελίδας',
     'by_sessions'            => 'Ανά συνεδρία',
-    'by_users'               => 'Ανά χρήστες',    
+    'by_users'               => 'Ανά χρήστες',
     'no_client_id'           => 'Για να δείτε στατιστικά θα πρέπει να πάρετε το google analytics client id και να το προσθέσετε στις ρυθμίσεις για το κλειδί <code>google_analytics_client_id</code>. Πάρτε το κλειδί σας από την Google developer console:',
     'set_view'               => 'Διαλέξτε τύπο εμφάνισης',
     'this_vs_last_week'      => 'Σύγκριση αυτής της εβδομάδας με την προηγούμενη',
