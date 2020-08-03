6ed14bd77b4c882f04e8fed03b9761dd514502db
Igor de Lorenzi Andrade <igordelorenzi@gmail.com>
2020-08-02 22:32:53 -0300

    Simplification of looping structure.

--- resources/views/bread/read.blade.php
+++ resources/views/bread/read.blade.php
@@ -56,16 +56,14 @@
                                 <img class="img-responsive"
                                      src="{{ filter_var($dataTypeContent->{$row->field}, FILTER_VALIDATE_URL) ? $dataTypeContent->{$row->field} : Voyager::image($dataTypeContent->{$row->field}) }}">
                             @elseif($row->type == 'multiple_images')
-                                @php $image_files = json_decode($dataTypeContent->{$row->field}); @endphp
-                                @if (is_array($image_files) && count($image_files) > 0)
-                                    @foreach ($image_files as $file)
-                                        <img class="img-responsive"
-                                             style="display: inline-block; margin-top: 4px;"
-                                             src="{{ filter_var($file, FILTER_VALIDATE_URL) ? $file : Voyager::image($file) }}">
-                                    @endforeach
-                                @else
+                                @php $image_files = json_decode($dataTypeContent->{$row->field}) ?? []; @endphp
+                                @forelse ($image_files as $file)
+                                    <img class="img-responsive"
+                                         style="display: inline-block; margin-top: 4px;"
+                                         src="{{ filter_var($file, FILTER_VALIDATE_URL) ? $file : Voyager::image($file) }}">
+                                @empty
                                     {{ __('voyager::generic.none') }}
-                                @endif
+                                @endforelse
                             @elseif($row->type == 'relationship')
                                  @include('voyager::formfields.relationship', ['view' => 'read', 'options' => $row->details])
                             @elseif($row->type == 'select_dropdown' && property_exists($row->details, 'options') &&
