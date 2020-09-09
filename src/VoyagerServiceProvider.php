28affa35836b49754b9c020838d07694878ad1b7
MrCrayon <crayon@mysticrayon.com>
2020-09-09 15:51:37 +0200

    fixes paginator
    Laravel 8 uses Tailwind as default styling

--- src/VoyagerServiceProvider.php
+++ src/VoyagerServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
@@ -131,6 +132,8 @@ class VoyagerServiceProvider extends ServiceProvider
         });
 
         $this->bootTranslatorCollectionMacros();
+
+        Paginator::useBootstrap();
     }
 
     /**
