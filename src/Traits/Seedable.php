6e626ac80a54ea610dc29c7d694bbec4118ef1bd
MrCrayon <crayon@mysticrayon.com>
2020-09-23 18:00:53 +0200

    Check if class is namespaced

--- src/Traits/Seedable.php
+++ src/Traits/Seedable.php
@@ -6,10 +6,12 @@ trait Seedable
 {
     public function seed($class)
     {
-        if (class_exists('Database\\Seeders\\'.$class)) {
-            $class = 'Database\\Seeders\\'.$class;
-        } elseif (!class_exists($class)) {
+        if (!class_exists($class)) {
             require_once $this->seedersPath.$class.'.php';
+
+            if (class_exists('Database\\Seeders\\'.$class)) {
+                $class = 'Database\\Seeders\\'.$class;
+            }
         }
 
         with(new $class())->run();
