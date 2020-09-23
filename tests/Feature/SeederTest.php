a3b36d7944eba249b430337333c40aee3b955da3
MrCrayon <crayon@mysticrayon.com>
2020-09-23 04:33:34 +0200

    just check that test is failing for Laravel 8

---
+++ tests/Feature/SeederTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TCG\Voyager\Tests\Feature;
+
+use TCG\Voyager\Tests\TestCase;
+
+class SeederTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->install();
+    }
+
+    /**
+     * Test manaually seeding is working.
+     *
+     */
+    public function testVoyagerDatabaseSeederCanBeCalled()
+    {
+        $exception = null;
+        try {
+            $this->artisan('db:seed', ['--class' => 'VoyagerDatabaseSeeder']);
+        } catch (\Exception $exception) {}
+
+        $this->assertNull($exception, 'An exception was thrown');
+    }
+}
