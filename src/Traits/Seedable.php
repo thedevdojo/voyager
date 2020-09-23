ab87ef5d4145ce6adc137b376fa08b2e209b38de
MrCrayon <crayon@mysticrayon.com>
2020-09-23 17:28:21 +0200

    changed seedable

--- src/Traits/Seedable.php
+++ src/Traits/Seedable.php
@@ -6,7 +6,9 @@ trait Seedable
 {
     public function seed($class)
     {
-        if (!class_exists($class)) {
+        if (class_exists('Database\\Seeders\\'.$class)) {
+            $class = 'Database\\Seeders\\'.$class;
+        } elseif (!class_exists($class)) {
             require_once $this->seedersPath.$class.'.php';
         }
 
