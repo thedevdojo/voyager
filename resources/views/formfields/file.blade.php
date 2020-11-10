be29967a967d8219093482b40550275952cea02f
Serge LEDIG <serge.ledig@gmail.com>
2020-11-10 02:39:30 +0100

    Modif JS link
    Change href="#" to "javascript:;" Because page scroll to top
    Make "Download" transltable

--- resources/views/formfields/file.blade.php
+++ resources/views/formfields/file.blade.php
@@ -2,22 +2,18 @@
     @if(json_decode($dataTypeContent->{$row->field}) !== null)
         @foreach(json_decode($dataTypeContent->{$row->field}) as $file)
           <div data-field-name="{{ $row->field }}">
-            <a class="fileType" target="_blank"
-              href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}"
-              data-file-name="{{ $file->original_name }}" data-id="{{ $dataTypeContent->getKey() }}">
+            <a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}" data-file-name="{{ $file->original_name }}" data-id="{{ $dataTypeContent->getKey() }}">
               {{ $file->original_name ?: '' }}
             </a>
-            <a href="#" class="voyager-x remove-multi-file"></a>
+            <a href="javascript:;" class="voyager-x remove-multi-file"></a>
           </div>
         @endforeach
     @else
       <div data-field-name="{{ $row->field }}">
-        <a class="fileType" target="_blank"
-          href="{{ Storage::disk(config('voyager.storage.disk'))->url($dataTypeContent->{$row->field}) }}"
-          data-file-name="{{ $dataTypeContent->{$row->field} }}" data-id="{{ $dataTypeContent->getKey() }}">>
-          Download
+        <a class="fileType" target="_blank" href="{{ Storage::disk(config('voyager.storage.disk'))->url($dataTypeContent->{$row->field}) }}" data-file-name="{{ $dataTypeContent->{$row->field} }}" data-id="{{ $dataTypeContent->getKey() }}">>
+          {{ __('Download') }}
         </a>
-        <a href="#" class="voyager-x remove-single-file"></a>
+        <a href="javascript:;" class="voyager-x remove-single-file"></a>
       </div>
     @endif
 @endif
