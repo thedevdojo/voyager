62bc1a05a16bc9ce4a185298bc6347e34b48d646
MrCrayon <crayon@mysticrayon.com>
2021-01-09 05:53:39 +0100

    Let user choose if relationship is required
    Add required attribute in select

--- resources/views/formfields/relationship.blade.php
+++ resources/views/formfields/relationship.blade.php
@@ -28,6 +28,7 @@
                     data-get-items-field="{{$row->field}}"
                     @if(!is_null($dataTypeContent->getKey())) data-id="{{$dataTypeContent->getKey()}}" @endif
                     data-method="{{ !is_null($dataTypeContent->getKey()) ? 'edit' : 'add' }}"
+                    @if($row->required == 1) required @endif
                 >
                     @php
                         $model = app($options->model);
@@ -163,6 +164,7 @@
                         data-label="{{$options->label}}"
                         data-error-message="{{__('voyager::bread.error_tagging')}}"
                     @endif
+                    @if($row->required == 1) required @endif
                 >
 
                         @php
