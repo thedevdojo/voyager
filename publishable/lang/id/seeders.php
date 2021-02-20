690513ff8efe0342c58945eec0b92b99effc93fe
vonsogt <35516476+vonsogt@users.noreply.github.com>
2021-02-20 11:10:35 +0700

    wip

---
+++ publishable/lang/id/seeders.php
@@ -0,0 +1,93 @@
+<?php
+
+return [
+    'data_rows'  => [
+        'author'           => 'Penulis',
+        'avatar'           => 'Avatar',
+        'body'             => 'Konten',
+        'category'         => 'Kategori',
+        'created_at'       => 'Dibuat Pada',
+        'display_name'     => 'Nama tampilan',
+        'email'            => 'Email',
+        'excerpt'          => 'Kutipan',
+        'featured'         => 'Unggulan',
+        'id'               => 'ID',
+        'meta_description' => 'Meta Deskripsi',
+        'meta_keywords'    => 'Meta Kata Kunci',
+        'name'             => 'Nama',
+        'order'            => 'Urutan',
+        'page_image'       => 'Gambar Halaman',
+        'parent'           => 'Induk',
+        'password'         => 'Kata sandi',
+        'post_image'       => 'Gambar Post',
+        'remember_token'   => 'Ingat Token',
+        'role'             => 'Wewenang',
+        'seo_title'        => 'Judul SEO',
+        'slug'             => 'Slug',
+        'status'           => 'Status',
+        'title'            => 'Judul',
+        'updated_at'       => 'Diperbarui Pada',
+    ],
+    'data_types' => [
+        'category' => [
+            'singular' => 'Kategori',
+            'plural'   => 'Kategori(s)',
+        ],
+        'menu'     => [
+            'singular' => 'Menu',
+            'plural'   => 'Menu(s)',
+        ],
+        'page'     => [
+            'singular' => 'Halaman',
+            'plural'   => 'Halaman(s)',
+        ],
+        'post'     => [
+            'singular' => 'Post',
+            'plural'   => 'Post(s)',
+        ],
+        'role'     => [
+            'singular' => 'Wewenang',
+            'plural'   => 'Wewenang(s)',
+        ],
+        'user'     => [
+            'singular' => 'Pengguna',
+            'plural'   => 'Pengguna(s)',
+        ],
+    ],
+    'menu_items' => [
+        'bread'        => 'BREAD',
+        'categories'   => 'Kategori',
+        'compass'      => 'Kompas',
+        'dashboard'    => 'Dasbor',
+        'database'     => 'Database',
+        'media'        => 'Media',
+        'menu_builder' => 'Pembuat Menu',
+        'pages'        => 'Halaman',
+        'posts'        => 'Postingan',
+        'roles'        => 'Wewenang',
+        'settings'     => 'Pengaturan',
+        'tools'        => 'Alat',
+        'users'        => 'Pengguna',
+    ],
+    'roles'      => [
+        'admin' => 'Administrator',
+        'user'  => 'Pengguna Normal',
+    ],
+    'settings'   => [
+        'admin' => [
+            'background_image'           => 'Gambar Latar Belakang Admin',
+            'description'                => 'Deskripsi Admin',
+            'description_value'          => 'Selamat datang di Voyager. Admin untuk Laravel',
+            'google_analytics_client_id' => 'ID Klien Google Analytics (digunakan untuk dasbor admin)',
+            'icon_image'                 => 'Gambar Ikon Admin',
+            'loader'                     => 'Loading Admin',
+            'title'                      => 'Judul Admin',
+        ],
+        'site'  => [
+            'description'                  => 'Deskripsi situs',
+            'google_analytics_tracking_id' => 'ID Pelacakan Google Analytics',
+            'logo'                         => 'Logo Situs',
+            'title'                        => 'Judul Situs',
+        ],
+    ],
+];
