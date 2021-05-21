f99c35b5e2460f83cd93d44d851f75bb102432b5
Marcelo <marcelosantos89@gmail.com>
2021-05-21 12:41:53 +0100

    Fix error syntax error, unexpected ')'
    Some of the computers in our company were getting errors because of the comma on the end of line 112 giving syntax error, unexpected ')'.
    This fixed it for our PCs, maybe it's just windows PHP that gets this error.

--- src/Http/Controllers/VoyagerBaseController.php
+++ src/Http/Controllers/VoyagerBaseController.php
@@ -109,7 +109,7 @@ class VoyagerBaseController extends Controller
                     ])->leftJoin(
                         $row->details->table.' as joined',
                         $dataType->name.'.'.$row->details->column,
-                        'joined.'.$row->details->key,
+                        'joined.'.$row->details->key
                     );
                 }
 
