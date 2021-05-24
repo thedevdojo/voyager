683b78903e662bcb07810947bb50e5fa52f03a70
Vitor Micillo <vitormicillo@gmail.com>
2021-05-24 11:55:47 +0100

    Update Controller.php (#5336)
    This will correct the error generated when saving a new record with media picker type fields.

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -168,7 +168,11 @@ abstract class Controller extends BaseController
                 $data->{$row->field} = str_replace($uuid, $data->getKey(), $data->{$row->field});
             });
             $data->save();
-            if ($old_path != $new_path && !Storage::disk(config('voyager.storage.disk'))->exists($new_path)) {
+            if ($old_path != $new_path && 
+                !Storage::disk(config('voyager.storage.disk'))->exists($new_path) && 
+                Storage::disk(config('voyager.storage.disk'))->exists($old_path)
+                ) 
+            {
                 $request->session()->forget([$slug.'_path', $slug.'_uuid']);
                 Storage::disk(config('voyager.storage.disk'))->move($old_path, $new_path);
                 Storage::disk(config('voyager.storage.disk'))->deleteDirectory($folder_path);
