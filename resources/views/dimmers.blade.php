10037ffd8d2e62237730270bda6f391e2a4fd78f
Vitor Micillo <vitormicillo@gmail.com>
2021-06-07 10:23:45 +0100

    Update dimmers.blade.php
    For the user to have the ability to see 4 widgets on the dashboard based on the new configuration options

--- resources/views/dimmers.blade.php
+++ resources/views/dimmers.blade.php
@@ -9,7 +9,7 @@ $dimmerGroups = Voyager::dimmers();
     $classes = [
         'col-xs-12',
         'col-sm-'.($count >= 2 ? '6' : '12'),
-        'col-md-'.($count >= 3 ? '4' : ($count >= 2 ? '6' : '12')),
+        'col-md-'.($count >= 3 ? '3' : ($count >= 2 ? '6' : '12')),
     ];
     $class = implode(' ', $classes);
     $prefix = "<div class='{$class}'>";
