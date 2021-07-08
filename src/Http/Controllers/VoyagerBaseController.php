d3a1ce57ecd9a40e355323de8bf8365a2a68bc91
MrCrayon <crayon@mysticrayon.com>
2021-07-08 06:38:57 +0200

    Fix relationship filter when an accessor is used as label together with a scope
    $model->all() cannot be called after applying a scope while get() works with and without scope.

--- src/Http/Controllers/VoyagerBaseController.php
+++ src/Http/Controllers/VoyagerBaseController.php
@@ -907,7 +907,7 @@ class VoyagerBaseController extends Controller
                 if ($search) {
                     // If we are using additional_attribute as label
                     if (in_array($options->label, $additional_attributes)) {
-                        $relationshipOptions = $model->all();
+                        $relationshipOptions = $model->get();
                         $relationshipOptions = $relationshipOptions->filter(function ($model) use ($search, $options) {
                             return stripos($model->{$options->label}, $search) !== false;
                         });
