db7b5581694897c381850072cecdfff54a962a55
Ronan-Lenor <Ronan-Lenor@users.noreply.github.com>
2021-08-17 18:18:55 +0200

    Update 2021_08_17_120402_alter_translations_table.php

--- migrations/2021_08_17_120402_alter_translations_table.php
+++ migrations/2021_08_17_120402_alter_translations_table.php
@@ -1,1 +1,40 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Support\Facades\DB;
+
+class AlterTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // clean all possible dead key to avoid the Illuminate\Database\QueryException :
+        //  Integrity constraint violation: 1452 Cannot add or update a child row: a foreign key constraint fails
+        DB::table('translations')->whereNotIn('foreign_key', DB::table('data_rows')->select('id'))->delete();
+
+        Schema::table('translations', function (Blueprint $table)
+        {
+            $table->foreign('foreign_key')->references('id')->on('data_rows')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('translations', function (Blueprint $table)
+        {
+            $table->dropForeign(['foreign_key']);
+        });
+    }
+}
