d6a11ed5708033baa8b76ce6a6255223fc77ed90
aytiqaqash <aytiqaqash@gmail.com>
2022-02-05 15:03:44 -0500

    form.php has been translated into Azerbaijani

---
+++ publishable/lang/az/form.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'field_password_keep'          => 'Əgər şifrəni dəyişmək istəmirsinizsə, boş saxlayın',
+    'field_select_dd_relationship' => 'Mütləq müvafiq əlaqələri (relationship) :class sinifinin :method metodunda ayarlayın.',
+    'type_checkbox'                => 'Bayraq',
+    'type_codeeditor'              => 'Kod redaktoru',
+    'type_file'                    => 'Fayl',
+    'type_image'                   => 'Şəkil',
+    'type_radiobutton'             => 'Radio-knopka',
+    'type_richtextbox'             => 'Vizual redaktor',
+    'type_selectdropdown'          => 'Düşən siyahı',
+    'type_textarea'                => 'Çoxsətirli mətn',
+    'type_textbox'                 => 'Təksətirli mətn',
+];
