0983dd484e5ca4bed5a4319ee68f660fecf820fd
aytiqaqash <aytiqaqash@gmail.com>
2022-02-05 11:40:51 -0500

    dimmer.php has been translated into Azerbaijani

---
+++ publishable/lang/az/dimmer.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'page'           => 'səhifə|səhifələr',
+    'page_link_text' => 'Bütün səhifələr',
+    'page_text'      => 'Verilənlər bazasında :count :string',
+    'post'           => 'yazı|yazılar',
+    'post_link_text' => 'Bütün yazılar',
+    'post_text'      => 'Verilənlər bazasında :count :string',
+    'user'           => 'istifadəçi|istifadəçilər',
+    'user_link_text' => 'Bütün istifadəçilər',
+    'user_text'      => 'Verilənlər bazasında :count :string',
+];
