4ea6bc28fcd6268b7c4898d2b45e3657b4b3c3ed
ShaungBhone <56265471+ShaungBhone@users.noreply.github.com>
2022-02-18 16:57:53 +0630

    add Burmese translation to all files (#5492)

---
+++ publishable/lang/my/post.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'additional_fields'=> 'အပိုအကွက်များ',
+    'category'         => 'ပို့စ်အမျိုးအစား',
+    'content'          => 'Post Content',
+    'details'          => 'Post Details',
+    'excerpt'          => 'Excerpt <small>Small description of this post</small>',
+    'image'            => 'ပုံတင်ပါ။',
+    'meta_description' => 'Meta ဖော်ပြချက်',
+    'meta_keywords'    => 'Meta သော့ချက်စာလုံးများ',
+    'new'              => 'ပို့စ်အသစ်တစ်ခုဖန်တီးပါ။',
+    'seo_content'      => 'SEO အကြောင်းအရာ',
+    'seo_title'        => 'SEO ခေါင်းစဉ်',
+    'slug'             => 'URL slug',
+    'status'           => 'Post Status',
+    'status_draft'     => 'အကြမ်း',
+    'status_pending'   => 'ဆိုင်းငံ့ထားသည်။',
+    'status_published' => 'အခုဖွင့်တယ်။',
+    'title'            => 'Post Title',
+    'title_sub'        => 'ဤပို့စ်၏ခေါင်းစဉ်',
+    'update'           => 'ပို့စ်ကို အပ်ဒိတ်လုပ်ပါ။',
+];
