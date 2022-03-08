dc8c3d878fcde24c846c1f71c4db10c2427a6ec8
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2022-03-08 21:52:35 +0530

    Allow to override action view

--- resources/views/bread/partials/actions.blade.php
+++ resources/views/bread/partials/actions.blade.php
@@ -6,16 +6,24 @@
     @endphp
     @can ($action->getPolicy(), $data)
         @if ($action->shouldActionDisplayOnRow($data))
-            <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
-                <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
-            </a>
+            @if (method_exists($action, 'view'))
+                @include($action->view(), ['action' => $action, 'data' => $data])
+            @else
+                <a href="{{ $action->getRoute($dataType->name) }}" title="{{ $action->getTitle() }}" {!! $action->convertAttributesToHtml() !!}>
+                    <i class="{{ $action->getIcon() }}"></i> <span class="hidden-xs hidden-sm">{{ $action->getTitle() }}</span>
+                </a>
+            @endif
         @endif
     @endcan
 @elseif (method_exists($action, 'massAction'))
-    <form method="post" action="{{ route('voyager.'.$dataType->slug.'.action') }}" style="display:inline">
-        {{ csrf_field() }}
-        <button type="submit" {!! $action->convertAttributesToHtml() !!}><i class="{{ $action->getIcon() }}"></i>  {{ $action->getTitle() }}</button>
-        <input type="hidden" name="action" value="{{ get_class($action) }}">
-        <input type="hidden" name="ids" value="" class="selected_ids">
-    </form>
+    @if (method_exists($action, 'view'))
+        @include($action->view(), ['action' => $action, 'data' => null])
+    @else
+        <form method="post" action="{{ route('voyager.'.$dataType->slug.'.action') }}" style="display:inline">
+            {{ csrf_field() }}
+            <button type="submit" {!! $action->convertAttributesToHtml() !!}><i class="{{ $action->getIcon() }}"></i>  {{ $action->getTitle() }}</button>
+            <input type="hidden" name="action" value="{{ get_class($action) }}">
+            <input type="hidden" name="ids" value="" class="selected_ids">
+        </form>
+    @endif
 @endif
