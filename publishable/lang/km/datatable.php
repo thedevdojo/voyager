507811c273449491176cdc933ea863b696ae8078
Hel Mab <mabhelitc@gmail.com>
2022-08-06 19:09:50 +0700

    add khmer translation

---
+++ publishable/lang/km/datatable.php
@@ -0,0 +1,26 @@
+<?php
+
+// DataTable translations from: https://github.com/DataTables/Plugins/tree/master/i18n
+return [
+    'sEmptyTable'     => 'មិនមានទិន្នន័យក្នុងតារាងនេះទេ',
+    'sInfo'           => 'បង្ហាញជួរទី _START_ ដល់ទី _END_ ក្នុងចំណោម _TOTAL_ ជួរ',
+    'sInfoEmpty'      => 'បង្ហាញជួរទី 0 ដល់ទី 0 ក្នុងចំណោម 0 ជួរ',
+    'sInfoFiltered'   => '(បានចម្រាញ់ចេញពីទិន្នន័យសរុប _MAX_ ជួរ)',
+    'sInfoPostFix'    => '',
+    'sInfoThousands'  => ',',
+    'sLengthMenu'     => 'បង្ហាញ _MENU_ ជួរ',
+    'sLoadingRecords' => 'កំពុងផ្ទុក...',
+    'sProcessing'     => 'កំពុងដំណើរការ...',
+    'sSearch'         => 'ស្វែងរក:',
+    'sZeroRecords'    => 'មិនមានទិន្នន័យត្រូវតាមលក្ខខណ្ឌស្វែងរកទេ',
+    'oPaginate'       => [
+        'sFirst'    => 'ដំបូងគេ',
+        'sLast'     => 'ចុងក្រោយ',
+        'sNext'     => 'បន្ទាប់',
+        'sPrevious' => 'ក្រោយ',
+    ],
+    'oAria' => [
+        'sSortAscending'  => ': ចុចដើម្បីរៀបជួរឈរនេះតាមលំដាប់ឡើង',
+        'sSortDescending' => ': ចុចដើម្បីរៀបជួរឈរនេះតាមលំដាប់ចុះ',
+    ],
+];
---
+++ publishable/lang/km/date.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'last_week' => 'សប្តាហ៍មុន',
+    'last_year' => 'ឆ្នាំមុន',
+    'this_week' => 'សប្តាហ៍នេះ',
+    'this_year' => 'ឆ្នាំនេះ',
+];
