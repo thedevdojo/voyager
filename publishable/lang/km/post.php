673d39442e359048341993e73a38aa15a93643b4
Christoph Schweppe <info@cschweppe.de>
2022-10-20 16:20:20 +0200

    Squashed commit of the following:
    commit 4f3ba997c4e32e5962e407b71ec8d9d6025c8c2f
    Author: ហ៊ែល ម៉ាប់ <HELMAB@users.noreply.github.com>
    Date:   Thu Oct 20 20:55:56 2022 +0700
    
        add khmer translation (#5646)

---
+++ publishable/lang/km/post.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'additional_fields'=> 'ទម្រង់បញ្ជុលបន្ថែមព័ត៌មាន',
+    'category'         => 'ប្រភេទអត្តបទ',
+    'content'          => 'អត្តបទ',
+    'details'          => 'សេចក្តីលំអិត',
+    'excerpt'          => 'ដកស្រង់<small>ការពិពណ៌នាតូចនៃប្រកាសនេះ</small>',
+    'image'            => 'រូបភាព',
+    'meta_description' => 'សេចក្តីសង្ខេបនៃ Meta',
+    'meta_keywords'    => 'ពាក្យគន្លឺះនៃ Meta',
+    'new'              => 'បន្ថែមអត្តបទថ្មី',
+    'seo_content'      => 'មាតិកានៃ SEO',
+    'seo_title'        => 'ចំណងជើងនៃ SEO',
+    'slug'             => 'URL slug',
+    'status'           => 'ស្ថានភាពនៃអត្តបទ',
+    'status_draft'     => 'ព្រាង',
+    'status_pending'   => 'កំពុងរង់ចាំ',
+    'status_published' => 'បានចេញផ្សាយ',
+    'title'            => 'ចំណងជើងអត្តបទ',
+    'title_sub'        => 'ចំនងជើររងអត្តបទ',
+    'update'           => 'កែសម្រួលអត្តបទ',
+];
