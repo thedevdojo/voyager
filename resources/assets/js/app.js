bf39f593b3f582bfbaf31914c3e4daece210a5c5
Laode Muhammad Al Fatih <lamualfa@gmail.com>
2022-12-18 23:13:47 +0700

    Fix improperly 3rd sidebar dropdown (#5713)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -135,20 +135,17 @@ $(document).ready(function () {
         "dom": '<"top"fl<"clear">>rt<"bottom"ip<"clear">>'
     });
 
-    $(".side-menu .nav .dropdown").on('show.bs.collapse', function () {
-        return $(".side-menu .nav .dropdown .collapse").collapse('hide');
+    $(".side-menu .nav .dropdown").on("show.bs.collapse", function (e) {
+        return $(".side-menu .nav .dropdown .collapse").collapse("hide");
     });
 
-    $('.panel-collapse').on('hide.bs.collapse', function(e) {
-        var target = $(e.target);
-        if (!target.is('a')) {
-            target = target.parent();
+    $(".panel-collapse").on("hide.bs.collapse", function (e) {
+        let $target = $(e.target);
+        if ($target.parent('.collapse[aria-expanded="true"]').length ||
+            $target.find('.collapse[aria-expanded="false"]').length) {
+            e.stopPropagation();
+            e.preventDefault();
         }
-        if (!target.hasClass('collapsed')) {
-            return;
-        }
-        e.stopPropagation();
-        e.preventDefault();
     });
 
     $(document).on('click', '.panel-heading a.panel-action[data-toggle="panel-collapse"]', function (e) {
