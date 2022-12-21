0de4b0e1d39aa89663d4e19c579b50ae8ec4e268
Andrea Pozza <64172274+andreapozza@users.noreply.github.com>
2022-12-21 01:15:38 +0100

    Fix datatables responsive

---
+++ publishable/assets/lib/js/responsive.dataTables.min.js
@@ -0,0 +1,4 @@
+/*! DataTables styling wrapper for Responsive
+ * © SpryMedia Ltd - datatables.net/license
+ */
+!function(n){"function"==typeof define&&define.amd?define(["jquery","datatables.net-dt","datatables.net-responsive"],function(e){return n(e,window,document)}):"object"==typeof exports?module.exports=function(e,t){return e=e||window,(t=t||("undefined"!=typeof window?require("jquery"):require("jquery")(e))).fn.dataTable||require("datatables.net-dt")(e,t),t.fn.dataTable||require("datatables.net-responsive")(e,t),n(t,0,e.document)}:n(jQuery,window,document)}(function(e,t,n,d){"use strict";return e.fn.dataTable});
