08529d24565281942f33b0da0588043b88acbdee
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-10-24 23:46:12 -0600

    Add clean text helper function to remove HTML tags and entities

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -17,7 +17,7 @@ if (!function_exists('menu')) {
 if (!function_exists('voyager_asset')) {
     function voyager_asset($path, $secure = null)
     {
-        return route('voyager.voyager_assets').'?path='.urlencode($path);
+        return route('voyager.voyager_assets') . '?path=' . urlencode($path);
     }
 }
 
@@ -26,9 +26,18 @@ if (!function_exists('get_file_name')) {
     {
         preg_match('/(_)([0-9])+$/', $name, $matches);
         if (count($matches) == 3) {
-            return Illuminate\Support\Str::replaceLast($matches[0], '', $name).'_'.(intval($matches[2]) + 1);
+            return Illuminate\Support\Str::replaceLast($matches[0], '', $name) . '_' . (intval($matches[2]) + 1);
         } else {
-            return $name.'_1';
+            return $name . '_1';
         }
+    }
+}
+
+if (!function_exists('cleanText')) {
+    function cleanText($text)
+    {
+        $removedTags = preg_replace('/<[^>]*>/', '', $text);
+        $cleanedText = html_entity_decode($removedTags);
+        return $cleanedText;
     }
 }
